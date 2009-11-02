b3269c3ebb8d7a72339f06eb74480da5c5916b12
Andreas Pieber <anpi@gmx.at>
2009-11-02 15:49:45 +0100

    restructured startup
    minor restructuring to the startup class for better readability
    
    Signed-off-by: Andreas Pieber <anpi@gmx.at>

--- openengsb-config-ui/src/test/java/org/openengsb/config/Start.java
+++ openengsb-config-ui/src/test/java/org/openengsb/config/Start.java
@@ -17,49 +17,42 @@
  */
 package org.openengsb.config;
 
+import java.io.IOException;
+
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.bio.SocketConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
 
 public class Start {
-
     public static void main(String[] args) throws Exception {
         Server server = new Server();
         SocketConnector connector = new SocketConnector();
 
-        // Set some timeout options to make debugging easier.
+        configureTimeoutOptionsForDebugging(server, connector);
+        configureWebAppContext(server);
+        startServer(server);
+    }
+
+    private static void configureTimeoutOptionsForDebugging(Server server, SocketConnector connector) {
         connector.setMaxIdleTime(1000 * 60 * 60);
         connector.setSoLingerTime(-1);
         connector.setPort(8080);
         server.setConnectors(new Connector[] { connector });
+    }
 
+    private static void configureWebAppContext(Server server) {
         WebAppContext bb = new WebAppContext();
         bb.setServer(server);
         bb.setContextPath("/");
         bb.setWar("src/test/webapp");
-
-        // START JMX SERVER
-        // MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-        // MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
-        // server.getContainer().addEventListener(mBeanContainer);
-        // mBeanContainer.start();
-
         server.addHandler(bb);
+    }
 
-        try {
-            System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
-            server.start();
-            System.in.read();
-            System.out.println(">>> STOPPING EMBEDDED JETTY SERVER");
-            // while (System.in.available() == 0) {
-            // Thread.sleep(5000);
-            // }
-            server.stop();
-            server.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(100);
-        }
+    private static void startServer(Server server) throws Exception, IOException, InterruptedException {
+        server.start();
+        System.in.read();
+        server.stop();
+        server.join();
     }
 }
