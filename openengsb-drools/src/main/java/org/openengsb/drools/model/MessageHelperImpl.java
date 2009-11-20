61fe4724816d3b9b5ef5f5f69af110c944dc7037
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2009-11-20 13:40:08 +0100

    use non-static log so class can be easily renamed.

--- openengsb-drools/src/main/java/org/openengsb/drools/model/MessageHelperImpl.java
+++ openengsb-drools/src/main/java/org/openengsb/drools/model/MessageHelperImpl.java
@@ -1,21 +1,22 @@
 package org.openengsb.drools.model;
+
 /**
 
-Copyright 2009 OpenEngSB Division, Vienna University of Technology
+ Copyright 2009 OpenEngSB Division, Vienna University of Technology
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE\-2.0
+ http://www.apache.org/licenses/LICENSE\-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
-*/
+ */
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,7 +25,7 @@ import org.openengsb.drools.MessageHelper;
 @SuppressWarnings("serial")
 public class MessageHelperImpl implements MessageHelper {
 
-    private static Log log = LogFactory.getLog(MessageHelperImpl.class);
+    private Log log = LogFactory.getLog(getClass());
 
     @Override
     public boolean createIssue(String subject, String body) {
