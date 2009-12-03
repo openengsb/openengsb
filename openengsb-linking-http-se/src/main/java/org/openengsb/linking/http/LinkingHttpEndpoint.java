dc6f71f8740702741d21ad3211c2f83d0ea50612
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2009-12-03 15:34:50 +0100

    issue #196: finalize the SE and provide sample-SU

--- openengsb-linking-http-se/src/main/java/org/openengsb/linking/http/LinkingHttpEndpoint.java
+++ openengsb-linking-http-se/src/main/java/org/openengsb/linking/http/LinkingHttpEndpoint.java
@@ -1,5 +1,24 @@
+/**
+
+   Copyright 2009 OpenEngSB Division, Vienna University of Technology
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
 package org.openengsb.linking.http;
 
+import javax.jbi.management.DeploymentException;
+import javax.jbi.messaging.InOut;
 import javax.jbi.messaging.MessageExchange;
 import javax.jbi.messaging.NormalizedMessage;
 import javax.jbi.servicedesc.ServiceEndpoint;
@@ -24,6 +43,48 @@ public class LinkingHttpEndpoint extends ProviderEndpoint {
     private static Log log = LogFactory.getLog(LinkingHttpEndpoint.class);
     private static final SourceTransformer st = new SourceTransformer();
 
+    /*
+     * TODO configure with spring
+     */
+    private QName linkServiceName;
+    private QName jmsServiceName;
+
+    @Override
+    public void validate() throws DeploymentException {
+        if (linkServiceName == null || jmsServiceName == null) {
+            throw new DeploymentException("linkServiceName and jmsServiceName must be set");
+        }
+        super.validate();
+    }
+
+    /**
+     * @return the linkServiceName
+     */
+    public final QName getLinkServiceName() {
+        return linkServiceName;
+    }
+
+    /**
+     * @param linkServiceName the linkServiceName to set
+     */
+    public final void setLinkServiceName(QName linkServiceName) {
+        this.linkServiceName = linkServiceName;
+    }
+
+    /**
+     * @return the jmsServiceName
+     */
+    public final QName getJmsServiceName() {
+        return jmsServiceName;
+    }
+
+    /**
+     * @param jmsServiceName the jmsServiceName to set
+     */
+    public final void setJmsServiceName(QName jmsServiceName) {
+        this.jmsServiceName = jmsServiceName;
+    }
+
     public LinkingHttpEndpoint() {
         super();
         // TODO Auto-generated constructor stub
@@ -37,6 +98,13 @@ public class LinkingHttpEndpoint extends ProviderEndpoint {
     public LinkingHttpEndpoint(ServiceUnit serviceUnit, QName service, String endpoint) {
         super(serviceUnit, service, endpoint);
         // TODO Auto-generated constructor stub
+    }
+
+    private static final String linkRequestMessageString = "  <LinkQueryRequestMessage>" + "<body>"
+            + " <query>%s</query>" + "</body>" + "</LinkQueryRequestMessage>";
+
+    private String createLinkRequestMessage(String linkId) {
+        return String.format(linkRequestMessageString, linkId);
     }
 
     private void processMessage(MessageExchange exchange, NormalizedMessage in) throws Exception {
@@ -53,15 +121,29 @@ public class LinkingHttpEndpoint extends ProviderEndpoint {
             throw new IllegalArgumentException("invalid request-xml, parameter is missing");
         } else {
             linkId = children.item(0).getTextContent();
-            ip = children.item(0).getTextContent();
+            ip = children.item(1).getTextContent();
         }
         /* done parsing */
-
-        // TODO forward request to link-su
-
-        // TODO send response via jms
-
         log.info("requesting Link: " + linkId + " for ip: " + ip);
+
+        InOut linkEx = this.getExchangeFactory().createInOutExchange();
+        linkEx.setService(linkServiceName);
+
+        NormalizedMessage linkExIn = linkEx.createMessage();
+        linkExIn.setContent(new StringSource(createLinkRequestMessage(linkId)));
+        linkEx.setInMessage(linkExIn);
+        getChannel().sendSync(linkEx);
+        DOMSource linkResonse = st.toDOMSource(linkEx.getOutMessage().getContent());
+
+        log.info("link-service returned message: " + st.toString(linkResonse));
+        InOut jmsEx = this.getExchangeFactory().createInOutExchange();
+        jmsEx.setService(jmsServiceName);
+        NormalizedMessage jmsMsg = jmsEx.createMessage();
+        jmsMsg.setContent(linkResonse);
+        jmsEx.setInMessage(jmsMsg);
+
+        getChannel().send(jmsEx);
+
     }
 
     @Override
@@ -72,13 +154,8 @@ public class LinkingHttpEndpoint extends ProviderEndpoint {
     @Override
     protected void processInOut(MessageExchange exchange, NormalizedMessage in, NormalizedMessage out) throws Exception {
         processMessage(exchange, in);
+        // TODO return anything more or different?
         out.setContent(new StringSource("<success/>"));
-    }
-
-    @Override
-    public synchronized void start() throws Exception {
-        // new Thread(new ListenerThread(serverPort)).start();
-        super.start();
     }
 
 }
