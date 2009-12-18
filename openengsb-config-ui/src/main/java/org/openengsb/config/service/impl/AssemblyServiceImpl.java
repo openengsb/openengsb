755b844d4c077ada77b12b85607e8aeb7145a52f
Michael Zoech <michi.zoech@gmail.com>
2009-12-18 14:14:42 +0100

    service for holding the current constructed service assembly

---
+++ openengsb-config-ui/src/main/java/org/openengsb/config/service/impl/AssemblyServiceImpl.java
@@ -0,0 +1,38 @@
+/**
+
+   Copyright 2009 OpenEngSB Division, Vienna University of Technology
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+package org.openengsb.config.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openengsb.config.jbi.ServiceUnitInfo;
+import org.openengsb.config.service.AssemblyService;
+
+public class AssemblyServiceImpl implements AssemblyService {
+    private ArrayList<ServiceUnitInfo> serviceUnits = new ArrayList<ServiceUnitInfo>();
+
+    @Override
+    public void createNewAssembly() {
+        serviceUnits = new ArrayList<ServiceUnitInfo>();
+    }
+
+    @Override
+    public List<ServiceUnitInfo> getServiceUnits() {
+        return serviceUnits;
+    }
+}
