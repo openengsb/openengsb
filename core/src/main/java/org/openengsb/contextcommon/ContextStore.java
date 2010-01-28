213d963d1e484d8b825bf58417f01d6bfb9f72c5
Michael Handler <michael.handler@fullstop.at>
2010-01-28 22:26:10 +0100

    Change path to email configuration of cit workflow.
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

--- core/src/main/java/org/openengsb/contextcommon/ContextStore.java
+++ core/src/main/java/org/openengsb/contextcommon/ContextStore.java
@@ -176,7 +176,7 @@ public class ContextStore {
         setValue("42/event/defaultTarget/namespace", "urn:openengsb:drools");
         setValue("42/event/defaultTarget/servicename", "droolsService");
 
-        setValue("42/workflows/ci/notification/email", "email@openengsb.org");
+        setValue("42/workflows/ci/notification/recipient", "email@openengsb.org");
 
         setValue("42/issue/namespace", "urn:openengsb:issues");
         setValue("42/issue/servicename", "issuesService");
