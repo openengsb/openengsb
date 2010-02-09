c0197eec49a485a18b48915b0492a649469ff8e1
Florian Waltersdorfer <ignatiomobius@gmail.com>
2010-02-09 21:27:33 +0100

    now listing namespace in acmresponse message #7
    Signed-off-by: Florian Waltersdorfer <ignatiomobius@gmail.com>

--- features/edb/service-engine/src/main/java/org/openengsb/edb/jbi/endpoints/responses/DefaultAcmResponseBuilder.java
+++ features/edb/service-engine/src/main/java/org/openengsb/edb/jbi/endpoints/responses/DefaultAcmResponseBuilder.java
@@ -19,7 +19,7 @@ package org.openengsb.edb.jbi.endpoints.responses;
 
 public class DefaultAcmResponseBuilder implements EDBEndpointResponseBuilder {
     public String wrapIntoResponse(String body) {
-        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><acmResponseMessage><body>" + body
-                + "</body></acmResponseMessage>";
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><acmResponseMessage xmlns=\"http://www.openengsb.org/engsb/acm/response\"><body>"
+                + body + "</body></acmResponseMessage>";
     }
 }
