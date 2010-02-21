ea245592f1331a5102258f984a1c342e4c88e492
Michael Handler <michael.handler@fullstop.at>
2010-02-21 10:34:34 +0100

    Changed context endpoint so that it is part of the endpoint framework.
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

--- core/context/service-engine/src/main/java/org/openengsb/context/ContextEndpoint.java
+++ core/context/service-engine/src/main/java/org/openengsb/context/ContextEndpoint.java
@@ -13,21 +13,22 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-   
+
  */
 package org.openengsb.context;
 
+import javax.jbi.messaging.ExchangeStatus;
 import javax.jbi.messaging.MessageExchange;
 import javax.jbi.messaging.NormalizedMessage;
-import javax.jbi.messaging.RobustInOnly;
 import javax.xml.transform.TransformerException;
 
-import org.apache.servicemix.common.endpoints.ProviderEndpoint;
 import org.apache.servicemix.jbi.jaxp.SourceTransformer;
 import org.apache.servicemix.jbi.jaxp.StringSource;
 import org.openengsb.contextcommon.Context;
 import org.openengsb.contextcommon.ContextSegmentTransformer;
 import org.openengsb.contextcommon.ContextStore;
+import org.openengsb.core.MessageProperties;
+import org.openengsb.core.endpoints.OpenEngSBEndpoint;
 import org.openengsb.core.messaging.ListSegment;
 import org.openengsb.core.messaging.Segment;
 import org.openengsb.core.messaging.TextSegment;
@@ -38,52 +39,9 @@ import org.openengsb.util.serialization.SerializationException;
  * @org.apache.xbean.XBean element="contextEndpoint"
  *                         description="Context Component"
  */
-public class ContextEndpoint extends ProviderEndpoint {
+public class ContextEndpoint extends OpenEngSBEndpoint {
 
     private ContextStore contextStore = new ContextStore(WorkingDirectory.getFile("context", "contextdata.xml"));
-
-    @Override
-    protected void processInOnly(MessageExchange exchange, NormalizedMessage in) throws Exception {
-        if (!(exchange instanceof RobustInOnly)) {
-            throw new UnsupportedOperationException("Unsupported MEP: " + exchange.getPattern());
-        }
-
-        String operation = exchange.getOperation().getLocalPart();
-        String id = getContextId(in);
-
-        if (operation.equals("store")) {
-            handleStore(id, in);
-        } else {
-            // TODO throw specialized exception
-            throw new RuntimeException("Illegal operation: " + operation);
-        }
-    }
-
-    @Override
-    protected void processInOut(MessageExchange exchange, NormalizedMessage in, NormalizedMessage out) throws Exception {
-        String id = getContextId(in);
-        String operation = exchange.getOperation().getLocalPart();
-
-        String result = null;
-        if (operation == null) {
-            throw new RuntimeException("Operation not set");
-        } else if (operation.equals("request")) {
-            result = handleRequest(in, id);
-        } else if (operation.equals("store")) {
-            handleStore(id, in);
-            result = "<result>success</result>";
-        } else if (operation.equals("addContext")) {
-            handleAddContext(id, in);
-            result = "<result>success</result>";
-        } else if (operation.equals("remove")) {
-            handleRemove(id, in);
-            result = "<result>success</result>";
-        } else {
-            throw new RuntimeException("Illegal operation: " + operation);
-        }
-
-        out.setContent(new StringSource(result));
-    }
 
     private void handleAddContext(String id, NormalizedMessage in) throws SerializationException, TransformerException {
         SourceTransformer sourceTransformer = new SourceTransformer();
@@ -151,7 +109,58 @@ public class ContextEndpoint extends ProviderEndpoint {
         return segment.toXML();
     }
 
-    private String getContextId(NormalizedMessage in) {
-        return (String) in.getProperty("contextId");
+    @Override
+    protected final void processInOut(MessageExchange exchange, NormalizedMessage in, NormalizedMessage out)
+            throws Exception {
+        if (exchange.getStatus() != ExchangeStatus.ACTIVE) {
+            return;
+        }
+        MessageProperties msgProperties = readProperties(in);
+        String id = msgProperties.getContextId();
+        String operation = exchange.getOperation().getLocalPart();
+
+        String result = null;
+        if (operation == null) {
+            throw new RuntimeException("Operation not set");
+        } else if (operation.equals("request")) {
+            result = handleRequest(in, id);
+        } else if (operation.equals("store")) {
+            handleStore(id, in);
+            result = "<result>success</result>";
+        } else if (operation.equals("addContext")) {
+            handleAddContext(id, in);
+            result = "<result>success</result>";
+        } else if (operation.equals("remove")) {
+            handleRemove(id, in);
+            result = "<result>success</result>";
+        } else {
+            throw new RuntimeException("Illegal operation: " + operation);
+        }
+
+        out.setContent(new StringSource(result));
     }
+
+    @Override
+    protected void processInOnly(MessageExchange exchange, NormalizedMessage in) throws Exception {
+        if (exchange.getStatus() != ExchangeStatus.ACTIVE) {
+            return;
+        }
+        MessageProperties msgProperties = readProperties(in);
+        String id = msgProperties.getContextId();
+        String operation = exchange.getOperation().getLocalPart();
+
+        if (operation == null) {
+            throw new RuntimeException("Operation not set");
+        } else if (operation.equals("store")) {
+            handleStore(id, in);
+        } else if (operation.equals("addContext")) {
+            handleAddContext(id, in);
+        } else if (operation.equals("remove")) {
+            handleRemove(id, in);
+        } else {
+            throw new RuntimeException("Illegal operation: " + operation + " for message exchange pattern: "
+                    + exchange.getPattern());
+        }
+    }
+
 }
