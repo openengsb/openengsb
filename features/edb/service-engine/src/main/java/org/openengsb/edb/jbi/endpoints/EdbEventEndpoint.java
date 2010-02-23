ee37cafcdb61c6b32068ae62a1b67c44eeec504a
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2010-02-23 14:32:17 +0100

    use EventForwardingEndpoint

--- features/edb/service-engine/src/main/java/org/openengsb/edb/jbi/endpoints/EdbEventEndpoint.java
+++ features/edb/service-engine/src/main/java/org/openengsb/edb/jbi/endpoints/EdbEventEndpoint.java
@@ -1,23 +1,27 @@
-package org.openengsb.edb.jbi.endpoints;
-import org.openengsb.contextcommon.ContextHelper;
-import org.openengsb.core.EventHelper;
-import org.openengsb.core.EventHelperImpl;
-import org.openengsb.core.MessageProperties;
-import org.openengsb.core.endpoints.SimpleEventEndpoint;
-import org.openengsb.core.model.Event;
+/**
 
+   Copyright 2010 OpenEngSB Division, Vienna University of Technology
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+package org.openengsb.edb.jbi.endpoints;
+
+import org.openengsb.core.endpoints.EventForwardEndpoint;
 
 /**
  * @org.apache.xbean.XBean element="edbEvent" The Endpoint to the commit-feature
  *
  */
-public class EdbEventEndpoint extends SimpleEventEndpoint {
-
-    @Override
-    protected void handleEvent(Event e, ContextHelper contextHelper, MessageProperties msgProperties) {
-        EventHelper helper = new EventHelperImpl(this, msgProperties);
-        // simply forward to drools
-        helper.sendEvent(e, "urn:openengsb:drools", "droolsService");
-    }
-
+public class EdbEventEndpoint extends EventForwardEndpoint {
 }
