3ed5d38a250ed68111a57a5f47eed77f93ec349a
Christoph Leiter <openengsb@christophleiter.com>
2010-04-21 10:11:12 +0200

    Improve deployment script
    Signed-off-by: Christoph Leiter <openengsb@christophleiter.com>

--- scripts/deploy-smx41.sh
+++ scripts/deploy-smx41.sh
@@ -18,68 +18,71 @@
 # First parameter of this script is the servicemix path and second parameter the maven repository path.
 
 function usage() {
-  echo Usage: $0 servicemix-home maven-home
+  echo Usage: $0 '[servicemix-home [maven-home]]'
+  echo '  'This script will use \$SERVICEMIX_HOME and \$M2_HOME to determine where
+  echo '  'servicemix and the maven repository are located if these variables are set.
+  echo '  'If they aren\'t set, you have to provide the arguments.
+  echo '  'If \~/.m2/repository exists the script assumes it\'s your maven-home.
   exit 1
 }
 
-if [ $# != 2 ] ;
-then
+if [ $# -gt 0 ] ; then
+  SERVICEMIX_HOME=$1
+fi
+
+if [ ! -d ${SERVICEMIX_HOME:-X} ] ; then
+  echo Error: Can\'t find servicemix-home.
+  echo Set \$SERVICEMIX_HOME or provide argument 1
+  echo
   usage
 fi
 
-if [ ! -d $1/deploy ] ;
-then
-  echo Error: servicemix-home directory does not exist
+
+if [ $# -gt 1 ] ; then
+  M2_HOME=$2
+else
+# if M2_HOME not set then guess it is ~/.m2/repository
+  if [ x$M2_HOME = x ] ; then
+    if [ -d ~/.m2/repository ] ; then
+      M2_HOME=~/.m2/repository
+    fi
+  fi
+fi
+
+if [ ! -d ${M2_HOME:-X} ] ; then
+  echo Error: Can\'t find maven-home.
+  echo Set \$M2_HOME or provide argument 2
+  echo
   usage
 fi
 
-if [ ! -d $2/repository ] ;
-then
-  echo Error: maven-home directory does not exist
-  usage
-fi
 
 cd $(dirname $0)/..
 
 smx_comp_version=`grep '<servicemix.subprojects.version>' pom.xml | sed 's! *<[^>]*> *!!g'`
 openengsb_version=`grep '<version>' pom.xml | head -1 | sed 's! *<[^>]*> *!!g'`
 
-files="
-$2/repository/org/apache/servicemix/servicemix-shared/$smx_comp_version/servicemix-shared-${smx_comp_version}-installer.zip
-features/edb/core/target/openengsb-features-edb-core-${openengsb_version}.zip
-core/context/service-engine/target/openengsb-context-se-${openengsb_version}-installer.zip
-core/workflow/service-engine/target/openengsb-workflow-se-${openengsb_version}-installer.zip
-domains/build/implementation/service-engine/target/openengsb-domains-build-implementation-se-${openengsb_version}-installer.zip
-domains/build/maven/service-engine/target/openengsb-domains-build-maven-se-${openengsb_version}-installer.zip
-domains/deploy/implementation/service-engine/target/openengsb-domains-deploy-implementation-se-${openengsb_version}-installer.zip
-domains/deploy/maven/service-engine/target/openengsb-domains-deploy-maven-se-${openengsb_version}-installer.zip
-domains/issues/implementation/service-engine/target/openengsb-domains-issues-implementation-se-${openengsb_version}-installer.zip
-domains/issues/trac/service-engine/target/openengsb-domains-issues-trac-se-${openengsb_version}-installer.zip
-domains/notification/email/service-engine/target/openengsb-domains-notification-email-se-${openengsb_version}-installer.zip
-domains/notification/implementation/service-engine/target/openengsb-domains-notification-implementation-se-${openengsb_version}-installer.zip
-domains/notification/twitter/service-engine/target/openengsb-domains-notification-twitter-se-${openengsb_version}-installer.zip
-domains/report/implementation/service-engine/target/openengsb-domains-report-implementation-se-${openengsb_version}-installer.zip
-domains/report/plaintext/service-engine/target/openengsb-domains-report-plaintext-se-${openengsb_version}-installer.zip
-domains/scm/implementation/service-engine/target/openengsb-domains-scm-implementation-se-${openengsb_version}-installer.zip
-domains/scm/subversion/service-engine/target/openengsb-domains-scm-subversion-se-${openengsb_version}-installer.zip
-domains/test/implementation/service-engine/target/openengsb-domains-test-implementation-se-${openengsb_version}-installer.zip
-domains/test/maven/service-engine/target/openengsb-domains-test-maven-se-${openengsb_version}-installer.zip
-features/edb/service-engine/target/openengsb-features-edb-se-${openengsb_version}-installer.zip
-features/link/http-processor/service-engine/target/openengsb-features-link-http-processor-se-${openengsb_version}-installer.zip
-package/all/target/openengsb-package-all-${openengsb_version}.zip"
+echo Settings:
+echo "  OpenEngSB Version: $openengsb_version"
+echo "  Servicemix Version: $smx_comp_version"
+echo "  Servicemix Home: $SERVICEMIX_HOME"
+echo "  Maven Home: $M2_HOME"
+echo
 
-for file in $files;
-do
-  if [ ! -f $file ];
-  then
+files=`find . -iname '*-installer.zip'`
+files="$M2_HOME/org/apache/servicemix/servicemix-shared/$smx_comp_version/servicemix-shared-${smx_comp_version}-installer.zip $files"
+
+for file in $files; do
+  if [ ! -f $file ] ; then
     echo Error: Could not find file $file
     exit 1
   fi
 done
 
-for file in $files;
-do
-  echo Deploying `basename $file`
-  cp $file $1/deploy/
+echo Deploying:
+
+for file in $files; do
+  echo '  '`basename $file`
+  cp $file $SERVICEMIX_HOME/deploy/
 done
 
