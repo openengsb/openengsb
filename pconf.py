d3e760a9949e79228339180c9879cee35d01aac7
Andreas Pieber <anpieber@gmail.com>
2010-05-03 21:37:25 +0200

    added way to make update process easier
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

---
+++ pconf.py
@@ -0,0 +1,14 @@
+
+def ignored_version_files():
+	return ['domains/toolabstraction/maven/src/test/resources/pom.xml',
+		'domains/toolabstraction/maven/src/test/resources/build-test/pom.xml',
+		'domains/toolabstraction/maven/src/test/resources/deploy-test/pom.xml',
+		'domains/toolabstraction/maven/src/test/resources/test-invalid-pom/pom.xml',
+		'domains/toolabstraction/maven/src/test/resources/test-unit-fail/pom.xml',
+		'domains/toolabstraction/maven/src/test/resources/test-unit-success/pom.xml',
+		'domains/toolabstraction/maven/target/test-classes/test-invalid-pom/pom.xml',
+		'domains/toolabstraction/maven/target/test-classes/deploy-test/pom.xml',
+		'domains/toolabstraction/maven/target/test-classes/test-unit-success/pom.xml',
+		'domains/toolabstraction/maven/target/test-classes/build-test/pom.xml',
+		'domains/toolabstraction/maven/target/test-classes/test-unit-fail/pom.xml',
+		'domains/toolabstraction/maven/target/test-classes/pom.xml']
