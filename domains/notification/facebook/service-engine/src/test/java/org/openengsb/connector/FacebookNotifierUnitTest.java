6d26edbc06929c1f11f6360fa3ef3a6fa9baddc4
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2010-05-26 12:59:06 +0200

    facebook notification added facebook client login under toolabstraction/facebook

---
+++ domains/notification/facebook/service-engine/src/test/java/org/openengsb/connector/FacebookNotifierUnitTest.java
@@ -0,0 +1,16 @@
+package org.openengsb.connector;
+
+import org.openengsb.facebook.common.FacebookConnector;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bonomat
+ * Date: 26.05.2010
+ * Time: 12:23:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class FacebookNotifierUnitTest {
+
+    FacebookConnector facebooConnector;
+  
+}
---
+++ domains/toolabstraction/facebook/src/main/java/org/openengsb/facebook/common/FacebookConnectorImpl.java
@@ -0,0 +1,93 @@
+/**
+
+ Copyright 2010 OpenEngSB Division, Vienna University of Technology
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
+package org.openengsb.facebook.common;
+
+import com.google.code.facebookapi.FacebookException;
+import com.google.code.facebookapi.FacebookJaxbRestClient;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openengsb.facebook.common.exceptions.FacebookConnectorException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bonomat
+ * Date: 26.05.2010
+ * Time: 10:52:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class FacebookConnectorImpl implements FacebookConnector {
+    private static Log log = LogFactory.getLog(FacebookConnectorImpl.class);
+    private FacebookLoginImpl login;
+    private FacebookJaxbRestClient client;
+    private FacebookClientImpl facebookClient;
+
+    FacebookConnectorImpl(String email, String password, String API_KEY, String SECRET) {
+
+        try {
+            login = new FacebookLoginImpl();
+            login.initHttpClient();
+            login.loginUser(email, password, API_KEY);
+            client = login.createLoggedInFacebookClient(API_KEY, SECRET);
+        } catch (FacebookException e) {
+            handleFacebookException(e);
+        } catch (FacebookConnectorException e) {
+            handleFacebookException(e);
+        }
+        facebookClient = new FacebookClientImpl(client);
+    }
+
+    @Override
+    public void updateStatus(String message) {
+        try {
+            facebookClient.updateStatus(message);
+        } catch (FacebookException e) {
+            handleFacebookException(e);
+        }
+    }
+
+   // public FacebookClientImpl getFacebookClient() {
+   //     return facebookClient;
+   // }
+
+    private void handleFacebookException(FacebookException e) {
+        switch (e.getCode()) {
+            case -1:
+                log.error("HTTP - error on login, wrong password or username");
+                break;
+            default:
+                log.error("Action failed. Cause: " + e.getMessage() +" " +e.getCode() + " " );
+                e.printStackTrace();
+                break;
+        }
+    }
+
+
+    private void handleFacebookException(FacebookConnectorException e) {
+        switch (e.getStatusCode()) {
+            case -1:
+                log.error("HTTP - error befor login");
+                break;
+            case -2:
+                log.error("Incorrect or missing username or password. Authentication failed.");
+                break;
+            default:
+                log.error("Action failed. Cause: " + e.getMessage());
+                break;
+        }
+    }
+}
