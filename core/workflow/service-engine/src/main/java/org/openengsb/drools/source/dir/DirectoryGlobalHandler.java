d71a5215dae58c2c0ece843f38aba28eae83f19c
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2010-06-07 11:21:27 +0200

    use abstract class for shared code between import- and globals-handler

--- core/workflow/service-engine/src/main/java/org/openengsb/drools/source/dir/DirectoryGlobalHandler.java
+++ core/workflow/service-engine/src/main/java/org/openengsb/drools/source/dir/DirectoryGlobalHandler.java
@@ -17,42 +17,36 @@
  */
 package org.openengsb.drools.source.dir;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.TreeSet;
 
 import org.openengsb.drools.RuleBaseException;
 import org.openengsb.drools.source.DirectoryRuleSource;
-import org.openengsb.drools.source.ResourceHandler;
 
-public class DirectoryGlobalHandler extends ResourceHandler<DirectoryRuleSource> {
-
-    private File globalsFile;
+public class DirectoryGlobalHandler extends SingleFileResourceHandler {
 
     public DirectoryGlobalHandler(DirectoryRuleSource source) {
         super(source);
-        globalsFile = new File(source.getPath() + File.separator + DirectoryRuleSource.GLOBALS_FILENAME);
+    }
+
+    @Override
+    public String getFileName() {
+        return DirectoryRuleSource.GLOBALS_FILENAME;
     }
 
     @Override
     public void create(String name, String code) throws RuleBaseException {
-        Set<String> globalList = readGlobals();
+        Set<String> globalList = readFile();
         String line = String.format("%s %s", code, name);
         globalList.add(line);
-        writeGlobals(globalList);
+        writeFile(globalList);
         source.readRuleBase();
     }
 
     @Override
     public void delete(String name) throws RuleBaseException {
-        Set<String> globalList = readGlobals();
+        Set<String> globalList = readFile();
         Iterator<String> it = globalList.iterator();
         String line;
         for (line = it.next(); it.hasNext(); line = it.next()) {
@@ -61,48 +55,12 @@ public class DirectoryGlobalHandler extends ResourceHandler<DirectoryRuleSource>
                 break;
             }
         }
-        writeGlobals(globalList);
+        writeFile(globalList);
     }
 
     @Override
     public String get(String name) throws RuleBaseException {
         return source.getPackage().getGlobals().get(name);
-    }
-
-    private Set<String> readGlobals() throws RuleBaseException {
-        try {
-            return doReadGlobals();
-        } catch (IOException e) {
-            throw new RuleBaseException("cannot read imports", e);
-        }
-    }
-
-    private Set<String> doReadGlobals() throws IOException {
-        Set<String> result = new TreeSet<String>();
-        BufferedReader reader = new BufferedReader(new FileReader(globalsFile));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            result.add(line);
-        }
-        reader.close();
-        return result;
-    }
-
-    private void writeGlobals(Collection<String> list) throws RuleBaseException {
-        try {
-            doWriteGlobals(list);
-        } catch (IOException e) {
-            throw new RuleBaseException("cannot write imports", e);
-        }
-    }
-
-    private void doWriteGlobals(Collection<String> list) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new FileWriter(globalsFile));
-        for (String line : list) {
-            bw.write(line);
-            bw.newLine();
-        }
-        bw.close();
     }
 
     @Override
--- core/workflow/service-engine/src/main/java/org/openengsb/drools/source/dir/DirectoryImportHandler.java
+++ core/workflow/service-engine/src/main/java/org/openengsb/drools/source/dir/DirectoryImportHandler.java
@@ -17,34 +17,27 @@
  */
 package org.openengsb.drools.source.dir;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Collection;
-import java.util.Set;
-import java.util.TreeSet;
 
 import org.openengsb.drools.RuleBaseException;
 import org.openengsb.drools.source.DirectoryRuleSource;
-import org.openengsb.drools.source.ResourceHandler;
 
-public class DirectoryImportHandler extends ResourceHandler<DirectoryRuleSource> {
-
-    private File importsFile;
+public class DirectoryImportHandler extends SingleFileResourceHandler {
 
     public DirectoryImportHandler(DirectoryRuleSource source) {
         super(source);
-        importsFile = new File(source.getPath() + File.separator + DirectoryRuleSource.IMPORTS_FILENAME);
+    }
+
+    @Override
+    public String getFileName() {
+        return DirectoryRuleSource.IMPORTS_FILENAME;
     }
 
     @Override
     public void create(String name, String code) throws RuleBaseException {
-        Collection<String> imports = readImports();
+        Collection<String> imports = readFile();
         imports.add(name);
-        writeImports(imports);
+        writeFile(imports);
         System.err.println("reread rulebase");
         source.readRuleBase();
 
@@ -52,56 +45,22 @@ public class DirectoryImportHandler extends ResourceHandler<DirectoryRuleSource>
 
     @Override
     public void delete(String name) throws RuleBaseException {
-        Collection<String> imports = readImports();
+        Collection<String> imports = readFile();
         imports.remove(name);
-        writeImports(imports);
+        writeFile(imports);
         source.getRulebase().getPackage("org.openengsb").removeImport(name);
     }
 
     @Override
     public String get(String name) throws RuleBaseException {
-        // TODO Auto-generated method stub
+        if (source.getPackage().getImports().containsKey(name)) {
+            return name;
+        }
         return null;
     }
 
     @Override
     public Collection<String> list() throws RuleBaseException {
         return source.getPackage().getImports().keySet();
-    }
-
-    private Set<String> readImports() throws RuleBaseException {
-        try {
-            return doReadImports();
-        } catch (IOException e) {
-            throw new RuleBaseException("cannot read imports", e);
-        }
-    }
-
-    private Set<String> doReadImports() throws IOException {
-        Set<String> result = new TreeSet<String>();
-        BufferedReader reader = new BufferedReader(new FileReader(importsFile));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            result.add(line);
-        }
-        reader.close();
-        return result;
-    }
-
-    private void writeImports(Collection<String> list) throws RuleBaseException {
-        try {
-            doWriteImports(list);
-        } catch (IOException e) {
-            throw new RuleBaseException("cannot write imports", e);
-        }
-    }
-
-    private void doWriteImports(Collection<String> list) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new FileWriter(importsFile));
-        for (String line : list) {
-            bw.write(line);
-            bw.newLine();
-        }
-        bw.close();
     }
 }
