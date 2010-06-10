19df4a98083b4c7ee224aae5c3a3a6ae67566e75
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2010-06-10 11:38:00 +0200

    merge common code from Rule and FunctionHandler

--- core/workflow/service-engine/src/main/java/org/openengsb/drools/source/dir/DirectoryFunctionHandler.java
+++ core/workflow/service-engine/src/main/java/org/openengsb/drools/source/dir/DirectoryFunctionHandler.java
@@ -17,9 +17,6 @@
  */
 package org.openengsb.drools.source.dir;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -28,70 +25,25 @@ import org.openengsb.drools.RuleBaseException;
 import org.openengsb.drools.message.RuleBaseElementId;
 import org.openengsb.drools.message.RuleBaseElementType;
 import org.openengsb.drools.source.DirectoryRuleSource;
-import org.openengsb.drools.source.ResourceHandler;
 
-public class DirectoryFunctionHandler extends ResourceHandler<DirectoryRuleSource> {
+public class DirectoryFunctionHandler extends MultiFileResourceHandler {
 
     public DirectoryFunctionHandler(DirectoryRuleSource source) {
         super(source);
     }
 
     @Override
-    public void create(RuleBaseElementId name, String code) throws RuleBaseException {
-        File funcFile = source.getFilePath(name);
-        if (funcFile.exists()) {
-            throw new RuleBaseException("File already exists");
+    protected Collection<RuleBaseElementId> listElementsInPackage(Package p) {
+        Collection<RuleBaseElementId> result = new HashSet<RuleBaseElementId>();
+        for (String s : p.getFunctions().keySet()) {
+            result.add(new RuleBaseElementId(RuleBaseElementType.Function, p.getName(), s));
         }
-        if (!funcFile.getParentFile().exists()) {
-            funcFile.getParentFile().mkdirs();
-        }
-        FileWriter fw;
-        try {
-            fw = new FileWriter(funcFile);
-            fw.append(code);
-            fw.close();
-        } catch (IOException e) {
-            throw new RuleBaseException("could not write the function to the filesystem", e);
-        }
-        source.readRuleBase();
+        return result;
     }
 
     @Override
-    public void delete(RuleBaseElementId name) throws RuleBaseException {
-        File ruleFile = source.getFilePath(name);
-        if (!ruleFile.exists()) {
-            // fail silently if the function does not exist
-            return;
-        }
-        ruleFile.delete();
+    protected void removeFromRuleBase(RuleBaseElementId name) throws RuleBaseException {
         source.getRulebase().removeFunction(name.getPackageName(), name.getName());
-    }
-
-    @Override
-    public String get(RuleBaseElementId name) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Collection<RuleBaseElementId> list() throws RuleBaseException {
-        Collection<RuleBaseElementId> result = new HashSet<RuleBaseElementId>();
-        for (Package p : source.getRulebase().getPackages()) {
-            String packageName = p.getName();
-            for (String s : p.getFunctions().keySet()) {
-                result.add(new RuleBaseElementId(RuleBaseElementType.Function, packageName, s));
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public Collection<RuleBaseElementId> list(String packageName) throws RuleBaseException {
-        Collection<RuleBaseElementId> result = new HashSet<RuleBaseElementId>();
-        for (String name : source.getRulebase().getPackage(packageName).getFunctions().keySet()) {
-            result.add(new RuleBaseElementId(RuleBaseElementType.Function, packageName, name));
-        }
-        return result;
     }
 
 }
--- core/workflow/service-engine/src/main/java/org/openengsb/drools/source/dir/DirectoryRuleHandler.java
+++ core/workflow/service-engine/src/main/java/org/openengsb/drools/source/dir/DirectoryRuleHandler.java
@@ -17,75 +17,33 @@
  */
 package org.openengsb.drools.source.dir;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
 
+import org.drools.rule.Package;
 import org.drools.rule.Rule;
 import org.openengsb.drools.RuleBaseException;
 import org.openengsb.drools.message.RuleBaseElementId;
 import org.openengsb.drools.message.RuleBaseElementType;
 import org.openengsb.drools.source.DirectoryRuleSource;
-import org.openengsb.drools.source.ResourceHandler;
 
-public class DirectoryRuleHandler extends ResourceHandler<DirectoryRuleSource> {
+public class DirectoryRuleHandler extends MultiFileResourceHandler {
 
     public DirectoryRuleHandler(DirectoryRuleSource source) {
         super(source);
     }
 
     @Override
-    public void create(RuleBaseElementId name, String code) throws RuleBaseException {
-        File ruleFile = source.getFilePath(name);
-        if (ruleFile.exists()) {
-            throw new RuleBaseException("File already exists");
-        }
-        if (!ruleFile.getParentFile().exists()) {
-            ruleFile.getParentFile().mkdirs();
-        }
-        FileWriter fw;
-        try {
-            fw = new FileWriter(ruleFile);
-            fw.append(code);
-            fw.close();
-        } catch (IOException e) {
-            // ruleFile.delete();
-            throw new RuleBaseException("could not write the rule to the filesystem", e);
-        }
-        source.readRuleBase();
-    }
-
-    @Override
-    public void delete(RuleBaseElementId name) throws RuleBaseException {
-        File ruleFile = source.getFilePath(name);
-        if (!ruleFile.exists()) {
-            // fail silently if the rule does not exist
-            return;
-        }
-        ruleFile.delete();
-        source.getRulebase().removeRule(name.getPackageName(), name.getName());
-    }
-
-    @Override
-    public String get(RuleBaseElementId name) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Collection<RuleBaseElementId> list(String packageName) throws RuleBaseException {
+    protected Collection<RuleBaseElementId> listElementsInPackage(Package p) {
         Collection<RuleBaseElementId> result = new HashSet<RuleBaseElementId>();
-        for (Rule rule : source.getRulebase().getPackage(packageName).getRules()) {
-            result.add(new RuleBaseElementId(RuleBaseElementType.Function, packageName, rule.getName()));
+        for (Rule rule : p.getRules()) {
+            result.add(new RuleBaseElementId(RuleBaseElementType.Function, p.getName(), rule.getName()));
         }
         return result;
     }
 
     @Override
-    public Collection<RuleBaseElementId> list() throws RuleBaseException {
-        // TODO Auto-generated method stub
-        return null;
+    protected void removeFromRuleBase(RuleBaseElementId name) throws RuleBaseException {
+        source.getRulebase().removeRule(name.getPackageName(), name.getName());
     }
 }
