15f50e870d7704764ad1a3ce245dfe5a9d5e832b
Michael Zoech <michi.zoech@gmail.com>
2010-06-18 16:54:30 +0200

    annotate mappablelist class

--- core/core/src/main/java/org/openengsb/core/xmlmapping/XMLMappableList.java
+++ core/core/src/main/java/org/openengsb/core/xmlmapping/XMLMappableList.java
@@ -4,34 +4,19 @@ package org.openengsb.core.xmlmapping;
 import java.util.ArrayList;
 import java.util.List;
 
-/** 
- * Schema fragment(s) for this class:
- * <pre>
- * &lt;xs:complexType xmlns:xs="http://www.w3.org/2001/XMLSchema" name="XMLMappableList">
- *   &lt;xs:sequence>
- *     &lt;xs:element type="XMLMappable" name="mappable" maxOccurs="unbounded"/>
- *   &lt;/xs:sequence>
- * &lt;/xs:complexType>
- * </pre>
- */
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
 public class XMLMappableList
 {
     private List<XMLMappable> mappableList = new ArrayList<XMLMappable>();
 
-    /** 
-     * Get the list of 'mappable' element items.
-     * 
-     * @return list
-     */
+    @XmlElement(required = true)
     public List<XMLMappable> getMappables() {
         return mappableList;
     }
 
-    /** 
-     * Set the list of 'mappable' element items.
-     * 
-     * @param list
-     */
     public void setMappables(List<XMLMappable> list) {
         mappableList = list;
     }
