2634ac74480c48716144af21c29a79c7b9c66496
Michael Zoech <michi.zoech@gmail.com>
2010-06-18 16:46:39 +0200

    annotate typedvalue class

--- core/core/src/main/java/org/openengsb/core/xmlmapping/XMLTypedValue.java
+++ core/core/src/main/java/org/openengsb/core/xmlmapping/XMLTypedValue.java
@@ -1,54 +1,29 @@
 
 package org.openengsb.core.xmlmapping;
 
-/** 
- * Schema fragment(s) for this class:
- * <pre>
- * &lt;xs:complexType xmlns:xs="http://www.w3.org/2001/XMLSchema" name="XMLTypedValue">
- *   &lt;xs:sequence>
- *     &lt;xs:element type="xs:string" name="type"/>
- *     &lt;xs:element type="XMLMappable" name="value"/>
- *   &lt;/xs:sequence>
- * &lt;/xs:complexType>
- * </pre>
- */
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(propOrder = { "type", "value" })
 public class XMLTypedValue
 {
     private String type;
     private XMLMappable value;
 
-    /** 
-     * Get the 'type' element value.
-     * 
-     * @return value
-     */
+    @XmlElement(required = true)
     public String getType() {
         return type;
     }
 
-    /** 
-     * Set the 'type' element value.
-     * 
-     * @param type
-     */
     public void setType(String type) {
         this.type = type;
     }
 
-    /** 
-     * Get the 'value' element value.
-     * 
-     * @return value
-     */
+    @XmlElement(required = true)
     public XMLMappable getValue() {
         return value;
     }
 
-    /** 
-     * Set the 'value' element value.
-     * 
-     * @param value
-     */
     public void setValue(XMLMappable value) {
         this.value = value;
     }
