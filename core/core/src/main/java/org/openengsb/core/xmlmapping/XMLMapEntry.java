2f97236134a4a6798ddbfcc4d0f52b0f271860f2
Michael Zoech <michi.zoech@gmail.com>
2010-06-18 16:50:12 +0200

    annotate mapentry class

--- core/core/src/main/java/org/openengsb/core/xmlmapping/XMLMapEntry.java
+++ core/core/src/main/java/org/openengsb/core/xmlmapping/XMLMapEntry.java
@@ -1,54 +1,29 @@
 
 package org.openengsb.core.xmlmapping;
 
-/** 
- * Schema fragment(s) for this class:
- * <pre>
- * &lt;xs:complexType xmlns:xs="http://www.w3.org/2001/XMLSchema" name="XMLMapEntry">
- *   &lt;xs:sequence>
- *     &lt;xs:element type="XMLMappable" name="key"/>
- *     &lt;xs:element type="XMLMappable" name="value"/>
- *   &lt;/xs:sequence>
- * &lt;/xs:complexType>
- * </pre>
- */
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(propOrder = { "key", "value" })
 public class XMLMapEntry
 {
     private XMLMappable key;
     private XMLMappable value;
 
-    /** 
-     * Get the 'key' element value.
-     * 
-     * @return value
-     */
+    @XmlElement(required = true)
     public XMLMappable getKey() {
         return key;
     }
 
-    /** 
-     * Set the 'key' element value.
-     * 
-     * @param key
-     */
     public void setKey(XMLMappable key) {
         this.key = key;
     }
 
-    /** 
-     * Get the 'value' element value.
-     * 
-     * @return value
-     */
+    @XmlElement(required = true)
     public XMLMappable getValue() {
         return value;
     }
 
-    /** 
-     * Set the 'value' element value.
-     * 
-     * @param value
-     */
     public void setValue(XMLMappable value) {
         this.value = value;
     }
