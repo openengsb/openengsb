d12fbfccff3cacdc29aff02d55da4ab34135bb38
Michael Zoech <michi.zoech@gmail.com>
2010-06-18 16:46:59 +0200

    annotate bean class

--- core/core/src/main/java/org/openengsb/core/xmlmapping/XMLBean.java
+++ core/core/src/main/java/org/openengsb/core/xmlmapping/XMLBean.java
@@ -4,54 +4,29 @@ package org.openengsb.core.xmlmapping;
 import java.util.ArrayList;
 import java.util.List;
 
-/** 
- * Schema fragment(s) for this class:
- * <pre>
- * &lt;xs:complexType xmlns:xs="http://www.w3.org/2001/XMLSchema" name="XMLBean">
- *   &lt;xs:sequence>
- *     &lt;xs:element type="xs:string" name="className"/>
- *     &lt;xs:element type="XMLField" name="fields" maxOccurs="unbounded"/>
- *   &lt;/xs:sequence>
- * &lt;/xs:complexType>
- * </pre>
- */
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(propOrder = { "className", "fields" })
 public class XMLBean
 {
     private String className;
     private List<XMLField> fieldList = new ArrayList<XMLField>();
 
-    /** 
-     * Get the 'className' element value.
-     * 
-     * @return value
-     */
+    @XmlElement(required = true)
     public String getClassName() {
         return className;
     }
 
-    /** 
-     * Set the 'className' element value.
-     * 
-     * @param className
-     */
     public void setClassName(String className) {
         this.className = className;
     }
 
-    /** 
-     * Get the list of 'fields' element items.
-     * 
-     * @return list
-     */
+    @XmlElement(required = true)
     public List<XMLField> getFields() {
         return fieldList;
     }
 
-    /** 
-     * Set the list of 'fields' element items.
-     * 
-     * @param list
-     */
     public void setFields(List<XMLField> list) {
         fieldList = list;
     }
