8a3873ebbb7508132c081f204545f12e03c55487
Michael Zoech <michi.zoech@gmail.com>
2010-06-18 16:51:50 +0200

    annotate mapentrylist class

--- core/core/src/main/java/org/openengsb/core/xmlmapping/XMLMapEntryList.java
+++ core/core/src/main/java/org/openengsb/core/xmlmapping/XMLMapEntryList.java
@@ -4,34 +4,19 @@ package org.openengsb.core.xmlmapping;
 import java.util.ArrayList;
 import java.util.List;
 
-/** 
- * Schema fragment(s) for this class:
- * <pre>
- * &lt;xs:complexType xmlns:xs="http://www.w3.org/2001/XMLSchema" name="XMLMapEntryList">
- *   &lt;xs:sequence>
- *     &lt;xs:element type="XMLMapEntry" name="mapEntry" maxOccurs="unbounded"/>
- *   &lt;/xs:sequence>
- * &lt;/xs:complexType>
- * </pre>
- */
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
 public class XMLMapEntryList
 {
     private List<XMLMapEntry> mapEntryList = new ArrayList<XMLMapEntry>();
 
-    /** 
-     * Get the list of 'mapEntry' element items.
-     * 
-     * @return list
-     */
+    @XmlElement(required = true)
     public List<XMLMapEntry> getMapEntries() {
         return mapEntryList;
     }
 
-    /** 
-     * Set the list of 'mapEntry' element items.
-     * 
-     * @param list
-     */
     public void setMapEntries(List<XMLMapEntry> list) {
         mapEntryList = list;
     }
