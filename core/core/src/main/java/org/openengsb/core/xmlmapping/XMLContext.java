29254946eaffbfb38d8d7acd87f032ae10271dd9
Michael Zoech <michi.zoech@gmail.com>
2010-06-18 16:47:17 +0200

    annotate context class

--- core/core/src/main/java/org/openengsb/core/xmlmapping/XMLContext.java
+++ core/core/src/main/java/org/openengsb/core/xmlmapping/XMLContext.java
@@ -4,34 +4,21 @@ package org.openengsb.core.xmlmapping;
 import java.util.ArrayList;
 import java.util.List;
 
-/** 
- * Schema fragment(s) for this class:
- * <pre>
- * &lt;xs:complexType xmlns:xs="http://www.w3.org/2001/XMLSchema" name="XMLContext">
- *   &lt;xs:sequence>
- *     &lt;xs:element type="XMLContextEntry" name="entries" maxOccurs="unbounded"/>
- *   &lt;/xs:sequence>
- * &lt;/xs:complexType>
- * </pre>
- */
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement
+@XmlType
 public class XMLContext
 {
     private List<XMLContextEntry> entryList = new ArrayList<XMLContextEntry>();
 
-    /** 
-     * Get the list of 'entries' element items.
-     * 
-     * @return list
-     */
+    @XmlElement(required = true)
     public List<XMLContextEntry> getEntries() {
         return entryList;
     }
 
-    /** 
-     * Set the list of 'entries' element items.
-     * 
-     * @param list
-     */
     public void setEntries(List<XMLContextEntry> list) {
         entryList = list;
     }
