836fdf201a0827f0031e06718c0e3c10b1d9db9d
Michael Zoech <michi.zoech@gmail.com>
2010-06-18 16:45:58 +0200

    annotate returnvalue class

--- core/core/src/main/java/org/openengsb/core/xmlmapping/XMLReturnValue.java
+++ core/core/src/main/java/org/openengsb/core/xmlmapping/XMLReturnValue.java
@@ -1,36 +1,21 @@
 
 package org.openengsb.core.xmlmapping;
 
-/** 
- * Schema fragment(s) for this class:
- * <pre>
- * &lt;xs:element xmlns:xs="http://www.w3.org/2001/XMLSchema" name="XMLReturnValue">
- *   &lt;xs:complexType>
- *     &lt;xs:sequence>
- *       &lt;xs:element type="XMLTypedValue" name="value"/>
- *     &lt;/xs:sequence>
- *   &lt;/xs:complexType>
- * &lt;/xs:element>
- * </pre>
- */
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement
+@XmlType
 public class XMLReturnValue
 {
     private XMLTypedValue value;
 
-    /** 
-     * Get the 'value' element value.
-     * 
-     * @return value
-     */
+    @XmlElement(required = true)
     public XMLTypedValue getValue() {
         return value;
     }
 
-    /** 
-     * Set the 'value' element value.
-     * 
-     * @param value
-     */
     public void setValue(XMLTypedValue value) {
         this.value = value;
     }
