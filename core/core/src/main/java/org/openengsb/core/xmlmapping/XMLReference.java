9e21e8ef7d57d70b0f535489f7780075a09d3a96
Michael Zoech <michi.zoech@gmail.com>
2010-06-18 16:45:03 +0200

    annotate reference class

--- core/core/src/main/java/org/openengsb/core/xmlmapping/XMLReference.java
+++ core/core/src/main/java/org/openengsb/core/xmlmapping/XMLReference.java
@@ -1,34 +1,19 @@
 
 package org.openengsb.core.xmlmapping;
 
-/** 
- * Schema fragment(s) for this class:
- * <pre>
- * &lt;xs:complexType xmlns:xs="http://www.w3.org/2001/XMLSchema" name="XMLReference">
- *   &lt;xs:sequence>
- *     &lt;xs:element type="xs:string" name="id"/>
- *   &lt;/xs:sequence>
- * &lt;/xs:complexType>
- * </pre>
- */
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
 public class XMLReference
 {
     private String id;
 
-    /** 
-     * Get the 'id' element value.
-     * 
-     * @return value
-     */
+    @XmlElement(required = true)
     public String getId() {
         return id;
     }
 
-    /** 
-     * Set the 'id' element value.
-     * 
-     * @param id
-     */
     public void setId(String id) {
         this.id = id;
     }
