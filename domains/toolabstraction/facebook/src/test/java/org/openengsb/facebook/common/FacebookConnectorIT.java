c4c2138837e7e3f1ba9c1a68b6661e3fca796e5d
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2010-07-07 09:30:26 +0200

    new test added, for testing truncate message and more code coverage

--- domains/toolabstraction/facebook/src/test/java/org/openengsb/facebook/common/FacebookConnectorIT.java
+++ domains/toolabstraction/facebook/src/test/java/org/openengsb/facebook/common/FacebookConnectorIT.java
@@ -20,7 +20,6 @@ package org.openengsb.facebook.common;
 import com.google.code.facebookapi.FacebookException;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -59,12 +58,55 @@ public class FacebookConnectorIT {
     public void simpleUpdateUserStatus() throws FacebookException, IOException {
         facebookConnector.updateMessageNoErrorHandling("TestMessage send on: " + new Date());
     }
-    
+
     @Test
-    public void simpleUpdateUserStatus_retryToCheckIfItIsPossibleToPostASecondTime() throws FacebookException, IOException {
+    public void simpleUpdateUserStatusPostTwoMessages() throws FacebookException, IOException {
         facebookConnector.updateMessageNoErrorHandling("TestMessage 1 send on: " + new Date());
         facebookConnector.updateMessageNoErrorHandling("TestMessage 2 send on: " + new Date());
     }
 
+    @Test
+    public void updateUserMessageTruncateMessage() throws FacebookException, IOException {
+        facebookConnector.updateMessageNoErrorHandling("" +
+                "Who rides, so late, through night and wind?" +
+                "It is the father with his child." +
+                "He has the boy well in his arm," +
+                "He holds him safely, he keeps him warm." +
+
+                "\"My son, why do you hide your face so anxiously?" +
+                "\"Father, do you not see the Erl king?" +
+                "The Erl king with crown and tail?" +
+                "\"My son, it's a wisp of fog." +
+
+                "\"You lovely child, come, go with me!\n" +
+                "Many a beautiful game I'll play with you;" +
+                "Many colourful flowers are on the shore," +
+                "My mother has many golden robes." +
+
+                "\"My father, my father, and don't you hear" +
+                "What Erl king is quietly promising me?" +
+                "\"Be calm, stay calm, my child;" +
+                "The wind is rustling through withered leaves." +
+
+                "\"Do you want to come with me, dear boy?" +
+                "My daughters shall wait on you fine;" +
+                "My daughters will lead the nightly dance," +
+                "And rock and dance and sing you to sleep." +
+
+                "\"My father, my father, and don't you see there" +
+                "Erl king's daughters in the gloomy place?" +
+                "\"My son, my son, I see it clearly:" +
+                "The old willows they shimmer so grey." +
+
+                "\"I love you, your beautiful form entices me;" +
+                "And if you're not willing, I shall use force." +
+                "\"My father, my father, he's grabbing me now!" +
+                "Erl king has done me some harm!" +
+
+                "The father shudders; he swiftly rides on," +
+                "He holds the moaning child in his arms," +
+                "is hardly able to reach his farm;" +
+                "In his arms, the child was dead.");
+    }
 
 }
