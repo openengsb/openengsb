bb8b9b85793cdceb85c9dfdac352eda70104544f
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2010-07-09 12:55:45 +0200

    fix build-error

--- core/core/src/main/java/org/openengsb/core/endpoints/RPCEndpoint.java
+++ core/core/src/main/java/org/openengsb/core/endpoints/RPCEndpoint.java
@@ -13,7 +13,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-   
+
  */
 package org.openengsb.core.endpoints;
 
@@ -69,7 +69,7 @@ public abstract class RPCEndpoint<T> extends OpenEngSBEndpoint {
 
         QName operation = exchange.getOperation();
         if (operation == null || !operation.getLocalPart().equals("methodcall")) {
-            throw new javax.resource.spi.IllegalStateException(
+            throw new IllegalStateException(
                     "'methodcall' expected in opertation field of exchange, but '" + operation + "' found.");
         }
 
