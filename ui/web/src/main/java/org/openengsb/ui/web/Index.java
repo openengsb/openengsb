4ebacef241ddcc710c74fc99ec47fc3ef231fa64
Andreas Pieber <anpieber@gmail.com>
2010-07-11 21:27:38 +0200

    imported test osgi service
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- ui/web/src/main/java/org/openengsb/ui/web/Index.java
+++ ui/web/src/main/java/org/openengsb/ui/web/Index.java
@@ -1,5 +1,6 @@
 package org.openengsb.ui.web;
 
+import org.apache.servicemix.jbi.deployer.AdminCommandsService;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
@@ -11,6 +12,9 @@ public class Index extends BasePage {
 
 	// @SpringBean(name = "testDao")
 	// private TestDaoInteface dao;
+
+	@SpringBean(name = "testService")
+	private AdminCommandsService adminCmd;
 
 	public Index() {
 		add(new Label("helloworld", "Hello World"));
