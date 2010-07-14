348e15bc8cf73164a9b6c6ec16b8d328aecedea4
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2010-07-14 12:22:51 +0200

    make the persistence-endpoit a linking endpoint

--- core/persistence/service-engine/src/main/java/org/openengsb/persistence/PersistenceEndpoint.java
+++ core/persistence/service-engine/src/main/java/org/openengsb/persistence/PersistenceEndpoint.java
@@ -17,141 +17,21 @@
  */
 package org.openengsb.persistence;
 
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.jbi.messaging.MessageExchange;
-import javax.jbi.messaging.NormalizedMessage;
-
-import org.apache.servicemix.jbi.jaxp.SourceTransformer;
-import org.apache.servicemix.jbi.jaxp.StringSource;
 import org.openengsb.contextcommon.ContextHelper;
 import org.openengsb.core.MessageProperties;
-import org.openengsb.core.endpoints.DirectMessageHandlingEndpoint;
-import org.openengsb.core.model.MethodCall;
-import org.openengsb.core.model.ReturnValue;
-import org.openengsb.core.transformation.Transformer;
-import org.openengsb.core.xmlmapping.XMLEvent;
-import org.openengsb.core.xmlmapping.XMLMappable;
-import org.openengsb.core.xmlmapping.XMLMappableList;
-import org.openengsb.core.xmlmapping.XMLMethodCall;
-import org.openengsb.core.xmlmapping.XMLReturnValue;
-import org.openengsb.core.xmlmapping.XMLTypedValue;
-import org.openengsb.util.serialization.JaxbXmlSerializer;
-import org.openengsb.util.serialization.SerializationException;
-import org.openengsb.util.serialization.Serializer;
+import org.openengsb.core.endpoints.LinkingEndpoint;
 
 /**
  * @org.apache.xbean.XBean element="persistenceEndpoint"
  *                         description="Persistence Component"
  */
-public class PersistenceEndpoint extends DirectMessageHandlingEndpoint<Persistence> {
-
-    private static Serializer serializer = null;
+public class PersistenceEndpoint extends LinkingEndpoint<PersistenceInternal> {
 
     private PersistenceInternal persistence;
 
-    private static Serializer getSerializer() throws SerializationException {
-        if (serializer == null) {
-            serializer = new JaxbXmlSerializer(XMLEvent.class.getPackage().getName());
-        }
-        return serializer;
-    }
-
     @Override
-    protected void handleMethodCallManually(MessageExchange exchange, NormalizedMessage in, NormalizedMessage out,
-            ContextHelper contextHelper, MessageProperties msgProperties) throws Exception {
-        String inMessage = new SourceTransformer().toString(in.getContent());
-        String outMessage = handlePersistenceCall(inMessage);
-        out.setContent(new StringSource(outMessage));
-    }
-
-    protected String handlePersistenceCall(String inMessage) throws Exception {
-        XMLMethodCall xmlCall = getXmlMethodCall(inMessage);
-
-        List<Object> args = new ArrayList<Object>();
-        List<Class<?>> types = new ArrayList<Class<?>>();
-
-        for (XMLTypedValue arg : xmlCall.getArgs()) {
-            transformArgument(args, types, arg);
-        }
-
-        MethodCall methodCall = new MethodCall(xmlCall.getMethodName(), args.toArray(), types
-                .toArray(new Class<?>[types.size()]));
-        ReturnValue returnValue = methodCall.invoke(persistence);
-
-        return transformReturnValue(returnValue);
-    }
-
-    private void transformArgument(List<Object> args, List<Class<?>> types, XMLTypedValue arg)
-            throws SerializationException {
-        XMLMappable value = arg.getValue();
-        if (value.ifList()) {
-            List<PersistenceObject> list = transformList(value);
-            args.add(list);
-            types.add(List.class);
-        } else if (value.ifBean()) {
-            args.add(new PersistenceObject(toXml(value), arg.getType()));
-            types.add(PersistenceObject.class);
-        } else {
-            throw new IllegalStateException("Only java.util.List and beans are supported.");
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private String transformReturnValue(ReturnValue returnValue) throws SerializationException {
-        if (returnValue.getValue() == null) {
-            return Transformer.toXml(returnValue);
-        }
-        List<PersistenceObject> result = (List<PersistenceObject>) returnValue.getValue();
-        List<XMLMappable> transformed = new ArrayList<XMLMappable>(result.size());
-        for (PersistenceObject po : result) {
-            transformed.add(toMappable(po.getXml()));
-        }
-
-        XMLMappableList list = new XMLMappableList();
-        list.setMappables(transformed);
-
-        XMLMappable mappable = new XMLMappable();
-        mappable.setList(list);
-
-        XMLTypedValue typedValue = new XMLTypedValue();
-        typedValue.setType(List.class.getName());
-        typedValue.setValue(mappable);
-
-        XMLReturnValue xmlReturnValue = new XMLReturnValue();
-        xmlReturnValue.setValue(typedValue);
-
-        return toXml(xmlReturnValue);
-    }
-
-    private XMLMappable toMappable(String xml) throws SerializationException {
-        return getSerializer().deserialize(XMLMappable.class, new StringReader(xml));
-    }
-
-    private List<PersistenceObject> transformList(XMLMappable value) throws SerializationException {
-        List<PersistenceObject> resultList = new ArrayList<PersistenceObject>();
-        XMLMappableList mappableList = value.getList();
-        for (XMLMappable mappable : mappableList.getMappables()) {
-            if (!mappable.ifBean()) {
-                throw new IllegalStateException("Only beans are supported as part of the list.");
-            }
-            resultList.add(new PersistenceObject(toXml(mappable), mappable.getBean().getClassName()));
-        }
-        return resultList;
-    }
-
-    private String toXml(Object value) throws SerializationException {
-        StringWriter writer = new StringWriter();
-        getSerializer().serialize(value, writer);
-        return writer.toString();
-    }
-
-    private XMLMethodCall getXmlMethodCall(String inMessage) throws SerializationException {
-        XMLMethodCall xmc = getSerializer().deserialize(XMLMethodCall.class, new StringReader(inMessage));
-        return xmc;
+    protected PersistenceInternal getImplementation(ContextHelper arg0, MessageProperties arg1) {
+        return persistence;
     }
 
     public void setPersistence(PersistenceInternal persistence) {
