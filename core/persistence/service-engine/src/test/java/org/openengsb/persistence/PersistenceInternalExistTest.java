0be5e94baf1124b1e7063e7797cb3978374b18d7
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2010-07-14 23:19:23 +0200

    more tests for PersistenceInternal (parametrized now)

--- core/persistence/service-engine/src/test/java/org/openengsb/persistence/PersistenceInternalExistTest.java
+++ core/persistence/service-engine/src/test/java/org/openengsb/persistence/PersistenceInternalExistTest.java
@@ -1,7 +1,10 @@
 package org.openengsb.persistence;
 
-
 public class PersistenceInternalExistTest extends PersistenceInternalTest {
+
+    public PersistenceInternalExistTest(Class<?> objectClass, Object o1, Object sample1, Object udpated1) {
+        super(objectClass, o1, sample1, udpated1);
+    }
 
     @Override
     protected PersistenceInternal getPersistenceImpl() throws Exception {
--- core/persistence/service-engine/src/test/java/org/openengsb/persistence/PersistenceInternalTest.java
+++ core/persistence/service-engine/src/test/java/org/openengsb/persistence/PersistenceInternalTest.java
@@ -1,10 +1,17 @@
 package org.openengsb.persistence;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.transform.OutputKeys;
@@ -21,8 +28,12 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.w3c.dom.Node;
 
+@RunWith(Parameterized.class)
 public abstract class PersistenceInternalTest {
 
     private static Log log = LogFactory.getLog(PersistenceInternalTest.class);
@@ -30,7 +41,53 @@ public abstract class PersistenceInternalTest {
     protected PersistenceInternal persistence;
     protected static UniversalJaxbSerializer serializer;
 
-    protected SimpleTestBean simple1;
+    protected Class<?> objectClass;
+    protected Object o1;
+    protected Object sample1;
+    protected Object updated1;
+
+    public PersistenceInternalTest(Class<?> objectClass, Object o1, Object sample1, Object udpated1) {
+        this.objectClass = objectClass;
+        this.o1 = o1;
+        this.sample1 = sample1;
+        this.updated1 = udpated1;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        Collection<Object[]> result = new LinkedList<Object[]>();
+        result.add(new Object[] { SimpleTestBean.class, new SimpleTestBean(42, "bla"), new SimpleTestBean(42, null),
+                new SimpleTestBean(42, "blah"), });
+
+        ComplexTestBean o1 = new ComplexTestBean();
+        o1.simple = new SimpleTestBean(42, "42");
+        ComplexTestBean sample1 = new ComplexTestBean();
+        sample1.simple = new SimpleTestBean(42, "42");
+        ComplexTestBean updated1 = new ComplexTestBean();
+        updated1.simple = new SimpleTestBean(42, "21");
+
+        result.add(new Object[] { ComplexTestBean.class, o1, sample1, updated1, });
+
+        ComplexTestBean o2 = new ComplexTestBean();
+        o2.simple = new SimpleTestBean(42, "42");
+        List<String> testList = new ArrayList<String>();
+        testList.add("item1");
+        testList.add("item2");
+        o2.testList = testList;
+
+        ComplexTestBean sample2 = new ComplexTestBean();
+        sample2.simple = new SimpleTestBean(42, "42");
+        ComplexTestBean updated2 = new ComplexTestBean();
+        updated2.simple = new SimpleTestBean(42, "42");
+        Map<Long, String> testMap = new HashMap<Long, String>();
+        testMap.put(1L, "value1");
+        testMap.put(2L, "value");
+        updated2.testMap = testMap;
+
+        result.add(new Object[] { ComplexTestBean.class, o2, sample2, updated2, });
+
+        return result;
+    }
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
@@ -45,8 +102,6 @@ public abstract class PersistenceInternalTest {
     @Before
     public void setUp() throws Exception {
         persistence = getPersistenceImpl();
-        simple1 = new SimpleTestBean(42, "bleh");
-        simple1.moreContent = "moaaar";
     }
 
     @After
@@ -57,27 +112,58 @@ public abstract class PersistenceInternalTest {
 
     @Test
     public void testCreateAndQueryAll() throws Exception {
-        PersistenceObject s1 = makePersistenceObject(simple1);
+        PersistenceObject s1 = makePersistenceObject(o1);
         persistence.create(s1);
-        SimpleTestBean example = new SimpleTestBean();
-        PersistenceObject sample = makePersistenceObject(example);
-        List<PersistenceObject> resultList = persistence.query(sample);
-        assertNotNull(resultList);
-        assertEquals(1, resultList.size());
+        Object example = objectClass.newInstance();
+        Collection<Object> resultObjects = doQuery(example);
+        assertTrue(resultObjects.contains(o1));
     }
 
     @Test
     public void testCreateAndQuery() throws Exception {
-        PersistenceObject s1 = makePersistenceObject(simple1);
+        PersistenceObject s1 = makePersistenceObject(o1);
         persistence.create(s1);
-        SimpleTestBean example = new SimpleTestBean(42, null);
+        Object example = sample1;
+        Collection<Object> resultObjects = doQuery(example);
+        assertTrue(resultObjects.contains(o1));
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        PersistenceObject s1 = makePersistenceObject(o1);
+        persistence.create(s1);
+        PersistenceObject s1u = makePersistenceObject(updated1);
+        persistence.update(s1, s1u);
+        Collection<Object> result = doQuery(updated1);
+        assertTrue(result.contains(updated1));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        PersistenceObject s1 = makePersistenceObject(o1);
+        persistence.create(s1);
+        persistence.delete(s1);
+        Collection<Object> result = doQuery(objectClass.newInstance());
+        assertFalse(result.contains(o1));
+    }
+
+    protected Collection<Object> doQuery(Object example) throws JAXBException, PersistenceException,
+            ClassNotFoundException {
         PersistenceObject sample = makePersistenceObject(example);
         List<PersistenceObject> resultList = persistence.query(sample);
         assertNotNull(resultList);
-        assertEquals(1, resultList.size());
-        PersistenceObject resultPo = resultList.get(0);
-        SimpleTestBean result = (SimpleTestBean) parsePersistenceObject(resultPo);
-        assertEquals(simple1, result);
+        Collection<Object> resultObjects = convertResult(resultList);
+        return resultObjects;
+    }
+
+    protected Collection<Object> convertResult(Collection<PersistenceObject> resultList) throws JAXBException,
+            ClassNotFoundException {
+        Collection<Object> result = new HashSet<Object>();
+        for (PersistenceObject po : resultList) {
+            Object o = parsePersistenceObject(po);
+            result.add(o);
+        }
+        return result;
     }
 
     protected PersistenceObject makePersistenceObject(Object o) throws JAXBException {
