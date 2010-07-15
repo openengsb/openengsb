aca3232e8cdea57520009e20e25ec8ecfc00bd00
Andreas Pieber <anpieber@gmail.com>
2010-07-15 13:09:14 +0200

    corrected current ui implementation to run in pax
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- ui/web/src/main/java/org/openengsb/ui/web/Index.java
+++ ui/web/src/main/java/org/openengsb/ui/web/Index.java
@@ -1,6 +1,5 @@
 package org.openengsb.ui.web;
 
-import org.apache.servicemix.jbi.deployer.AdminCommandsService;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
@@ -9,12 +8,6 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 public class Index extends BasePage {
-
-	// @SpringBean(name = "testDao")
-	// private TestDaoInteface dao;
-
-	@SpringBean(name = "testService")
-	private AdminCommandsService adminCmd;
 
 	public Index() {
 		add(new Label("helloworld", "Hello World"));
