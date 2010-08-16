689020c5dee36067f8a3894bcfd555da37d0fb83
Florian Motlik <flomotlik@gmail.com>
2010-08-16 13:05:17 +0200

    Merging Domain/Managed Services into single DomainService
    Signed-off-by: Florian Motlik <flomotlik@gmail.com>

--- ui/web/src/main/java/org/openengsb/ui/web/service/impl/OsgiDomainService.java
+++ ui/web/src/main/java/org/openengsb/ui/web/service/impl/OsgiDomainService.java
@@ -17,21 +17,72 @@
  */
 package org.openengsb.ui.web.service.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openengsb.core.config.Domain;
 import org.openengsb.core.config.DomainProvider;
+import org.openengsb.core.config.ServiceManager;
 import org.openengsb.ui.web.service.DomainService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 
 public class OsgiDomainService implements DomainService {
 
     private List<DomainProvider> domains;
+    private final BundleContext bundleContext;
 
-    @Override
-    public List<DomainProvider> getDomains() {
-        return domains;
+    Log log = LogFactory.getLog(OsgiDomainService.class);
+
+    public OsgiDomainService(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
     }
 
     public void setDomains(List<DomainProvider> domains) {
-        this.domains = domains;
+        this.domains = new ArrayList<DomainProvider>(domains);
+    }
+
+    @Override
+    public List<DomainProvider> domains() {
+        return new ArrayList<DomainProvider>(this.domains);
+    }
+
+    @Override
+    public List<ServiceManager> serviceManagersForDomain(Class<? extends Domain> domain) {
+        List<ServiceManager> serviceManagers = new ArrayList<ServiceManager>();
+        try {
+            ServiceReference[] allServiceReferences = bundleContext.getAllServiceReferences(
+                    ServiceManager.class.getName(), "(domain=" + domain.getName() + ")");
+
+            if (allServiceReferences != null) {
+                for (ServiceReference serviceReference : allServiceReferences) {
+                    Object service = bundleContext.getService(serviceReference);
+                    if (service instanceof ServiceManager) {
+                        serviceManagers.add((ServiceManager) service);
+                    }
+                }
+            }
+        } catch (InvalidSyntaxException e) {
+            log.debug(e.getMessage());
+        }
+        return serviceManagers;
+    }
+
+    @Override
+    public List<ServiceReference> serviceReferencesForConnector(Class<? extends Domain> connectorClass) {
+        List<ServiceReference> serviceReferences = new ArrayList<ServiceReference>();
+        try {
+            ServiceReference[] allServiceReferences = bundleContext.getAllServiceReferences(connectorClass.getName(), null);
+            if(allServiceReferences != null){
+            serviceReferences = Arrays.asList(allServiceReferences);
+            }
+        } catch (InvalidSyntaxException e) {
+            log.debug(e.getMessage());
+        }
+        return serviceReferences;
     }
 }
--- ui/web/src/test/java/org/openengsb/ui/web/mock/BundleContextMock.java
+++ ui/web/src/test/java/org/openengsb/ui/web/mock/BundleContextMock.java
@@ -1,6 +1,9 @@
 package org.openengsb.ui.web.mock;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,11 +12,18 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openengsb.core.config.ServiceManager;
 import org.openengsb.core.config.descriptor.ServiceDescriptor;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.BundleListener;
+import org.osgi.framework.Filter;
+import org.osgi.framework.FrameworkListener;
 import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceReference;
-import org.springframework.osgi.mock.MockBundleContext;
+import org.osgi.framework.ServiceRegistration;
 
-public class BundleContextMock extends MockBundleContext {
+public class BundleContextMock implements BundleContext {
 
     private final Log log = LogFactory.getLog(BundleContextMock.class);
 
@@ -39,7 +49,6 @@ public class BundleContextMock extends MockBundleContext {
         return list;
     }
 
-    @Override
     public ServiceReference[] getAllServiceReferences(String clazz, String filter) throws InvalidSyntaxException {
         log.info("Getting Service References for: " + clazz);
         if (filter != null) {
@@ -47,5 +56,112 @@ public class BundleContextMock extends MockBundleContext {
         }
         List<ServiceReference> list = references.get(clazz);
         return list.toArray(new ServiceReference[list.size()]);
+    }
+
+    @Override
+    public String getProperty(String key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle getBundle() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle installBundle(String location, InputStream input) throws BundleException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle installBundle(String location) throws BundleException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle getBundle(long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle[] getBundles() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addServiceListener(ServiceListener listener, String filter) throws InvalidSyntaxException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addServiceListener(ServiceListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeServiceListener(ServiceListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addBundleListener(BundleListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeBundleListener(BundleListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addFrameworkListener(FrameworkListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeFrameworkListener(FrameworkListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ServiceRegistration registerService(String[] clazzes, Object service, Dictionary properties) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ServiceRegistration registerService(String clazz, Object service, Dictionary properties) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ServiceReference[] getServiceReferences(String clazz, String filter) throws InvalidSyntaxException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ServiceReference getServiceReference(String clazz) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public Object getService(ServiceReference reference) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public boolean ungetService(ServiceReference reference) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getDataFile(String filename) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Filter createFilter(String filter) throws InvalidSyntaxException {
+        throw new UnsupportedOperationException();
     }
 }
