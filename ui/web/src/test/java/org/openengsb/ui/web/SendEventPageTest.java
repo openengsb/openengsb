30346d9ab82a2ab93759edc459c8e71399ee1765
Tobias Hochwallner <tobster@gmx.at>
2010-08-19 12:19:08 +0200

    created SendEventPage with drop down choice for classes

---
+++ ui/web/src/test/java/org/openengsb/ui/web/SendEventPageTest.java
@@ -0,0 +1,55 @@
+/**
+
+Copyright 2010 OpenEngSB Division, Vienna University of Technology
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ */
+package org.openengsb.ui.web;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SendEventPageTest {
+
+    private WicketTester tester;
+
+    @Before
+    public void setup() {
+        tester = new WicketTester();
+    }
+
+    private class Dummy {
+    }
+
+    private class Dummy2 {
+    }
+
+    @Test
+    public void mytest() {
+        List<Class> classes = Arrays.<Class>asList(Dummy.class, Dummy2.class);
+        tester.startPage(new SendEventPage(classes));
+        tester.dumpPage();
+        DropDownChoice dropdown = (DropDownChoice) tester.getComponentFromLastRenderedPage("dropdown");
+        assertNotNull(dropdown);
+        tester.assertComponent("dropdown",DropDownChoice.class);
+        assertEquals(2, dropdown.getChoices().size());
+        assertEquals("Dummy", dropdown.getChoices().get(0));
+        assertEquals("Dummy2", dropdown.getChoices().get(1));
+    }
+}
