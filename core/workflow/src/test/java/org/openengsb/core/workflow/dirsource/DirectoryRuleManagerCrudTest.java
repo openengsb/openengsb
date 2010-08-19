2f37e95a7a002065bd23b678a5c108a6d7f5dd4b
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2010-08-19 10:36:56 +0200

    add rule-management-tests form servicemix-endpoint

---
+++ core/workflow/src/test/java/org/openengsb/core/workflow/dirsource/DirectoryRuleManagerCrudTest.java
@@ -0,0 +1,55 @@
+/**
+
+   Copyright 2010 OpenEngSB Division, Vienna University of Technology
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+package org.openengsb.core.workflow.dirsource;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openengsb.core.workflow.RuleManager;
+import org.openengsb.core.workflow.AbstractRuleManagerCrudTest;
+import org.openengsb.core.workflow.model.RuleBaseElementId;
+import org.openengsb.core.workflow.source.dir.DirectoryRuleSource;
+
+public class DirectoryRuleManagerCrudTest extends AbstractRuleManagerCrudTest<DirectoryRuleSource> {
+
+    public DirectoryRuleManagerCrudTest(RuleBaseElementId id1, String code1, RuleBaseElementId id2, String code2) {
+        super(id1, code1, id2, code2);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        File rulebaseReferenceDirectory = new File("src/main/resources/rulebase");
+        File rulebaseTestDirectory = new File("data/rulebase");
+        FileUtils.copyDirectory(rulebaseReferenceDirectory, rulebaseTestDirectory);
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+        File ruleDir = new File("data");
+        while (ruleDir.exists()) {
+            FileUtils.deleteQuietly(ruleDir);
+        }
+    }
+
+    @Override
+    protected RuleManager getRuleBaseSource() {
+        return new DirectoryRuleSource("data/rulebase");
+    }
+}
