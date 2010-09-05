4a43d2eac6568e1b173881aa10a70f4d43d40a92
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2010-09-05 16:01:34 +0200

    navigation test added

--- ui/web/src/test/java/org/openengsb/ui/web/global/header/HeaderTemplateTest.java
+++ ui/web/src/test/java/org/openengsb/ui/web/global/header/HeaderTemplateTest.java
@@ -1,67 +1,95 @@
 package org.openengsb.ui.web.global.header;
+/**
 
-import static junit.framework.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
+ Copyright 2010 OpenEngSB Division, Vienna University of Technology
 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
+
+import junit.framework.Assert;
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.apache.wicket.spring.injection.annot.test.AnnotApplicationContextMock;
 import org.apache.wicket.spring.test.ApplicationContextMock;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.openengsb.core.common.Event;
 import org.openengsb.core.common.context.ContextCurrentService;
+import org.openengsb.core.workflow.WorkflowService;
+import org.openengsb.ui.web.ContextSetPage;
 import org.openengsb.ui.web.Index;
+import org.openengsb.ui.web.SendEventPage;
 import org.openengsb.ui.web.TestClient;
+import org.openengsb.ui.web.editor.EditorPanel;
+import org.openengsb.ui.web.global.BookmarkablePageLabelLink;
 import org.openengsb.ui.web.global.footer.ImprintPage;
 import org.openengsb.ui.web.service.DomainService;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+
 public class HeaderTemplateTest {
-    public interface TestInterface {
 
-        void update(String id, String name);
-    }
-
-    public class TestService implements TestInterface {
-
-        public boolean called = false;
-
-        @Override
-        public void update(String id, String name) {
-            called = true;
-        }
-
-        public String getName(String id) {
-            return "";
-        }
-
-    }
 
     private WicketTester tester;
     private ApplicationContextMock context;
+
 
     @Before
     public void setup() {
         tester = new WicketTester();
         context = new ApplicationContextMock();
-        context.putBean(mock(ContextCurrentService.class));
+        context.putBean(Mockito.mock(ContextCurrentService.class));
     }
 
     @Test
     public void testNavigationFieldForIndex() {
         setupIndexPage();
-        assertTrue(testNavigation(Index.class, Index.class.getSimpleName()));
+        Assert.assertTrue(testNavigation(Index.class, Index.class.getSimpleName()));
+        Assert.assertEquals(Index.class, tester.getLastRenderedPage().getClass());
     }
 
     @Test
     public void testNavigationFieldForTestClient() {
         setupTestClientPage();
-        assertTrue(testNavigation(TestClient.class, TestClient.class.getSimpleName()));
+        Assert.assertTrue(testNavigation(TestClient.class, TestClient.class.getSimpleName()));
+        Assert.assertEquals(TestClient.class, tester.getLastRenderedPage().getClass());
     }
 
     @Test
     public void testNavigationForNonExistingNavigationButton() {
-        assertTrue(testNavigation(ImprintPage.class, Index.class.getSimpleName()));
+        Assert.assertTrue(testNavigation(ImprintPage.class, Index.class.getSimpleName()));
+        Assert.assertEquals(ImprintPage.class, tester.getLastRenderedPage().getClass());
+    }
+
+    @Test
+    public void testToNavigate() {
+        setUpSendEventPage();
+        Assert.assertEquals(Index.class, tester.getLastRenderedPage().getClass());
+        tester.clickLink("header:headerMenuItems:0:link");
+        tester.assertRenderedPage(Index.class);
+        tester.clickLink("header:headerMenuItems:1:link");
+        tester.assertRenderedPage(TestClient.class);
+        tester.clickLink("header:headerMenuItems:2:link");
+        tester.assertRenderedPage(SendEventPage.class);
     }
 
     private boolean testNavigation(Class<? extends WebPage> page, String expectedIndexName) {
@@ -79,10 +107,39 @@ public class HeaderTemplateTest {
         DomainService domainServiceMock = Mockito.mock(DomainService.class);
         context.putBean(domainServiceMock);
         setupTesterWithSpringMockContext();
+        tester.startPage(Index.class);
     }
 
     private void setupTesterWithSpringMockContext() {
         tester.getApplication().addComponentInstantiationListener(
                 new SpringComponentInjector(tester.getApplication(), context, true));
     }
+
+    private void setUpSendEventPage() {
+        tester = new WicketTester();
+        AnnotApplicationContextMock context = new AnnotApplicationContextMock();
+        tester.getApplication().addComponentInstantiationListener(
+                new SpringComponentInjector(tester.getApplication(), context, false));
+        WorkflowService eventService = mock(WorkflowService.class);
+        context.putBean("eventService", eventService);
+        context.putBean("domainService", mock(DomainService.class));
+        context.putBean("contextCurrentService", mock(ContextCurrentService.class));
+        List<Class<? extends Event>> eventClasses = Arrays.<Class<? extends Event>>asList(Dummy.class);
+        tester.startPage(new SendEventPage(eventClasses));
+        tester.startPage(Index.class);
+
+    }
+    static class Dummy extends Event {
+
+           private String testProperty;
+
+           public String getTestProperty() {
+               return testProperty;
+           }
+
+           public void setTestProperty(String testProperty) {
+               this.testProperty = testProperty;
+           }
+       }
+    
 }
