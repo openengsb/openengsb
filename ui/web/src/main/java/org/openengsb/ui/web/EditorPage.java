e34247bd41b5eda401068eef75a1984797f01d72
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2010-09-13 20:36:22 +0200

    new constructor added, that editing is possible

--- ui/web/src/main/java/org/openengsb/ui/web/EditorPage.java
+++ ui/web/src/main/java/org/openengsb/ui/web/EditorPage.java
@@ -1,18 +1,18 @@
 /**
 
-   Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ Copyright 2010 OpenEngSB Division, Vienna University of Technology
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
  */
 package org.openengsb.ui.web;
@@ -20,6 +20,7 @@ package org.openengsb.ui.web;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
@@ -40,15 +41,28 @@ public class EditorPage extends BasePage {
         add(new Label("service.name", descriptor.getName()));
         add(new Label("service.description", descriptor.getDescription()));
         this.add(new BookmarkablePageLink<Index>("index", Index.class));
-        createEditor();
+        createEditor( new HashMap<String, String>());
     }
 
+    public EditorPage(ServiceManager serviceManager, String serviceId) {
+        this.serviceManager = serviceManager;
+        Map<String, String> attributeValues = serviceManager.getAttributeValues(serviceId);
+
+        ServiceDescriptor descriptor = serviceManager.getDescriptor(getSession().getLocale());
+        add(new Label("service.name", descriptor.getName()));
+        add(new Label("service.description", descriptor.getDescription()));
+        this.add(new BookmarkablePageLink<Index>("index", Index.class));
+        createEditor(attributeValues);
+    }
+
+
     @SuppressWarnings("serial")
-    private void createEditor() {
+    private void createEditor(Map<String, String> values) {
         List<AttributeDefinition> attributes = buildAttributeList(serviceManager);
-        HashMap<String, String> values = new HashMap<String, String>();
         for (AttributeDefinition attribute : attributes) {
-            values.put(attribute.getId(), attribute.getDefaultValue());
+            if(!values.containsKey(attribute.getId())) { // do not overwrite attributes with default value
+                values.put(attribute.getId(), attribute.getDefaultValue());
+            }
         }
         editorPanel = new EditorPanel("editor", attributes, values) {
             @Override
