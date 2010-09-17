ed3c9ece713d591fe569b61141547a9bc38d2f93
Tobias Hochwallner <tobster@gmx.at>
2010-09-17 20:07:42 +0200

    moved getCurrentContextId from contextcurrentservice to contextservice interface

--- core/common/src/main/java/org/openengsb/core/common/context/ContextCurrentService.java
+++ core/common/src/main/java/org/openengsb/core/common/context/ContextCurrentService.java
@@ -45,12 +45,6 @@ public interface ContextCurrentService extends ContextService {
     void createContext(String contextId);
 
     /**
-     * 
-     * @return the id of the current context
-     */
-    String getCurrentContextId();
-
-    /**
      * Get all root context nodes in a natural order. These can be set using the
      * {@link #setThreadLocalContext(String)} method.
      */
