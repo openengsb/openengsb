0c78009b1d05728f860071ee9fc412ce0fcc8b9b
Florian Motlik <flomotlik@gmail.com>
2010-09-20 19:36:12 +0200

    Adding JMSConnector which adds Proxy for every Domain at runtime
    Signed-off-by: Florian Motlik <flomotlik@gmail.com>

---
+++ domains/jms/src/main/java/org/openengsb/domains/jms/JMSConnector.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.openengsb.domains.jms;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+import org.openengsb.core.common.Domain;
+import org.openengsb.core.common.DomainProvider;
+import org.openengsb.core.common.service.DomainService;
+import org.osgi.framework.BundleContext;
+
+public class JMSConnector {
+
+    public JMSConnector(BundleContext context, DomainService domainService, InvocationHandler handler) {
+        for (DomainProvider domain : domainService.domains()) {
+            Class<? extends Domain> domainInterface = domain.getDomainInterface();
+            Object newProxyInstance =
+                Proxy.newProxyInstance(domainInterface.getClassLoader(), new Class[]{domainInterface}, handler);
+            context.registerService(domainInterface.getName(), newProxyInstance, null);
+        }
+    }
+}
---
+++ domains/jms/src/test/java/org/openengsb/domains/jms/JMSConnectorTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.openengsb.domains.jms;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.same;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.Dictionary;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.openengsb.core.common.Domain;
+import org.openengsb.core.common.DomainProvider;
+import org.openengsb.core.common.service.DomainService;
+import org.osgi.framework.BundleContext;
+
+public class JMSConnectorTest {
+
+    @Test
+    public void returnMockDomainInterface_shouldAddProxyToBundleContext() throws Throwable {
+        BundleContext mockContext = mock(BundleContext.class);
+        DomainService domainService = mock(DomainService.class);
+        DomainProvider provider = Mockito.mock(DomainProvider.class);
+        when(provider.getDomainInterface()).thenAnswer(new Answer<Class<? extends Domain>>() {
+            @Override
+            public Class<? extends Domain> answer(InvocationOnMock invocation) throws Throwable {
+                return TestInterface.class;
+            }
+        });
+        when(domainService.domains()).thenReturn(Arrays.asList((new DomainProvider[]{provider})));
+        InvocationHandler invocationHandlerMock = mock(InvocationHandler.class);
+        new JMSConnector(mockContext, domainService, invocationHandlerMock);
+        ArgumentCaptor<Object> captor = ArgumentCaptor.forClass(Object.class);
+        verify(mockContext).registerService(eq(TestInterface.class.getName()), captor.capture(), any(Dictionary.class));
+        assertTrue(captor.getValue() instanceof Proxy);
+        assertTrue(captor.getValue() instanceof TestInterface);
+        ((TestInterface) captor.getValue()).log(5);
+        ArgumentCaptor<Method> methodCaptor = ArgumentCaptor.forClass(Method.class);
+        verify(invocationHandlerMock).invoke(same(captor.getValue()), methodCaptor.capture(),
+            eq(new Object[]{new Integer(5)}));
+        Method value = methodCaptor.getValue();
+        assertEquals("log", value.getName());
+        assertEquals(TestInterface.class, value.getDeclaringClass());
+    }
+
+    private static interface TestInterface extends Domain {
+        public void log(int i);
+    }
+}
