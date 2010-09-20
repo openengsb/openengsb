55f78cfe6f7e226320fc3da06220d5e9fb027562
Michael Handler <michael.handler@fullstop.at>
2010-09-20 19:56:40 +0200

    Add implementation class for PersistenceService.
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

---
+++ core/persistence/src/main/java/org/openengsb/core/persistence/internal/PersistenceServiceImpl.java
@@ -0,0 +1,59 @@
+package org.openengsb.core.persistence.internal;
+
+import java.util.List;
+import java.util.Map;
+
+import org.openengsb.core.persistence.PersistenceService;
+import org.openengsb.core.persistence.PersistenceException;
+
+public class PersistenceServiceImpl implements PersistenceService {
+
+    @Override
+    public void create(Object bean) throws PersistenceException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void create(List<Object> beans) throws PersistenceException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void delete(Object example) throws PersistenceException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void delete(List<Object> examples) throws PersistenceException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<Object> query(Object example) throws PersistenceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Object> query(List<Object> examples) throws PersistenceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void update(Object oldBean, Object newBean) throws PersistenceException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void update(Map<Object, Object> beans) throws PersistenceException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
