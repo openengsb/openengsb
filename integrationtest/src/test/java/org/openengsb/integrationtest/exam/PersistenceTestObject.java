2189db72784b9562743d187dc64533c613304d53
Andreas Pieber <anpieber@gmail.com>
2010-09-29 04:14:11 +0200

    corrected checkstyle violations
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- integrationtest/src/test/java/org/openengsb/integrationtest/exam/PersistenceTestObject.java
+++ integrationtest/src/test/java/org/openengsb/integrationtest/exam/PersistenceTestObject.java
@@ -49,12 +49,15 @@ public class PersistenceTestObject {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         PersistenceTestObject other = (PersistenceTestObject) obj;
         return safeEquals(this.string, other.string) && safeEquals(this.integer, other.integer);
     }
