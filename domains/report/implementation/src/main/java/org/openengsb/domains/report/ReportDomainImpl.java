30d883422449661e6a4f82888e50d0221a6d9207
Michael Handler <michael.handler@fullstop.at>
2010-10-05 11:01:22 +0200

    Start porting report domain to new openengsb infrastructure.
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

---
+++ domains/report/implementation/src/main/java/org/openengsb/domains/report/ReportDomainImpl.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.domains.report;
+
+import java.util.List;
+
+import org.openengsb.core.common.Event;
+import org.openengsb.core.common.util.AliveState;
+
+public class ReportDomainImpl implements ReportDomain {
+
+    @Override
+    public AliveState getAliveState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Report generateReport(String reportId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Report generateReport(List<Event> events) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String collectData(String idType, String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
