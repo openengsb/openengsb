753d480a7aaf00affd322c48ba5569758df095fa
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2010-11-03 11:40:54 +0100

    extract factory for creating editorFields

---
+++ ui/web/src/main/java/org/openengsb/ui/web/editor/EditorFieldFactory.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openengsb.ui.web.editor;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.validator.AbstractValidator;
+import org.openengsb.core.common.descriptor.AttributeDefinition;
+import org.openengsb.core.common.validation.FieldValidator;
+import org.openengsb.core.common.validation.SingleAttributeValidationResult;
+import org.openengsb.ui.web.editor.fields.AbstractField;
+import org.openengsb.ui.web.editor.fields.CheckboxField;
+import org.openengsb.ui.web.editor.fields.DropdownField;
+import org.openengsb.ui.web.editor.fields.InputField;
+import org.openengsb.ui.web.editor.fields.PasswordField;
+
+public final class EditorFieldFactory {
+    private EditorFieldFactory() {
+    }
+
+    public static AbstractField<?> createEditorField(String id, IModel<String> model,
+            final AttributeDefinition attribute) {
+        if (attribute.isBoolean()) {
+            return new CheckboxField(id, model, attribute, new BooleanFieldValidator(attribute));
+        }
+        StringFieldValidator validator = new StringFieldValidator(attribute);
+        if (!attribute.getOptions().isEmpty()) {
+            return new DropdownField(id, model, attribute, validator);
+        } else if (attribute.isPassword()) {
+            return new PasswordField(id, model, attribute, validator);
+        } else {
+            return new InputField(id, model, attribute, validator);
+        }
+    }
+
+    @SuppressWarnings("serial")
+    private abstract static class EditorFieldValidator<T> extends AbstractValidator<T> {
+        private final AttributeDefinition attribute;
+
+        protected EditorFieldValidator(AttributeDefinition attribute) {
+            this.attribute = attribute;
+        }
+
+        @Override
+        protected void onValidate(IValidatable<T> validatable) {
+            FieldValidator validator = this.attribute.getValidator();
+            SingleAttributeValidationResult validationResult = validator.validate(validatable.getValue().toString());
+            if (!validationResult.isValid()) {
+                error(validatable, validationResult.getErrorMessageId());
+            }
+        }
+    }
+
+    @SuppressWarnings("serial")
+    private static final class BooleanFieldValidator extends EditorFieldValidator<Boolean> {
+        private BooleanFieldValidator(AttributeDefinition attribute) {
+            super(attribute);
+        }
+    }
+
+    @SuppressWarnings("serial")
+    private static final class StringFieldValidator extends EditorFieldValidator<String> {
+        private StringFieldValidator(AttributeDefinition attribute) {
+            super(attribute);
+        }
+    }
+}
--- ui/web/src/main/java/org/openengsb/ui/web/editor/EditorPanel.java
+++ ui/web/src/main/java/org/openengsb/ui/web/editor/EditorPanel.java
@@ -36,20 +36,12 @@ import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.IValidationError;
 import org.apache.wicket.validation.ValidationError;
-import org.apache.wicket.validation.validator.AbstractValidator;
 import org.openengsb.core.common.descriptor.AttributeDefinition;
-import org.openengsb.core.common.validation.FieldValidator;
 import org.openengsb.core.common.validation.FormValidator;
 import org.openengsb.core.common.validation.MultipleAttributeValidationResult;
-import org.openengsb.core.common.validation.SingleAttributeValidationResult;
 import org.openengsb.ui.web.editor.fields.AbstractField;
-import org.openengsb.ui.web.editor.fields.CheckboxField;
-import org.openengsb.ui.web.editor.fields.DropdownField;
-import org.openengsb.ui.web.editor.fields.InputField;
-import org.openengsb.ui.web.editor.fields.PasswordField;
 import org.openengsb.ui.web.model.MapModel;
 import org.openengsb.ui.web.validation.DefaultPassingFormValidator;
 
@@ -160,17 +152,7 @@ public abstract class EditorPanel extends Panel {
     }
 
     private AbstractField<?> createEditor(String id, IModel<String> model, final AttributeDefinition attribute) {
-        if (attribute.isBoolean()) {
-            return new CheckboxField(id, model, attribute, new BooleanFieldValidator(attribute));
-        }
-        StringFieldValidator validator = new StringFieldValidator(attribute);
-        if (!attribute.getOptions().isEmpty()) {
-            return new DropdownField(id, model, attribute, validator);
-        } else if (attribute.isPassword()) {
-            return new PasswordField(id, model, attribute, validator);
-        } else {
-            return new InputField(id, model, attribute, validator);
-        }
+        return EditorFieldFactory.createEditorField(id, model, attribute);
     }
 
     public abstract void onSubmit();
@@ -181,35 +163,6 @@ public abstract class EditorPanel extends Panel {
 
     public Map<String, String> getValues() {
         return values;
-    }
-
-    private abstract static class EditorFieldValidator<T> extends AbstractValidator<T> {
-        private final AttributeDefinition attribute;
-
-        protected EditorFieldValidator(AttributeDefinition attribute) {
-            this.attribute = attribute;
-        }
-
-        @Override
-        protected void onValidate(IValidatable<T> validatable) {
-            FieldValidator validator = this.attribute.getValidator();
-            SingleAttributeValidationResult validationResult = validator.validate(validatable.getValue().toString());
-            if (!validationResult.isValid()) {
-                error(validatable, validationResult.getErrorMessageId());
-            }
-        }
-    }
-
-    private static final class BooleanFieldValidator extends EditorFieldValidator<Boolean> {
-        private BooleanFieldValidator(AttributeDefinition attribute) {
-            super(attribute);
-        }
-    }
-
-    private static final class StringFieldValidator extends EditorFieldValidator<String> {
-        private StringFieldValidator(AttributeDefinition attribute) {
-            super(attribute);
-        }
     }
 
     public String getAttributeViewId(String attribute) {
