123f93ca9accf07144f2845620b0ccdcd3f4625d
Andreas Pieber <anpieber@gmail.com>
2010-11-10 18:41:05 +0100

    added support goal for not publishing on hp
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

---
+++ etc/scripts/release-support.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright 2010 OpenEngSB Division, Vienna University of Technology
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ -z "$1" ]; then
+  echo "The location of the repository to checkout have to be added."
+  exit 0
+fi
+
+cd $(dirname $0)/../../
+mvn release:prepare -Psupport -Dmaven.test.skip=true 
+mvn release:perform -Psupport -Dmaven.test.skip=true -DconnectionUrl=scm:git:file://$1
+
