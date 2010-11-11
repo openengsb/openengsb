79040d546481d1488145daa1e26ce130da94a00a
Petr Horinek <horinek.petr@gmail.com>
2010-11-11 14:44:38 +0100

    extended scmdomaindefinition

--- domain/scm/src/main/java/org/openengsb/domain/scm/ScmDomain.java
+++ domain/scm/src/main/java/org/openengsb/domain/scm/ScmDomain.java
@@ -24,6 +24,7 @@ public interface ScmDomain extends Domain {
 
     /**
      * Polls the represented repository for updates. Returns true if there have been changes since the last poll.
+     * @deprecated
      */
     boolean poll();
 
@@ -32,6 +33,80 @@ public interface ScmDomain extends Domain {
      *
      * @param directory if the directory is non-existent, it'll be created. if the directory already exists it must not
      *        contain any files.
+     *        
+     * @deprecated
      */
     void export(File directory);
+    
+	/**
+	 * Checks if item specified by relative path exists in repository
+	 * 
+	 * @param path repository item's relative path   
+	 * @return true if item exists in repository, otherwise false
+	 */
+	 boolean exists(String path);
+    
+    
+	/**
+	 * Checks if item specified by combination of relative path 
+	 * and commit-ref exists in repository
+	 * 
+	 * @param path repository item's relative path  
+	 * @param id commit reference, see {@link CommitRef}
+	 * @return true if item exists in repository, otherwise false
+	 */
+	 boolean exists(String path, CommitRef id);
+
+	/**
+	 * Adds new file to repository
+	 * 
+	 * @param file file to add
+	 */
+	 void addFile(File file);
+
+	/**
+	 * Adds new directory to repository
+	 * 
+	 * @param dir directory to add
+	 * @param recursive add directory and all its children to full recursion
+	 */
+	 void addDirectory(File dir, boolean recursive);
+
+	/**
+	 * Commit single file to repository
+	 * 
+	 * @param file file to commit
+	 * @param comment user comment
+	 * @return commit reference, see {@link CommitRef} 
+	 */
+	 CommitRef commitFile(File file, String comment);
+		
+	/**
+	 * Commit directory to repository
+	 * 
+	 * @param dir directory to commit
+	 * @param comment user comment
+	 * @param recursive commit directory and all its children to full recursion
+	 * @return commit reference, see {@link CommitRef}
+	 */
+	 CommitRef commitDirectory(File dir, String comment, boolean recursive);
+	
+	/**
+	 * Checkout single file from repository
+	 * 
+	 * @param path relative path to repository item  
+	 * @param id file commit reference, see {@link CommitRef}
+	 * @param dir local directory, the working copy 
+	 */
+	 void checkoutFile(String path, CommitRef id, File dir);
+	
+	/**
+	 * Checkout directory from repository
+	 * 
+	 * @param path relative path to repository item  
+	 * @param id directory commit reference, see {@link CommitRef}
+	 * @param depth checkout depth, see {@link CheckoutDepth}
+	 * @param dir local directory, the working copy directory
+	 */
+	 void checkoutDirectory(String path, CommitRef id, CheckoutDepth depth, File dir);
 }
