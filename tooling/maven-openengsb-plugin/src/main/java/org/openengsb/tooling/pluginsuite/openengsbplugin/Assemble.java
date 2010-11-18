69ea1ca9faf4e2aa4c923fed4d2d4e66e2d3936b
Eduard Szente <eduard.szente@gmail.com>
2010-11-18 16:17:00 +0100

    assemble and licensecheck mojo

---
+++ tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/Assemble.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.tooling.pluginsuite.openengsbplugin;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * @goal assemble
+ * 
+ * @requiresProject true
+ */
+public class Assemble extends AbstractOpenengsbMojo {
+
+    public void execute() throws MojoExecutionException {
+
+        if (!getProject().isExecutionRoot()) {
+            return;
+        }
+
+        assert (getMavenExecutor() != null);
+
+        List<String> goals = Arrays.asList(new String[] { "install" });
+        List<String> activatedProfiles = Arrays.asList(new String[] { "release", "nightly" });
+
+        Properties userproperties = new Properties();
+        userproperties.put("skipTests", "true");
+
+        getMavenExecutor().execute(this, goals, activatedProfiles, null, userproperties, getProject(), getSession(),
+                getMaven(), true);
+
+    }
+}
---
+++ tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/LicenseCheck.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.tooling.pluginsuite.openengsbplugin;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @goal licenseCheck
+ * 
+ * @inheritedByDefault false
+ * 
+ * @requiresProject true
+ */
+public class LicenseCheck extends AbstractOpenengsbMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        if (!getProject().isExecutionRoot()) {
+            return;
+        }
+
+        assert (getMavenExecutor() != null);
+
+        List<String> goals = Arrays.asList(new String[] { "clean", "validate" });
+        List<String> activatedProfiles = Arrays.asList(new String[] { "license-check" });
+
+        getMavenExecutor().execute(this, goals, activatedProfiles, null, null, getProject(), getSession(), getMaven(),
+                true);
+
+    }
+
+}
