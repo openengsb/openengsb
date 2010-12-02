3b14c5fe0ab94d9eed01a20f787e4d4a0df13019
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2010-12-02 19:29:12 +0100

    move user-management test to correct package

--- core/security/src/test/java/org/openengsb/core/security/usermanagement/UserManagerImplTest.java
+++ core/security/src/test/java/org/openengsb/core/security/usermanagement/UserManagerImplTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.openengsb.core.usermanagement;
+package org.openengsb.core.security.usermanagement;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
