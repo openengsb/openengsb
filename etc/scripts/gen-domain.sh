05cbd98df4c5b416dd0ccf1e513e8bee44894b9a
Eduard Szente <eduard.szente@gmail.com>
2010-12-04 00:12:38 +0100

    [OPENENGSB-684] * changed gen-domain.sh so that mojo is invoked

--- etc/scripts/gen-domain.sh
+++ etc/scripts/gen-domain.sh
@@ -18,79 +18,10 @@
 # Helper script to generate an OpenEngSB domain. Tries to guess need variables
 # based on provided input.
 
-capitalize_ichar ()          #  Capitalizes initial character
-{                            #+ of argument string(s) passed.
+source $(dirname $0)/openengsbplugin-common.sh
 
-  string0="$@"               # Accepts multiple arguments.
+cd $ABSPATH/../../
 
-  firstchar=${string0:0:1}   # First character.
-  string1=${string0:1}       # Rest of string(s).
+GOAL="genDomain"
 
-  FirstChar=`echo "$firstchar" | tr a-z A-Z`
-                             # Capitalize first character.
-
-  echo "$FirstChar$string1"  # Output to stdout.
-
-}  
-
-DEFAULT_DOMAIN="mydomain"
-echo -n "Domain Name (is mydomain): "
-read DOMAIN
-if [ "$DOMAIN" = "" ]; then
-       DOMAIN=$DEFAULT_DOMAIN
-fi
-
-DEFAULT_VERSION="1.1.0-SNAPSHOT"
-echo -n "Version (is $DEFAULT_VERSION): "
-read VERSION
-if [ "$VERSION" = "" ]; then
-	VERSION=$DEFAULT_VERSION
-fi
-
-CAP_DOMAIN=`capitalize_ichar ${DOMAIN}`
-DEFAULT_NAME_PREFIX="OpenEngSB :: Domain :: ${CAP_DOMAIN}"
-echo -n "Prefix for project names (is $DEFAULT_NAME_PREFIX): "
-read NAME_PREFIX
-if [ "$NAME_PREFIX" = "" ]; then
-	NAME_PREFIX=$DEFAULT_NAME_PREFIX
-fi
-
-artifactId="openengsb-domain-$DOMAIN"
-
-mvn archetype:generate \
-	-DarchetypeGroupId="org.openengsb.tooling.archetypes" \
-	-DarchetypeArtifactId="openengsb-tooling-archetypes-domain" \
-	-DarchetypeVersion="$VERSION" \
-	-DgroupId="org.openengsb.domain.$DOMAIN" \
-	-DartifactId="$artifactId" \
-	-Dversion="$VERSION" \
-    -DdomainName="$DOMAIN" \
-	-DimplementationArtifactId="openengsb-domain-$DOMAIN" \
-	-Dpackage="org.openengsb.domain.$DOMAIN" \
-	-Dname="$NAME_PREFIX" \
-    -DdomainInterface="${CAP_DOMAIN}Domain" \
-	-DimplementationName="$NAME_PREFIX"
-
-if [ $? != 0 ]; then
-	exit $?
-fi
-echo "WARNING: aid ${artifactId}"
-if [ -e "$artifactId" ]; then
-	if [ ! -e "$DOMAIN" ]; then
-		echo "INFO: Renaming project from '$artifactId' to '$DOMAIN'"
-		mv "$artifactId" "$DOMAIN"
-		if [ -f "pom.xml" ]; then
-			sed "s/<module>$artifactId<\/module>/<module>$DOMAIN<\/module>/" pom.xml >pom.xml.new
-			mv pom.xml.new pom.xml
-		fi
-	else
-		echo "WARNING: Renaming of domain directory to '$DOMAIN' not possible, directory already exists!"
-	fi
-else 
-    echo "WARNING: no rename ${artifactId}"
-fi
-
-echo ""
-echo "DON'T FORGET TO ADD THE DOMAIN TO YOUR RELEASE/ASSEMBLY PROJECT!"
-echo "SUCCESS"
-echo ""
+mvn org.openengsb.tooling.pluginsuite:$PLUGIN_ARTIFACT_ID:$PLUGIN_VERSION:$GOAL
