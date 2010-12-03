7e15bf390a8a61ebb31f49de0b7d82053c714f80
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2010-12-03 16:16:15 +0100

    updated interface descrption

--- core/common/src/main/java/org/openengsb/core/common/usermanagement/UserManager.java
+++ core/common/src/main/java/org/openengsb/core/common/usermanagement/UserManager.java
@@ -18,21 +18,39 @@ package org.openengsb.core.common.usermanagement;
 
 import java.util.List;
 
+import org.openengsb.core.common.usermanagement.exceptions.UserExistsException;
+import org.openengsb.core.common.usermanagement.exceptions.UserNotFoundException;
 import org.openengsb.core.common.usermanagement.model.User;
 import org.springframework.osgi.context.BundleContextAware;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserManager extends UserDetailsService, BundleContextAware {
 
+    /**
+     * create an user and save it to the persistence,
+     * throws UserExistsException if the user already exists
+     */
+    void createUser(User user) throws UserExistsException;
 
-    void createUser(User user) ;
+    /**
+     * update the specified user in the persistence
+     */
+    void updateUser(User user) throws UserNotFoundException;
 
-    void updateUser(User user) ;
+    /**
+     * delete an user, specified by its username from the persistence
+     */
+    void deleteUser(String username) throws UserNotFoundException;
 
-    void deleteUser(String username) ;
+    /**
+     * get one single user from the persistence, if user does not exist
+     * UserNotFoundException is thrown.
+     */
+    User loadUserByUsername(String username) throws UserNotFoundException;
 
-    User loadUserByUsername(String username);
-
+    /**
+     * returns a list of all existing users
+     */
     List<User> getAllUser();
 
 }
