903d098959116f115507e9b32a12950703e460dc
Eduard Szente <eduard.szente@gmail.com>
2010-12-03 14:06:04 +0100

    [OPENENGSB-502] fix check if project is the root openengsb project

--- tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/Assemble.java
+++ tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/Assemble.java
@@ -40,7 +40,7 @@ public class Assemble extends AbstractOpenengsbMojo {
 			return;
 		}
 
-		if (getProject().hasParent()) {
+		if (getProject().hasParent() && getProject().getParent().getName().equals("oss-parent")) {
 			throw new MojoExecutionException(
 					"Please invoke this mojo only in the OpenEngSB root!");
 		}
--- tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/LicenseCheck.java
+++ tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/LicenseCheck.java
@@ -41,7 +41,7 @@ public class LicenseCheck extends AbstractOpenengsbMojo {
 			return;
 		}
 
-		if (getProject().hasParent()) {
+		if (getProject().hasParent() && getProject().getParent().getName().equals("oss-parent")) {
 			throw new MojoExecutionException(
 					"Please invoke this mojo only in the OpenEngSB root!");
 		}
