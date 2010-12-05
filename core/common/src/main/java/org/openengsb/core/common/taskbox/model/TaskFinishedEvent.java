1f8130d3be10139122f850a647db6a9351ceb0f8
Gregor Ryba <gregor.ryba@infor.com>
2010-12-05 15:43:05 +0100

    added returnNullTask to Task; specified imports for TaskFinishedEvent; Changed HashMap to Map ProcessBag
    Signed-off-by: Gregor Ryba <gregor.ryba@infor.com>

--- core/common/src/main/java/org/openengsb/core/common/taskbox/model/TaskFinishedEvent.java
+++ core/common/src/main/java/org/openengsb/core/common/taskbox/model/TaskFinishedEvent.java
@@ -1,5 +1,8 @@
 package org.openengsb.core.common.taskbox.model;
 
+import org.openengsb.core.common.workflow.model.InternalWorkflowEvent;
+import org.openengsb.core.common.workflow.model.ProcessBag;
+
 public class TaskFinishedEvent extends InternalWorkflowEvent {
     public TaskFinishedEvent(ProcessBag processBag){
         super(processBag);
