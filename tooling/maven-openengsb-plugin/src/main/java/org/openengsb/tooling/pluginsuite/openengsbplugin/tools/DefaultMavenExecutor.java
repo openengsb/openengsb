305225c3c896ddcd0a0ed444d12337533d2ad2ec
Eduard Szente <eduard.szente@gmail.com>
2010-12-06 17:22:38 +0100

    [OPENENGSB-502], [OPENEGSB-684] refactored existing maven mojos

--- tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/tools/DefaultMavenExecutor.java
+++ tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/tools/DefaultMavenExecutor.java
@@ -26,29 +26,43 @@ import org.apache.maven.execution.MavenExecutionResult;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 
 public class DefaultMavenExecutor implements MavenExecutor {
-	
-	private boolean changeInterActiveMode = false;
-	private boolean interActiveMode = true;
+
+    private MavenExecutionRequest embeddedRequest;
+
+    private Boolean recursive = null;
+    private Boolean interActiveMode = null;
 
     @Override
     public void execute(AbstractMojo mojo, List<String> goals, List<String> activatedProfiles,
             List<String> deactivatedProfiles, Properties userproperties, MavenProject project, MavenSession session,
-            Maven maven, boolean showErrors) throws MojoExecutionException {
+            Maven maven) throws MojoExecutionException {
 
+        generateRequestFromWrapperRequest(session);
+        initExecParametersFromArguments(goals, activatedProfiles, deactivatedProfiles, userproperties);
+        changeExecutionParametersIfNecessary();
+
+        printExecutionStartInfoLog(mojo.getLog());
+
+        MavenExecutionResult result = maven.execute(embeddedRequest);
+
+        printExecutionEndInfoLog(mojo.getLog());
+        logAndPassOnExceptionIfAny(result, mojo.getLog());
+
+    }
+
+    private void generateRequestFromWrapperRequest(MavenSession session) {
         MavenExecutionRequest wrapperRequest = session.getRequest();
+        embeddedRequest = DefaultMavenExecutionRequest.copy(wrapperRequest);
+    }
 
-        MavenExecutionRequest embeddedRequest = DefaultMavenExecutionRequest.copy(wrapperRequest);
-        
-		if (changeInterActiveMode) {
-			embeddedRequest.setInteractiveMode(interActiveMode);
-		}
-
+    private void initExecParametersFromArguments(List<String> goals, List<String> activatedProfiles,
+            List<String> deactivatedProfiles, Properties userproperties) {
         embeddedRequest.getGoals().clear();
         embeddedRequest.getGoals().addAll(goals);
-        embeddedRequest.setShowErrors(showErrors);
 
         if (userproperties != null) {
             embeddedRequest.getUserProperties().clear();
@@ -62,50 +76,73 @@ public class DefaultMavenExecutor implements MavenExecutor {
             embeddedRequest.getInactiveProfiles().clear();
             embeddedRequest.getInactiveProfiles().addAll(deactivatedProfiles);
         }
+    }
 
-        mojo.getLog().info("////////////////////////////////////////////////");
-        mojo.getLog().info(String.format("EMBEDDED EXECUTION REQUESTS - BEGIN"));
-
-        if (mojo.getLog().isDebugEnabled()) {
-            String goalsDebug = "goals:";
-            // only iterate when necessary
-            for (String goal : embeddedRequest.getGoals()) {
-                goalsDebug += String.format(" %s", goal);
-            }
-            mojo.getLog().debug(goalsDebug);
+    private void changeExecutionParametersIfNecessary() {
+        if (recursive != null) {
+            embeddedRequest.setRecursive(recursive);
         }
+        if (interActiveMode != null) {
+            embeddedRequest.setInteractiveMode(interActiveMode);
+        }
+    }
 
-        MavenExecutionResult result = maven.execute(embeddedRequest);
-		mojo.getLog().info(String.format("EMBEDDED EXECUTION REQUESTS - END"));
-		mojo.getLog().info("////////////////////////////////////////////////");
+    private void printExecutionStartInfoLog(Log log) {
+        log.info("////////////////////////////////////////////////");
+        log.info(String.format("EMBEDDED EXECUTION REQUESTS - BEGIN"));
 
-		if (result.hasExceptions()) {
-			mojo.getLog().warn("###################");
-			mojo.getLog()
-					.warn(String
-							.format("The following exceptions occured during executions of mojo %s:",
-									mojo.getClass().getName()));
-			for (Throwable t : result.getExceptions()) {
-				mojo.getLog().warn("--------");
-				mojo.getLog().warn(t);
-			}
-			mojo.getLog().warn("###################");
-			Throwable ex = result.getExceptions().get(0);
-			Throwable cause = ex.getCause();
-			String errmsg = (cause != null ? cause.getMessage() : ex
-					.getMessage());
-			throw new MojoExecutionException(
-					String.format(
-							"%s\nFAIL - see log statements above for additional info",
-							errmsg));
-		}
+        String goalsInfo = "goals:";
+        // only iterate when necessary
+        for (String goal : embeddedRequest.getGoals()) {
+            goalsInfo += String.format(" %s", goal);
+        }
+        log.info(goalsInfo);
 
-	}
+        if (embeddedRequest.getActiveProfiles() != null && embeddedRequest.getActiveProfiles().size() > 0) {
+            String profilesInfo = "active profiles:";
+            for (String profile : embeddedRequest.getActiveProfiles()) {
+                goalsInfo += String.format(" %s", profile);
+            }
+            log.info(profilesInfo);
+        }
+    }
 
-	@Override
-	public void setInterActiveMode(boolean interactiveMode) {
-		this.changeInterActiveMode = true;
-		this.interActiveMode = interactiveMode;
-	}
+    private void printExecutionEndInfoLog(Log log) {
+        log.info(String.format("EMBEDDED EXECUTION REQUESTS - END"));
+        log.info("////////////////////////////////////////////////");
+    }
+
+    @Override
+    public MavenExecutor setInterActiveMode(boolean interactiveMode) {
+        this.interActiveMode = interactiveMode;
+        return this;
+    }
+
+    @Override
+    public MavenExecutor setRecursive(boolean recursive) {
+        this.recursive = recursive;
+        return this;
+    }
+
+    private void logAndPassOnExceptionIfAny(MavenExecutionResult result, Log log) throws MojoExecutionException {
+        if (result.hasExceptions()) {
+            log.warn("###################");
+            log
+                    .warn("The following exceptions occured during execution:");
+            for (Throwable t : result.getExceptions()) {
+                log.warn("--------");
+                log.warn(t);
+            }
+            log.warn("###################");
+            Throwable ex = result.getExceptions().get(0);
+            Throwable cause = ex.getCause();
+            String errmsg = (cause != null ? cause.getMessage() : ex
+                    .getMessage());
+            throw new MojoExecutionException(
+                    String.format(
+                            "%s\nFAIL - see log statements above for additional info",
+                            errmsg));
+        }
+    }
 
 }
