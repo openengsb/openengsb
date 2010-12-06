43d4390c308265d6159388a12cd52704717464ca
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2010-12-06 15:21:12 +0100

    add interfaces requires for external calls

---
+++ core/common/src/main/java/org/openengsb/core/common/communication/MethodCall.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.common.communication;
+
+import java.util.Map;
+
+public class MethodCall {
+
+    private String serviceId;
+    private String methodName;
+    private Object[] args;
+    private Map<String, String> metaData;
+
+    public MethodCall(String serviceId, String methodName, Object[] args, Map<String, String> metaData) {
+        this.serviceId = serviceId;
+        this.methodName = methodName;
+        this.args = args;
+        this.metaData = metaData;
+    }
+
+    public MethodCall() {
+    }
+
+    public String getServiceId() {
+        return this.serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getMethodName() {
+        return this.methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public Object[] getArgs() {
+        return this.args;
+    }
+
+    public void setArgs(Object[] args) {
+        this.args = args;
+    }
+
+    public Map<String, String> getMetaData() {
+        return this.metaData;
+    }
+
+    public void setMetaData(Map<String, String> metaData) {
+        this.metaData = metaData;
+    }
+
+}
---
+++ core/common/src/main/java/org/openengsb/core/common/communication/MethodReturn.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.common.communication;
+
+import java.util.Map;
+
+public class MethodReturn {
+    public enum ReturnType {
+            Object, Exception,
+    }
+
+    private ReturnType type;
+    private Object arg;
+    private Map<String, String> metaData;
+
+    public MethodReturn(ReturnType type, Object arg, Map<String, String> metaData) {
+        this.type = type;
+        this.arg = arg;
+        this.metaData = metaData;
+    }
+
+    public MethodReturn() {
+    }
+
+    public ReturnType getType() {
+        return this.type;
+    }
+
+    public void setType(ReturnType type) {
+        this.type = type;
+    }
+
+    public Object getArg() {
+        return this.arg;
+    }
+
+    public void setArg(Object arg) {
+        this.arg = arg;
+    }
+
+    public Map<String, String> getMetaData() {
+        return this.metaData;
+    }
+
+    public void setMetaData(Map<String, String> metaData) {
+        this.metaData = metaData;
+    }
+
+}
