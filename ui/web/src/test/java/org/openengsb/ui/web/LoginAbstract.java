c4178c4bb46606f06c20fa5294a912533b34208a
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2010-12-06 11:20:59 +0100

    created abstract class for reuse login

---
+++ ui/web/src/test/java/org/openengsb/ui/web/LoginAbstract.java
@@ -0,0 +1,92 @@
+package org.openengsb.ui.web;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.apache.wicket.spring.test.ApplicationContextMock;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.openengsb.core.common.context.ContextCurrentService;
+import org.openengsb.core.common.service.DomainService;
+import org.openengsb.core.usermanagement.UserManagerImpl;
+import org.openengsb.core.usermanagement.model.User;
+import org.osgi.framework.ServiceReference;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+
+public abstract class LoginAbstract {
+
+    private WicketTester tester;
+    private ApplicationContextMock contextMock;
+    private UserManagerImpl userManager;
+
+    @Before
+    public void setup() {
+        contextMock = new ApplicationContextMock();
+        mockAuthentication();
+        mockIndex();
+
+        WebApplication app = new WicketApplication() {
+            @Override
+            protected void addInjector() {
+                addComponentInstantiationListener(new SpringComponentInjector(this, contextMock, true));
+            }
+        };
+        tester = new WicketTester(app);
+    }
+
+    private void mockIndex() {
+        DomainService managedServicesMock = mock(DomainService.class);
+        when(managedServicesMock.getAllServiceInstances()).thenAnswer(new Answer<List<ServiceReference>>() {
+            @Override
+            public List<ServiceReference> answer(InvocationOnMock invocation) {
+                return Collections.emptyList();
+            }
+        });
+        contextMock.putBean(managedServicesMock);
+        contextMock.putBean(mock(ContextCurrentService.class));
+    }
+
+    private void mockAuthentication() {
+        ProviderManager authManager = new ProviderManager();
+        List<AuthenticationProvider> providers = new ArrayList<AuthenticationProvider>();
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        userManager = mock(UserManagerImpl.class);
+        provider.setUserDetailsService(userManager);
+        providers.add(provider);
+        authManager.setProviders(providers);
+
+        final User user = new User("test", "password");
+        when(userManager.loadUserByUsername("test")).thenAnswer(new Answer<User>() {
+            @Override
+            public User answer(InvocationOnMock invocationOnMock) {
+                return user;
+            }
+        });
+        ArrayList<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+        grantedAuthorities.add(new GrantedAuthorityImpl("ROLE_ADMIN"));
+        final User admin = new User("admin", "password", grantedAuthorities);
+        when(userManager.loadUserByUsername("admin")).thenAnswer(new Answer<User>() {
+            @Override
+            public User answer(InvocationOnMock invocationOnMock) {
+                return admin;
+            }
+        });
+        contextMock.putBean("authenticationManager", authManager);
+    }
+
+    public WicketTester getTester() {
+        return tester;
+    }
+}
--- ui/web/src/test/java/org/openengsb/ui/web/LoginPageTest.java
+++ ui/web/src/test/java/org/openengsb/ui/web/LoginPageTest.java
@@ -16,101 +16,25 @@
 
 package org.openengsb.ui.web;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.wicket.feedback.FeedbackMessage;
-import org.apache.wicket.protocol.http.WebApplication;
-import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
-import org.apache.wicket.spring.test.ApplicationContextMock;
 import org.apache.wicket.util.tester.FormTester;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.openengsb.core.common.context.ContextCurrentService;
-import org.openengsb.core.common.service.DomainService;
-import org.openengsb.core.usermanagement.UserManagerImpl;
-import org.openengsb.core.usermanagement.model.User;
-import org.openengsb.ui.web.global.BookmarkablePageLabelLink;
 import org.openengsb.ui.web.global.header.HeaderTemplate;
-import org.osgi.framework.ServiceReference;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.GrantedAuthorityImpl;
 
-public class LoginPageTest {
+public class LoginPageTest extends LoginAbstract {
 
-    private WicketTester tester;
-    private ApplicationContextMock contextMock;
-    private UserManagerImpl userManager;
+    WicketTester tester;
 
     @Before
     public void setUp() {
-        contextMock = new ApplicationContextMock();
-        mockAuthentication();
-        mockIndex();
-
-        WebApplication app = new WicketApplication() {
-            @Override
-            protected void addInjector() {
-                addComponentInstantiationListener(new SpringComponentInjector(this, contextMock, true));
-            }
-        };
-        tester = new WicketTester(app);
-    }
-
-    private void mockIndex() {
-        DomainService managedServicesMock = mock(DomainService.class);
-        when(managedServicesMock.getAllServiceInstances()).thenAnswer(new Answer<List<ServiceReference>>() {
-            @Override
-            public List<ServiceReference> answer(InvocationOnMock invocation) {
-                return Collections.emptyList();
-            }
-        });
-        contextMock.putBean(managedServicesMock);
-        contextMock.putBean(mock(ContextCurrentService.class));
-    }
-
-    private void mockAuthentication() {
-        ProviderManager authManager = new ProviderManager();
-        List<AuthenticationProvider> providers = new ArrayList<AuthenticationProvider>();
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        userManager = mock(UserManagerImpl.class);
-        provider.setUserDetailsService(userManager);
-        providers.add(provider);
-        authManager.setProviders(providers);
-
-        final User user = new User("test", "password");
-        when(userManager.loadUserByUsername("test")).thenAnswer(new Answer<User>() {
-            @Override
-            public User answer(InvocationOnMock invocationOnMock) {
-                return user;
-            }
-        });
-        ArrayList<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
-        grantedAuthorities.add(new GrantedAuthorityImpl("ROLE_ADMIN"));
-        final User admin = new User("admin", "password", grantedAuthorities);
-        when(userManager.loadUserByUsername("admin")).thenAnswer(new Answer<User>() {
-            @Override
-            public User answer(InvocationOnMock invocationOnMock) {
-                return admin;
-            }
-        });
-        contextMock.putBean("authenticationManager", authManager);
+        tester = getTester();
     }
 
     @Test
@@ -165,29 +89,5 @@ public class LoginPageTest {
         tester.assertComponent("header", HeaderTemplate.class);
     }
 
-    @Test
-    public void testHeaderComponentsForAdmin_UserServiceShouldBeVisible() {
-        tester.startPage(LoginPage.class);
-        FormTester formTester = tester.newFormTester("loginForm");
-        formTester.setValue("username", "admin");
-        formTester.setValue("password", "password");
-        formTester.submit();
-        BookmarkablePageLabelLink userServiceLink = (BookmarkablePageLabelLink) tester
-            .getComponentFromLastRenderedPage("header:headerMenuItems:5:link");
-        assertNotNull(userServiceLink);
-        assertThat(userServiceLink.getPageClass().getCanonicalName(), is(UserService.class.getCanonicalName()));        
-    }
-
-    @Test
-    public void testHeaderComponentsForNormalUser_UserServiceShouldNotBeVisible() {
-        tester.startPage(LoginPage.class);
-        FormTester formTester = tester.newFormTester("loginForm");
-        formTester.setValue("username", "user");
-        formTester.setValue("password", "password");
-        formTester.submit();
-        BookmarkablePageLabelLink userServiceLink = (BookmarkablePageLabelLink) tester
-            .getComponentFromLastRenderedPage("header:headerMenuItems:5:link");
-        assertNull(userServiceLink);
-    }
 
 }
