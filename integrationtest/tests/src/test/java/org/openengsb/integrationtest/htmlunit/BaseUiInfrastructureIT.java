e8a5f79c1a2b09f0cb8a4c076c8fb6ba4311b7ee
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2010-12-08 23:39:32 +0100

    add ui-test with accessDeniedException

--- integrationtest/tests/src/test/java/org/openengsb/integrationtest/htmlunit/BaseUiInfrastructureIT.java
+++ integrationtest/tests/src/test/java/org/openengsb/integrationtest/htmlunit/BaseUiInfrastructureIT.java
@@ -17,12 +17,16 @@
 package org.openengsb.integrationtest.htmlunit;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openengsb.integrationtest.util.AbstractExamTestHelper;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -31,9 +35,20 @@ import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 @RunWith(JUnit4TestRunner.class)
 public class BaseUiInfrastructureIT extends AbstractExamTestHelper {
 
+    private WebClient webClient;
+
+    @Before
+    public void setUp() throws Exception {
+        webClient = new WebClient();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        webClient.closeAllWindows();
+    }
+
     @Test
     public void testIfAllMainNavigationLinksWork() throws Exception {
-        final WebClient webClient = new WebClient();
         String loginPageEntryUrl =
             "http://localhost:8090/openengsb/?wicket:bookmarkablePage=:org.openengsb.ui.web.LoginPage";
         final HtmlPage page = webClient.getPage(loginPageEntryUrl);
@@ -53,7 +68,23 @@ public class BaseUiInfrastructureIT extends AbstractExamTestHelper {
         assertTrue(servicePage.asText().contains("Services with state = Connecting"));
         HtmlPage usermanagementPage = testClient.getAnchorByText("User Management").click();
         assertTrue(usermanagementPage.asText().contains("Create new user"));
-        webClient.closeAllWindows();
+
+    }
+
+    @Test(expected = FailingHttpStatusCodeException.class)
+    public void testUserLoginWithLimitedAccess() throws Exception {
+        String loginPageEntryUrl =
+            "http://localhost:8090/openengsb/?wicket:bookmarkablePage=:org.openengsb.ui.web.LoginPage";
+        final HtmlPage page = webClient.getPage(loginPageEntryUrl);
+        HtmlForm form = page.getForms().get(0);
+        HtmlSubmitInput loginButton = form.getInputByValue("Login");
+        form.getInputByName("username").setValueAttribute("user");
+        form.getInputByName("password").setValueAttribute("password");
+        HtmlPage indexPage = loginButton.click();
+        assertTrue(indexPage.asText().contains("This page represents"));
+        HtmlPage testClient;
+        testClient = indexPage.getAnchorByText("Test Client").click();
+        fail("could display Test client");
     }
 
 }
