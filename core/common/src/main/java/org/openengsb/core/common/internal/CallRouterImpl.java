e3e80b188dce214c5b26960ae11923e13c180bb7
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2010-12-08 14:31:34 +0100

    refactor PortHandler-Thread

--- core/common/src/main/java/org/openengsb/core/common/internal/CallRouterImpl.java
+++ core/common/src/main/java/org/openengsb/core/common/internal/CallRouterImpl.java
@@ -16,25 +16,19 @@
 
 package org.openengsb.core.common.internal;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openengsb.core.common.OpenEngSBService;
 import org.openengsb.core.common.communication.CallRouter;
 import org.openengsb.core.common.communication.IncomingPort;
 import org.openengsb.core.common.communication.MethodCall;
 import org.openengsb.core.common.communication.MethodReturn;
-import org.openengsb.core.common.communication.MethodReturn.ReturnType;
 import org.openengsb.core.common.communication.OutgoingPort;
-import org.openengsb.core.common.util.OsgiServiceUtils;
 import org.osgi.framework.BundleContext;
 import org.springframework.osgi.context.BundleContextAware;
 
@@ -44,67 +38,19 @@ public class CallRouterImpl implements CallRouter, BundleContextAware {
 
     private ExecutorService executor = Executors.newCachedThreadPool();
 
-    private class PortHandler implements Runnable {
-        private IncomingPort port;
-
-        public PortHandler(IncomingPort port) {
-            this.port = port;
-        }
-
-        @Override
-        public void run() {
-            while (true) {
-                UUID id = UUID.randomUUID();
-                MethodCall call = port.listen(id);
-                String serviceId = call.getServiceId();
-
-                OpenEngSBService service =
-                    OsgiServiceUtils.getService(bundleContext, OpenEngSBService.class, "(id=" + serviceId + ")");
-                Class<?>[] argTypes = getArgTypes(call.getArgs());
-
-                Method method;
-                try {
-                    method = service.getClass().getMethod(call.getMethodName(), argTypes);
-                } catch (NoSuchMethodException e) {
-                    throw new IllegalArgumentException(e);
-                }
-                MethodReturn resultContainer = new MethodReturn();
-                try {
-                    Object result = method.invoke(service, call.getArgs());
-                    resultContainer.setType(ReturnType.Object);
-                    resultContainer.setArg(result);
-                } catch (InvocationTargetException e) {
-                    resultContainer.setType(ReturnType.Exception);
-                    resultContainer.setArg(e.getCause());
-                } catch (IllegalAccessException e) {
-                    resultContainer.setType(ReturnType.Exception);
-                    resultContainer.setArg(e);
-                }
-
-                port.sendResponse(id, resultContainer);
-            }
-        }
-
-        private Class<?>[] getArgTypes(Object[] args) {
-            Class<?>[] result = new Class[args.length];
-            for (int i = 0; i < args.length; i++) {
-                result[i] = args[i].getClass();
-            }
-            return result;
-        }
-    }
-
     private Map<String, OutgoingPort> ports = new HashMap<String, OutgoingPort>();
-    private BundleContext bundleContext;
+    BundleContext bundleContext;
 
     @Override
     public void registerIncomingPort(IncomingPort port) {
-        PortHandler portHandler = new PortHandler(port);
+        log.info("registering incoming port " + port);
+        PortHandler portHandler = new PortHandler(bundleContext, port);
         executor.execute(portHandler);
     }
 
     @Override
     public void registerOutgoingPort(String scheme, OutgoingPort port) {
+        log.info(String.format("registering outgoing port for scheme %s: %s", scheme, port));
         ports.put(scheme, port);
     }
 
---
+++ core/common/src/main/java/org/openengsb/core/common/internal/PortHandler.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.common.internal;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+import org.openengsb.core.common.OpenEngSBService;
+import org.openengsb.core.common.communication.IncomingPort;
+import org.openengsb.core.common.communication.MethodCall;
+import org.openengsb.core.common.communication.MethodReturn;
+import org.openengsb.core.common.communication.MethodReturn.ReturnType;
+import org.openengsb.core.common.util.OsgiServiceUtils;
+import org.osgi.framework.BundleContext;
+
+class PortHandler implements Runnable {
+    private BundleContext bundleContext;
+    private IncomingPort port;
+
+    public PortHandler(BundleContext bundleContext, IncomingPort port) {
+        this.bundleContext = bundleContext;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            UUID id = UUID.randomUUID();
+            MethodCall call = port.listen(id);
+            handleMethodCall(id, call);
+        }
+    }
+
+    private void handleMethodCall(UUID id, MethodCall call) {
+        final String filter = "(id=" + call.getServiceId() + ")";
+        OpenEngSBService service = OsgiServiceUtils.getService(bundleContext, OpenEngSBService.class, filter);
+
+        Object[] args = call.getArgs();
+        Method method = findMethod(service, call.getMethodName(), getArgTypes(args));
+
+        MethodReturn resultContainer = invokeMethod(service, method, args);
+        port.sendResponse(id, resultContainer);
+    }
+
+    private MethodReturn invokeMethod(OpenEngSBService service, Method method, Object[] args) {
+        MethodReturn resultContainer = new MethodReturn();
+        try {
+            Object result = method.invoke(service, args);
+            resultContainer.setType(ReturnType.Object);
+            resultContainer.setArg(result);
+        } catch (InvocationTargetException e) {
+            resultContainer.setType(ReturnType.Exception);
+            resultContainer.setArg(e.getCause());
+        } catch (IllegalAccessException e) {
+            resultContainer.setType(ReturnType.Exception);
+            resultContainer.setArg(e);
+        }
+        return resultContainer;
+    }
+
+    private Method findMethod(OpenEngSBService service, String methodName, Class<?>[] argTypes) {
+        Method method;
+        try {
+            method = service.getClass().getMethod(methodName, argTypes);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return method;
+    }
+
+    private Class<?>[] getArgTypes(Object[] args) {
+        Class<?>[] result = new Class[args.length];
+        for (int i = 0; i < args.length; i++) {
+            result[i] = args[i].getClass();
+        }
+        return result;
+    }
+}
