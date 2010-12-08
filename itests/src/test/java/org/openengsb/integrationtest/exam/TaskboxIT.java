67d5cec8d52108926e6d7354a3265abde8300838
Lukas Puschmann <lukas.puschmann@gmx.at>
2010-12-08 10:33:18 +0100

    WIP IT Test for createHumanTask Ruleflow
    Signed-off-by: Lukas Puschmann <lukas.puschmann@gmx.at>

--- itests/src/test/java/org/openengsb/integrationtest/exam/TaskboxIT.java
+++ itests/src/test/java/org/openengsb/integrationtest/exam/TaskboxIT.java
@@ -16,35 +16,98 @@
 
 package org.openengsb.integrationtest.exam;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openengsb.core.common.context.ContextCurrentService;
-import org.openengsb.core.common.taskbox.TaskboxException;
 import org.openengsb.core.common.taskbox.TaskboxService;
-import org.openengsb.core.common.workflow.RuleManager;
+import org.openengsb.core.common.taskbox.model.Task;
 import org.openengsb.core.common.workflow.WorkflowException;
+import org.openengsb.core.common.workflow.WorkflowService;
+import org.openengsb.core.common.workflow.model.ProcessBag;
 import org.openengsb.integrationtest.util.AbstractExamTestHelper;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 
 @RunWith(JUnit4TestRunner.class)
 public class TaskboxIT extends AbstractExamTestHelper {
     private TaskboxService taskboxService;
-    private RuleManager ruleManager;
+    private WorkflowService workflowService;
 
     @Before
     public void setUp() throws Exception {
-        authenticateAsAdmin();
-        taskboxService = getOsgiService(TaskboxService.class);
-        ruleManager = getOsgiService(RuleManager.class);
         ContextCurrentService contextService = getOsgiService(ContextCurrentService.class);
         contextService.createContext("it-taskbox");
         contextService.setThreadLocalContext("it-taskbox");
-        ruleManager.addGlobal(TaskboxService.class.getCanonicalName(), "taskbox");
+
+        taskboxService = getOsgiService(TaskboxService.class);
+        ruleManager = getOsgiService(RuleManager.class);
+    }
+
+    @After
+    public void cleanup() throws IOException {
+        super.afterClass();
     }
 
     @Test
-    public void test_Name() throws TaskboxException, WorkflowException {
+    public void testHumanTaskFlow_shouldWorkWithGivenProcessBag() throws WorkflowException {
+        ProcessBag processBag = new ProcessBag();
+        processBag.addProperty("test", "test");
+        Map<String, Object> parameterMap = new HashMap<String, Object>();
+        parameterMap.put("processBag", processBag);
 
+        assertTrue(taskboxService.getOpenTasks().size() == 0);
+
+        workflowService.startFlow("humantask", parameterMap);
+
+        assertNotNull(processBag.getProcessId());
+        assertTrue(taskboxService.getOpenTasks().size() == 1);
+
+        Task task = taskboxService.getOpenTasks().get(0);
+        assertEquals(task.getProcessId(), processBag.getProcessId());
+        assertEquals(task.getProperty("test"), "test");
+        assertNotNull(task.getTaskId());
+
+        taskboxService.finishTask(task);
+        assertTrue(taskboxService.getOpenTasks().size() == 0);
+    }
+
+    @Ignore
+    @Test
+    public void testHumanTaskFlow_shouldCreateOwnProcessBag() throws WorkflowException {
+        assertTrue(taskboxService.getOpenTasks().size() == 0);
+
+        workflowService.startFlow("humantask");
+        assertTrue(taskboxService.getOpenTasks().size() == 1);
+
+        Task task = taskboxService.getOpenTasks().get(0);
+        assertNotNull(task.getProcessId());
+        assertNotNull(task.getTaskId());
+
+        taskboxService.finishTask(task);
+        assertTrue(taskboxService.getOpenTasks().size() == 0);
+    }
+
+    @Ignore
+    @Test
+    public void testHumanTaskFlow_shouldHandleMultipleTasks() throws WorkflowException {
+        assertTrue(taskboxService.getOpenTasks().size() == 0);
+
+        workflowService.startFlow("humantask");
+        workflowService.startFlow("humantask");
+        workflowService.startFlow("humantask");
+        assertTrue(taskboxService.getOpenTasks().size() == 3);
+
+        taskboxService.finishTask(taskboxService.getOpenTasks().get(0));
+        assertTrue(taskboxService.getOpenTasks().size() == 2);
     }
 }
