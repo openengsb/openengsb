481e25774da75f45e17bb839420a3e3fec120a78
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2010-12-15 23:08:54 +0100

    fixed ui-it tests

--- integrationtest/tests/src/test/java/org/openengsb/integrationtest/htmlunit/BaseUiInfrastructureIT.java
+++ integrationtest/tests/src/test/java/org/openengsb/integrationtest/htmlunit/BaseUiInfrastructureIT.java
@@ -16,37 +16,41 @@
 
 package org.openengsb.integrationtest.htmlunit;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.openengsb.integrationtest.util.AbstractExamTestHelper;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 
-import static org.junit.Assert.*;
-
 @RunWith(JUnit4TestRunner.class)
 public class BaseUiInfrastructureIT extends AbstractExamTestHelper {
 
     private WebClient webClient;
-    private String loginPageEntryUrl;
+    private String loginPageEntryUrl
+        = "http://localhost:8090/openengsb/?wicket:bookmarkablePage=:org.openengsb.ui.web.LoginPage";
 
     @Before
     public void setUp() throws Exception {
         webClient = new WebClient();
-        loginPageEntryUrl = "http://localhost:8090/openengsb/?wicket:bookmarkablePage=:org.openengsb.ui.web.LoginPage";
     }
 
     @After
     public void tearDown() throws Exception {
         webClient.closeAllWindows();
+        FileUtils.deleteDirectory(new File(getWorkingDirectory()));
     }
 
     @Test
@@ -68,10 +72,9 @@ public class BaseUiInfrastructureIT extends AbstractExamTestHelper {
         assertTrue(servicePage.asText().contains("Services with state = Connecting"));
         HtmlPage usermanagementPage = testClient.getAnchorByText("User Management").click();
         assertTrue(usermanagementPage.asText().contains("Create new user"));
-
     }
 
-    @Test(expected = FailingHttpStatusCodeException.class)
+    @Test
     public void testUserLoginWithLimitedAccess() throws Exception {
         final HtmlPage page = webClient.getPage(loginPageEntryUrl);
         HtmlForm form = page.getForms().get(0);
@@ -80,14 +83,14 @@ public class BaseUiInfrastructureIT extends AbstractExamTestHelper {
         form.getInputByName("password").setValueAttribute("password");
         HtmlPage indexPage = loginButton.click();
         assertTrue(indexPage.asText().contains("This page represents"));
-        HtmlPage testClient;
-        testClient = indexPage.getAnchorByText("Test Client").click();
-        fail("could display Test client");
+        assertFalse(indexPage.asText().contains("User Management"));
     }
 
     @Test
     public void testCreateNewUser_LoginAsNewUser_UserManagementTabShouldNotBeVisible() throws Exception {
-        final HtmlPage page = webClient.getPage(loginPageEntryUrl);
+        HtmlPage page = webClient.getPage("http://localhost:8090/openengsb/");
+        page = page.getAnchorByText("Login").click();
+
         HtmlForm form = page.getForms().get(0);
         HtmlSubmitInput loginButton = form.getInputByValue("Login");
         form.getInputByName("username").setValueAttribute("admin");
@@ -99,16 +102,16 @@ public class BaseUiInfrastructureIT extends AbstractExamTestHelper {
         assertTrue(usermanagementPage.asText().contains("Create new user"));
 
         //get user creation form:
-        HtmlForm userCreatForm = usermanagementPage.getForms().get(0);
-        assertNotNull(userCreatForm);
-        HtmlSubmitInput createButton = userCreatForm.getInputByValue("Ok");
-        userCreatForm.getInputByName("username").setValueAttribute("newUser");
-        userCreatForm.getInputByName("password").setValueAttribute("password");
-        userCreatForm.getInputByName("passwordVerification").setValueAttribute("password");
+        form = usermanagementPage.getForms().get(1);
+        assertNotNull(form);
+
+        HtmlSubmitInput createButton = form.getInputByValue("Ok");
+        form.getInputByName("username").setValueAttribute("newUser");
+        form.getInputByName("password").setValueAttribute("password");
+        form.getInputByName("passwordVerification").setValueAttribute("password");
         indexPage = createButton.click();
 
-        assertTrue(form.getInputsByName("username").isEmpty());
-        assertTrue(indexPage.asText().contains("newUSer"));
+        assertTrue(indexPage.asText().contains("newUser"));
 
         HtmlPage logoutPage = indexPage.getAnchorByText("Logout").click();
         HtmlPage loginPage = logoutPage.getAnchorByText("Login").click();
@@ -119,7 +122,6 @@ public class BaseUiInfrastructureIT extends AbstractExamTestHelper {
         HtmlPage userIndexPage = loginButton.click();
         assertTrue(userIndexPage.asText().contains("This page represents"));
         assertFalse(userIndexPage.asText().contains("User Management"));
-        
     }
 
 }
