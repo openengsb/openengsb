17502b07b4c0f28d651f4af8bebca3c0fbdc4cd1
Michael Handler <michael.handler@fullstop.at>
2010-12-29 13:36:15 +0100

    Quick fix integration tests.
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

--- itests/src/test/java/org/openengsb/integrationtest/util/AbstractExamTestHelper.java
+++ itests/src/test/java/org/openengsb/integrationtest/util/AbstractExamTestHelper.java
@@ -156,6 +156,8 @@ public abstract class AbstractExamTestHelper extends AbstractIntegrationTest {
             Helper.getDefaultOptions(),
             Helper.loadKarafFeatures("config", "ssh", "management", "wrapper", "obr"),
             Helper.setLogLevel("WARN"),
+            mavenBundle(maven().groupId("org.apache.aries.blueprint").artifactId("org.apache.aries.blueprint")
+                .version("0.2-incubating")),
             scanFeatures(maven().groupId("org.openengsb").artifactId("openengsb").type("xml").classifier("features")
                 .versionAsInProject(), "openengsb-core"), workingDirectory(getWorkingDirectory()),
             vmOption("-Dorg.osgi.framework.system.packages.extra=sun.reflect"),
