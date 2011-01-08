b6a6c8db52da89456c18201e29341acf4927fe65
Eduard Szente <eduard.szente@gmail.com>
2011-01-08 19:41:08 +0100

    * avoiding recursive execution request for licensecheck mojo * removed license-check profile and therefore changed the pre-push script

--- tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/LicenseCheck.java
+++ tooling/maven-openengsb-plugin/src/main/java/org/openengsb/tooling/pluginsuite/openengsbplugin/LicenseCheck.java
@@ -27,6 +27,7 @@ import javax.xml.xpath.XPathConstants;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.openengsb.tooling.pluginsuite.openengsbplugin.tools.Tools;
@@ -47,6 +48,8 @@ import org.w3c.dom.Node;
  * 
  */
 public class LicenseCheck extends AbstractOpenengsbMojo {
+
+    private static final Logger log = Logger.getLogger(LicenseCheck.class);
 
     private List<String> goals;
     private List<String> activatedProfiles;
@@ -77,10 +80,8 @@ public class LicenseCheck extends AbstractOpenengsbMojo {
     }
 
     private void initializeMavenExecutionProperties(String profileName) {
-        goals = Arrays
-            .asList(new String[]{ "clean", "validate" });
-        activatedProfiles = Arrays
-            .asList(new String[]{ profileName });
+        goals = Arrays.asList(new String[] { "clean", "validate" });
+        activatedProfiles = Arrays.asList(new String[] { profileName });
         userProperties = new Properties();
         userProperties.put("license.header", licenseHeaderFile.toURI().toString());
         userProperties.put("license.failIfMissing", "true");
@@ -90,15 +91,14 @@ public class LicenseCheck extends AbstractOpenengsbMojo {
 
     private void executeMavenWithCustomPom(File pom) throws MojoExecutionException {
         getNewMavenExecutor().setRecursive(true).setCustomPomFile(pom)
-            .execute(this, goals, activatedProfiles, null, userProperties,
-                getProject(), getSession(), getMaven());
+                .execute(this, goals, activatedProfiles, null, userProperties, getProject(), getSession(), getMaven());
     }
 
     private File readHeaderStringAndwriteHeaderIntoTmpFile() throws MojoExecutionException {
         try {
 
-            String headerString = IOUtils.toString(getClass().getClassLoader()
-                .getResourceAsStream("licenseCheck/header.txt"));
+            String headerString = IOUtils.toString(getClass().getClassLoader().getResourceAsStream(
+                    "licenseCheck/header.txt"));
             File generatedFile = Tools.generateTmpFile(headerString, ".txt");
             return generatedFile;
         } catch (Exception e) {
@@ -108,19 +108,15 @@ public class LicenseCheck extends AbstractOpenengsbMojo {
 
     private File configureTmpPom(String profileName) throws MojoExecutionException {
         try {
-            Document originalPomDocument =
-                Tools.parseXMLFromString(FileUtils.readFileToString(getSession().getRequest().getPom()));
+            Document originalPomDocument = Tools.parseXMLFromString(FileUtils.readFileToString(getSession()
+                    .getRequest().getPom()));
             // read plugin default configuration
-            Document configDocument = Tools.parseXMLFromString(
-                IOUtils.toString(getClass().getClassLoader()
+            Document configDocument = Tools.parseXMLFromString(IOUtils.toString(getClass().getClassLoader()
                     .getResourceAsStream("licenseCheck/licenseCheckConfig.xml")));
 
             // .. and insert the profile node into the pom dom tree ..
-            Node licenseCheckMojoProfileNode =
-                Tools.evaluateXPath("/lc:licenseCheckMojo/lc:profile", configDocument,
-                    nsContext,
-                    XPathConstants.NODE,
-                    Node.class);
+            Node licenseCheckMojoProfileNode = Tools.evaluateXPath("/lc:licenseCheckMojo/lc:profile", configDocument,
+                    nsContext, XPathConstants.NODE, Node.class);
 
             Node idNode = configDocument.createElement("id");
             idNode.setTextContent(profileName);
@@ -129,18 +125,19 @@ public class LicenseCheck extends AbstractOpenengsbMojo {
             Node importedLicenseCheckProfileNode = originalPomDocument.importNode(licenseCheckMojoProfileNode, true);
 
             Tools.insertDomNode(originalPomDocument, importedLicenseCheckProfileNode, "/pom:project/pom:profiles",
-                nsContext);
+                    nsContext);
 
             // .. the finally serialize that modified pom into a temporary file
             String serializedXml = Tools.serializeXML(originalPomDocument);
 
             String baseDirURI = getSession().getRequest().getPom().getParentFile().toURI().toString();
             File temporaryPom = new File(new URI(baseDirURI + "/" + "tmpPom.xml"));
-            
+
             FileUtils.writeStringToFile(temporaryPom, serializedXml);
 
             return temporaryPom;
         } catch (Exception e) {
+            log.warn(e.getMessage(), e);
             throw new MojoExecutionException("Couldn't configure temporary pom for this execution!", e);
         }
     }
