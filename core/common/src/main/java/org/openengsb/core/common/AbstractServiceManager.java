3f57f671830fb84049b90379e34c3d9454d78e93
Andreas Pieber <anpieber@gmail.com>
2011-01-16 18:52:34 +0100

    [OPENENGSB-805] fix syncroniced problem in servicemanager
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- core/common/src/main/java/org/openengsb/core/common/AbstractServiceManager.java
+++ core/common/src/main/java/org/openengsb/core/common/AbstractServiceManager.java
@@ -39,13 +39,13 @@ import org.springframework.aop.framework.ProxyFactory;
  * Base class for {@link ServiceManager} implementations. Handles all OSGi related stuff and exporting the right service
  * properties that are needed for service discovery. Furthermore this class also persists the connector state and
  * restores all persisted connectors at the next startup.
- * 
+ *
  * All service-specific action, like descriptor building, service instantiation and service updating are encapsulated in
  * a {@link ServiceInstanceFactory}. Creating a new service manager should be as simple as implementing the
  * {@link ServiceInstanceFactory} and creating a subclass of this class:
- * 
+ *
  * This class has to be instantiated via Spring, as the BundleContext has to be set as it is BundleContextAware.
- * 
+ *
  * <pre>
  * public class ExampleServiceManager extends AbstractServiceManager&lt;ExampleDomain, TheInstanceType&gt; {
  *     public ExampleServiceManager(ServiceInstanceFactory&lt;ExampleDomain, TheInstanceType&gt; factory) {
@@ -53,7 +53,7 @@ import org.springframework.aop.framework.ProxyFactory;
  *     }
  * }
  * </pre>
- * 
+ *
  * @param <DomainType> interface of the domain this service manages
  * @param <InstanceType> actual service implementation this service manages
  */
@@ -91,12 +91,12 @@ public abstract class AbstractServiceManager<DomainType extends Domain, Instance
     }
 
     @Override
-    public void updateWithoutValidation(String id, Map<String, String> attributes) {
+    public synchronized void updateWithoutValidation(String id, Map<String, String> attributes) {
         updateServiceInstance(id, attributes);
     }
 
     @Override
-    public MultipleAttributeValidationResult update(String id, Map<String, String> attributes) {
+    public synchronized MultipleAttributeValidationResult update(String id, Map<String, String> attributes) {
         MultipleAttributeValidationResult validateService;
         if (isAlreadyCreated(id)) {
             validateService = factory.updateValidation(getService(id), attributes);
@@ -197,7 +197,7 @@ public abstract class AbstractServiceManager<DomainType extends Domain, Instance
     }
 
     @Override
-    public void delete(String id) {
+    public synchronized void delete(String id) {
         synchronized (services) {
             final DomainRepresentation domainRepresentation = services.get(id);
             domainRepresentation.registration.unregister();
