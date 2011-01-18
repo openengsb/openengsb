8372b5d643b272224efec35658b78317e7da2c08
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-01-18 11:00:43 +0100

    add dedicated ContextHolder for contextid-threadlocal

---
+++ core/common/src/main/java/org/openengsb/core/common/ContextHolder.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.common;
+
+public class ContextHolder {
+
+    private static ContextHolder instance = new ContextHolder();
+
+    private ThreadLocal<String> currentContextId = new InheritableThreadLocal<String>();
+
+    public static ContextHolder get() {
+        return instance;
+    }
+
+    public void setCurrentContextId(String value) {
+        currentContextId.set(value);
+    }
+
+    public String getCurrentContextId() {
+        return currentContextId.get();
+    }
+
+    private ContextHolder() {
+    }
+
+}
