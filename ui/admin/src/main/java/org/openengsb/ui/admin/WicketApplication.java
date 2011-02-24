aac290a666207b54ad3a98b87ca509a7d0193014
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-02-24 09:48:39 +0100

    [OPENENGSB-615] mount pages with their class' simple name

--- ui/admin/src/main/java/org/openengsb/ui/admin/WicketApplication.java
+++ ui/admin/src/main/java/org/openengsb/ui/admin/WicketApplication.java
@@ -18,11 +18,33 @@ package org.openengsb.ui.admin;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.target.coding.MixedParamUrlCodingStrategy;
+import org.openengsb.ui.admin.basePage.BasePage;
+import org.openengsb.ui.admin.contextSetPage.ContextSetPage;
 import org.openengsb.ui.admin.index.Index;
 import org.openengsb.ui.admin.loginPage.LoginPage;
+import org.openengsb.ui.admin.sendEventPage.SendEventPage;
+import org.openengsb.ui.admin.serviceListPage.ServiceListPage;
+import org.openengsb.ui.admin.taskOverview.TaskOverview;
+import org.openengsb.ui.admin.testClient.TestClient;
+import org.openengsb.ui.admin.userService.UserService;
+import org.openengsb.ui.common.OpenEngSBPage;
 import org.openengsb.ui.common.OpenEngSBWicketApplication;
 
 public class WicketApplication extends OpenEngSBWicketApplication {
+
+    public WicketApplication() {
+        @SuppressWarnings("unchecked")
+        Class<? extends Page>[] pages =
+            new Class[]{ OpenEngSBPage.class, BasePage.class, Index.class, TestClient.class,
+                ContextSetPage.class, LoginPage.class, ServiceListPage.class, TaskOverview.class, UserService.class,
+                SendEventPage.class,
+        };
+        for (Class<? extends Page> page : pages) {
+            mount(new MixedParamUrlCodingStrategy(page.getSimpleName(), page, null));
+        }
+    }
+
     @Override
     public Class<? extends Page> getHomePage() {
         return Index.class;
