50a60cd2386cf4f9bb5bb6985bcbf3f2b71a9de9
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-03-03 20:22:58 +0100

    [OPENENGSB-1025] make itest working and actually validate something

--- itests/src/test/java/org/openengsb/itests/exam/JMSPortIT.java
+++ itests/src/test/java/org/openengsb/itests/exam/JMSPortIT.java
@@ -17,18 +17,36 @@
 package org.openengsb.itests.exam;
 
 import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openengsb.core.common.communication.OutgoingPort;
 import org.openengsb.core.common.util.OsgiServiceUtils;
+import org.openengsb.core.common.workflow.RuleBaseException;
+import org.openengsb.core.common.workflow.RuleManager;
+import org.openengsb.core.common.workflow.model.RuleBaseElementId;
+import org.openengsb.core.common.workflow.model.RuleBaseElementType;
 import org.openengsb.itests.util.AbstractExamTestHelper;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.springframework.jms.core.JmsTemplate;
 
 @RunWith(JUnit4TestRunner.class)
 public class JMSPortIT extends AbstractExamTestHelper {
+
+    private RuleManager ruleManager;
+
+    @Before
+    public void setUp() throws Exception {
+        this.ruleManager = getOsgiService(RuleManager.class);
+    }
 
     // @Test
     public void jmsPort_shouldBeExportedWithCorrectId() throws Exception {
@@ -38,17 +56,44 @@ public class JMSPortIT extends AbstractExamTestHelper {
     }
 
     @Test
-    public void startSimpleWorkflow_ShouldReturn42() {
+    public void startSimpleWorkflow_ShouldReturn42() throws Exception {
+        addWorkflow("simpleFlow");
         System.out.println("Starting Integration Test");
         ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory("tcp://localhost:6549");
         JmsTemplate template = new JmsTemplate(cf);
         // template.setReceiveTimeout(4000);
-        String request =
-            "{\"callId\":\"12345\",\"answer\":true,\"classes\":[\"java.lang.String\", \"org.openengsb.core.common.workflow.model.ProcessBag\"],"
-                    + "\"methodName\":\"executeWorkflow\",\"metaData\":{\"serviceId\":\"workflowService\", \"contextId\":\"foo\"},\"args\":[\"simpleFlow\", {}]}";
-        System.out.println("Seding request");
+        String request = ""
+            + "{"
+            + "    \"callId\": \"12345\","
+            + "    \"answer\": true,"
+            + "    \"classes\": ["
+            + "        \"java.lang.String\","
+            + "        \"org.openengsb.core.common.workflow.model.ProcessBag\""
+            + "    ],"
+            + "    \"methodName\": \"executeWorkflow\","
+            + "    \"metaData\": {"
+            + "        \"serviceId\": \"workflowService\","
+            + "        \"contextId\": \"foo\""
+            + "    },"
+            + "    \"args\": ["
+            + "        \"simpleFlow\","
+            + "        {"
+            + "        }"
+            + "    ]"
+            + "}";
         template.convertAndSend("receive", request);
-        Object receiveAndConvert = template.receiveAndConvert("12345");
-        System.out.println(receiveAndConvert);
+        String result = (String) template.receiveAndConvert("12345");
+        assertThat(result, containsString("The answer to life the universe and everything"));
+    }
+
+    private void addWorkflow(String workflow) throws IOException, RuleBaseException {
+        if (ruleManager.get(new RuleBaseElementId(RuleBaseElementType.Process, workflow)) == null) {
+            InputStream is =
+                getClass().getClassLoader().getResourceAsStream("rulebase/org/openengsb/" + workflow + ".rf");
+            String testWorkflow = IOUtils.toString(is);
+            RuleBaseElementId id = new RuleBaseElementId(RuleBaseElementType.Process, workflow);
+            ruleManager.add(id, testWorkflow);
+            IOUtils.closeQuietly(is);
+        }
     }
 }
