70e4c92b07b34907ffe2ba5b9b27394caee5c176
Florian Motlik <flomotlik@gmail.com>
2011-03-03 10:58:32 +0100

    [OPENENGSB-1026] ITest to start workflow over JMS Signed-off-by: Florian Motlik <flomotlik@gmail.com>

--- itests/src/test/java/org/openengsb/itests/exam/JMSPortIT.java
+++ itests/src/test/java/org/openengsb/itests/exam/JMSPortIT.java
@@ -18,21 +18,38 @@ package org.openengsb.itests.exam;
 
 import static junit.framework.Assert.assertNotNull;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openengsb.core.common.communication.OutgoingPort;
 import org.openengsb.core.common.util.OsgiServiceUtils;
 import org.openengsb.itests.util.AbstractExamTestHelper;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.springframework.jms.core.JmsTemplate;
 
 @RunWith(JUnit4TestRunner.class)
 public class JMSPortIT extends AbstractExamTestHelper {
 
-    @Test
+    // @Test
     public void jmsPort_shouldBeExportedWithCorrectId() throws Exception {
         OutgoingPort serviceWithId =
             OsgiServiceUtils.getServiceWithId(getBundleContext(), OutgoingPort.class, "jms-json");
         System.out.println("ServiceID:" + serviceWithId);
         assertNotNull(serviceWithId);
+    }
+
+    @Test
+    public void startSimpleWorkflow_ShouldReturn42() {
+        System.out.println("Starting Integration Test");
+        ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory("tcp://localhost:6549");
+        JmsTemplate template = new JmsTemplate(cf);
+        // template.setReceiveTimeout(4000);
+        String request =
+            "{\"callId\":\"12345\",\"answer\":true,\"classes\":[\"java.lang.String\", \"org.openengsb.core.common.workflow.model.ProcessBag\"],"
+                    + "\"methodName\":\"executeWorkflow\",\"metaData\":{\"serviceId\":\"workflowService\"},\"args\":[\"simpleFlow\", {}]}";
+        System.out.println("Seding request");
+        template.convertAndSend("receive", request);
+        Object receiveAndConvert = template.receiveAndConvert("12345");
+        System.out.println(receiveAndConvert);
     }
 }
