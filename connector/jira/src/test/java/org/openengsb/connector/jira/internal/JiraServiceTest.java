6b423134b5b90a594f51831c9b37b491f47f8675
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-03-09 00:41:32 +0100

    update licences

--- connector/jira/src/test/java/org/openengsb/connector/jira/internal/JiraServiceTest.java
+++ connector/jira/src/test/java/org/openengsb/connector/jira/internal/JiraServiceTest.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.openengsb.connector.jira.internal;
 
 import static org.hamcrest.MatcherAssert.assertThat;
