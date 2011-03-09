6b45fe8f5a07190348ef7255a7ed7c05e49dd549
Andreas Pieber <anpieber@gmail.com>
2011-03-09 02:32:58 +0100

    [OPENENGSB-844] Adapted licenses to new model
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- ui/admin/src/test/java/org/openengsb/ui/admin/workflowEditor/event/EditEventTest.java
+++ ui/admin/src/test/java/org/openengsb/ui/admin/workflowEditor/event/EditEventTest.java
@@ -1,11 +1,12 @@
 /**
- * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
