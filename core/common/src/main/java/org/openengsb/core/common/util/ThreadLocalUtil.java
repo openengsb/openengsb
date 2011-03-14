d92de2a82a7cbeb181a0f8b33ef220cf95c61380
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-03-14 21:24:08 +0100

    [OPENENGSB-1111] also take SecurityContext into account

--- core/common/src/main/java/org/openengsb/core/common/util/ThreadLocalUtil.java
+++ core/common/src/main/java/org/openengsb/core/common/util/ThreadLocalUtil.java
@@ -24,26 +24,46 @@ import java.util.Collection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 
-import org.openengsb.core.common.context.ContextHolder;
-
 public class ThreadLocalUtil {
+
+    private static final ClassLoader CLASS_LOADER = ThreadLocalUtil.class.getClassLoader();
+    private static final Class<?>[] INTERFACES = new Class<?>[] { ExecutorService.class };
+
+    static final class ExecutorServiceHandler implements InvocationHandler {
+        private final ExecutorService original;
+
+        public ExecutorServiceHandler(ExecutorService original) {
+            this.original = original;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            String methodName = method.getName();
+            if ("submit".equals(methodName)) {
+                transformSingleInvocation(args);
+            } else if ("invokeAll".equals(methodName) || "invokeAny".equals(methodName)) {
+                transformBatchInvocation(args);
+            }
+            return method.invoke(original, args);
+        }
+    }
 
     @SuppressWarnings("unchecked")
     private static Object transform(Object arg) {
         if (arg instanceof Runnable && arg.getClass() != ContextAwareRunnable.class) {
             return new ContextAwareRunnable((Runnable) arg);
-        } else if (arg instanceof Callable<?>) {
+        } else if (arg instanceof Callable<?> && arg.getClass() != ContextAwareCallable.class) {
             return new ContextAwareCallable((Callable<Object>) arg);
         }
         return arg;
     }
 
-    public static void transformSingleInvocation(Object[] args) {
+    private static void transformSingleInvocation(Object[] args) {
         args[0] = transform(args[0]);
     }
 
     @SuppressWarnings("unchecked")
-    public static void transformBatchInvocation(Object[] args) {
+    private static void transformBatchInvocation(Object[] args) {
         Collection<?> tasks = (Collection<?>) args[0];
         Collection<Object> result;
         try {
@@ -59,53 +79,7 @@ public class ThreadLocalUtil {
         args[0] = result;
     }
 
-    static class ContextAwareRunnable implements Runnable {
-        private Runnable original;
-        private String context = ContextHolder.get().getCurrentContextId();
-
-        public ContextAwareRunnable(Runnable original) {
-            this.original = original;
-        }
-
-        @Override
-        public void run() {
-            ContextHolder.get().setCurrentContextId(context);
-            original.run();
-        }
-    }
-
-    static class ContextAwareCallable implements Callable<Object> {
-        private Callable<Object> original;
-        private String context = ContextHolder.get().getCurrentContextId();
-
-        public ContextAwareCallable(Callable<Object> original) {
-            this.original = original;
-        }
-
-        @Override
-        public Object call() throws Exception {
-            ContextHolder.get().setCurrentContextId(context);
-            return original.call();
-        }
-    }
-
     public static ExecutorService contextAwareExecutor(final ExecutorService original) {
-        return (ExecutorService) Proxy.newProxyInstance(ThreadLocalUtil.class.getClassLoader(),
-            new Class<?>[] { ExecutorService.class },
-            new InvocationHandler() {
-                private ExecutorService originalExecutor = original;
-
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    String methodName = method.getName();
-                    if ("submit".equals(methodName)) {
-                        transformSingleInvocation(args);
-                    } else if ("invokeAll".equals(methodName) || "invokeAny".equals(methodName)) {
-                        transformBatchInvocation(args);
-                    }
-                    return method.invoke(originalExecutor, args);
-                }
-
-            });
+        return (ExecutorService) Proxy.newProxyInstance(CLASS_LOADER, INTERFACES, new ExecutorServiceHandler(original));
     }
 }
