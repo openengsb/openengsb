f6e4e3a8d3dee3ee0574ed3e35c920ebf19425eb
Lukas Stampf <e0725140@student.tuwien.ac.at>
2011-04-03 13:52:41 +0200

    [OPENENGSB-698] create taskbox ui itest

--- ui/common/src/main/java/org/openengsb/ui/common/taskbox/web/TaskComparator.java
+++ ui/common/src/main/java/org/openengsb/ui/common/taskbox/web/TaskComparator.java
@@ -32,17 +32,19 @@ public class TaskComparator implements Comparator<Task> {
     @Override
     public int compare(Task arg0, Task arg1) {
         int ret = 0;
-
-        if (sp.getProperty().equals("taskId")) {
-            ret = arg0.getTaskId().compareTo(arg1.getTaskId());
-        } else if (sp.getProperty().equals("taskType")) {
-            ret = arg0.getTaskType().compareTo(arg1.getTaskType());
-        } else if (sp.getProperty().equals("description")) {
-            ret = arg0.getDescription().compareTo(arg1.getDescription());
-        } else if (sp.getProperty().equals("taskCreationTimestamp")) {
-            ret = arg0.getTaskCreationTimestamp().compareTo(arg1.getTaskCreationTimestamp());
+        try {
+            if (sp.getProperty().equals("taskId")) {
+                ret = arg0.getTaskId().compareTo(arg1.getTaskId());
+            } else if (sp.getProperty().equals("taskType")) {
+                ret = arg0.getTaskType().compareTo(arg1.getTaskType());
+            } else if (sp.getProperty().equals("description")) {
+                ret = arg0.getDescription().compareTo(arg1.getDescription());
+            } else if (sp.getProperty().equals("taskCreationTimestamp")) {
+                ret = arg0.getTaskCreationTimestamp().compareTo(arg1.getTaskCreationTimestamp());
+            }
+        } catch (NullPointerException ex) {
+            ret = -1;
         }
-
         if (!sp.isAscending()) {
             return ret * -1;
         }
