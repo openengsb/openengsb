81fa9c6c2e0023489674dc6d5a470a10f8638754
Andreas Pieber <anpieber@gmail.com>
2011-04-03 16:26:29 +0200

    [OPENENGSB-1084] Renamed DomainEndpointService to Wireing Service
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

---
+++ core/services/src/main/java/org/openengsb/core/services/internal/DefaultWireingService.java
@@ -0,0 +1,107 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.services.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openengsb.core.api.Domain;
+import org.openengsb.core.api.OsgiServiceNotAvailableException;
+import org.openengsb.core.api.WireingService;
+import org.openengsb.core.api.context.ContextHolder;
+import org.openengsb.core.common.util.OsgiServiceUtils;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.Filter;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * Default Wiring Implementation which can be overwritten by another service implementation easily if required.
+ */
+public class DefaultWireingService implements WireingService {
+
+    private static Log log = LogFactory.getLog(DefaultWireingService.class);
+
+    private static BundleContext bundleContext;
+
+    @Override
+    public <T extends Domain> T getDomainEndpoint(Class<T> domainType, String location) {
+        Filter filter = OsgiServiceUtils.getFilterForLocation(domainType, location);
+        return OsgiServiceUtils.getOsgiServiceProxy(filter, domainType);
+    }
+
+    @Override
+    public <T extends Domain> List<T> getDomainEndpoints(Class<T> domainType, String location) {
+        return getDomainEndpoints(domainType, location, ContextHolder.get().getCurrentContextId());
+    }
+
+    @Override
+    public <T extends Domain> T getDomainEndpoint(Class<T> domainType, String location, String context) {
+        Filter filter = OsgiServiceUtils.getFilterForLocation(domainType, location, context);
+        return OsgiServiceUtils.getOsgiServiceProxy(filter, domainType);
+    }
+
+    @Override
+    public <T extends Domain> List<T> getDomainEndpoints(Class<T> domainType, String location, String context) {
+        Filter filterForLocation = OsgiServiceUtils.getFilterForLocation(domainType, location);
+        ServiceReference[] allServiceReferences;
+        try {
+            allServiceReferences =
+                bundleContext.getAllServiceReferences(domainType.getName(), filterForLocation.toString());
+        } catch (InvalidSyntaxException e) {
+            // this can never happen, because the filter has been compiled before
+            throw new RuntimeException(e);
+        }
+        List<T> result = new ArrayList<T>();
+        if (allServiceReferences == null) {
+            log.info("no references found for filter: " + filterForLocation.toString());
+            return result;
+        }
+        log.debug(String.format("found %s references for %s", allServiceReferences.length, filterForLocation));
+        for (ServiceReference ref : allServiceReferences) {
+            String filterString = String.format("(%s=%s)", Constants.SERVICE_ID, ref.getProperty(Constants.SERVICE_ID));
+            try {
+                T osgiServiceProxy =
+                    OsgiServiceUtils.getOsgiServiceProxy(FrameworkUtil.createFilter(filterString), domainType);
+                result.add(osgiServiceProxy);
+            } catch (InvalidSyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean isConnectorCurrentlyPresent(Class<? extends Domain> domainType) {
+        Domain service;
+        try {
+            service = OsgiServiceUtils.getService(domainType, 5000L);
+        } catch (OsgiServiceNotAvailableException e) {
+            return false;
+        }
+        return service != null;
+    }
+
+    public void setBundleContext(BundleContext bundleContext) {
+        DefaultWireingService.bundleContext = bundleContext;
+    }
+}
