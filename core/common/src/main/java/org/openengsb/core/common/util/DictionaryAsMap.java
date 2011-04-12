37d9f00317b042d8008b0e7310b9bffb8eb9d790
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-04-12 13:56:18 +0200

    [OPENENGSB-993] add util-methods for handling dictionaries and Maps

---
+++ core/common/src/main/java/org/openengsb/core/common/util/DictionaryAsMap.java
@@ -0,0 +1,109 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.common.util;
+
+import java.util.AbstractMap;
+import java.util.AbstractSet;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A wrapper around a dictionary access it as a Map
+ */
+public class DictionaryAsMap<K, V> extends AbstractMap<K, V> {
+
+    private Dictionary<K, V> dictionary;
+
+    public DictionaryAsMap(Dictionary<K, V> dict) {
+        this.dictionary = dict;
+    }
+
+    public static <K, V> Map<K, V> wrap(Dictionary<K, V> dictionary) {
+        if (dictionary instanceof MapAsDictionary) {
+            return ((MapAsDictionary<K, V>) dictionary).getMap();
+        }
+        return new DictionaryAsMap<K, V>(dictionary);
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return new AbstractSet<Entry<K, V>>() {
+            @Override
+            public Iterator<Entry<K, V>> iterator() {
+                final Enumeration<K> e = dictionary.keys();
+                return new Iterator<Entry<K, V>>() {
+                    private K key;
+
+                    public boolean hasNext() {
+                        return e.hasMoreElements();
+                    }
+
+                    public Entry<K, V> next() {
+                        key = e.nextElement();
+                        return new KeyEntry(key);
+                    }
+
+                    public void remove() {
+                        if (key == null) {
+                            throw new IllegalStateException();
+                        }
+                        dictionary.remove(key);
+                    }
+                };
+            }
+
+            @Override
+            public int size() {
+                return dictionary.size();
+            }
+        };
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return dictionary.put(key, value);
+    }
+
+    class KeyEntry implements Map.Entry<K, V> {
+
+        private final K key;
+
+        KeyEntry(K key) {
+            this.key = key;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return dictionary.get(key);
+        }
+
+        public V setValue(V value) {
+            return DictionaryAsMap.this.put(key, value);
+        }
+    }
+
+    public Dictionary<K, V> getDictionary() {
+        return this.dictionary;
+    }
+}
---
+++ core/common/src/main/java/org/openengsb/core/common/util/MapAsDictionary.java
@@ -0,0 +1,108 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.common.util;
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MapAsDictionary<K, V> extends Dictionary<K, V> {
+
+    private Map<K, V> map;
+
+    public MapAsDictionary(Map<K, V> map) {
+        this.map = map;
+    }
+
+    public static <K, V> Dictionary<K, V> wrap(Map<K, V> map) {
+        if (map instanceof DictionaryAsMap) {
+            return ((DictionaryAsMap<K, V>) map).getDictionary();
+        }
+        return new MapAsDictionary<K, V>(map);
+    }
+
+    public void setSourceMap(Map<K, V> map) {
+        this.map = map;
+    }
+
+    @Override
+    public Enumeration<V> elements() {
+        return new IteratorToEnumeration<V>(map.values().iterator());
+    }
+
+    @Override
+    public V get(Object key) {
+        return map.get(key);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public Enumeration<K> keys() {
+        return new IteratorToEnumeration<K>(map.keySet().iterator());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public V put(Object key, Object value) {
+        return map.put((K) key, (V) value);
+    }
+
+    @Override
+    public V remove(Object key) {
+        return map.remove(key);
+    }
+
+    @Override
+    public int size() {
+        if (map == null) {
+            return 0;
+        }
+        return map.size();
+    }
+
+    public Map<K, V> getMap() {
+        return this.map;
+    }
+
+    class IteratorToEnumeration<T> implements Enumeration<T> {
+        private final Iterator<T> iter;
+
+        public IteratorToEnumeration(Iterator<T> iter) {
+            this.iter = iter;
+        }
+
+        public boolean hasMoreElements() {
+            if (iter == null) {
+                return false;
+            }
+            return iter.hasNext();
+        }
+
+        public T nextElement() {
+            if (iter == null) {
+                return null;
+            }
+            return iter.next();
+        }
+    }
+}
