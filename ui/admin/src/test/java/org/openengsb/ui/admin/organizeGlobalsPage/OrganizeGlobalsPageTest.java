6dc83160c2d15327fb7c5176a32cea305f36c01b
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2011-04-18 16:03:41 +0200

    [OPENENGSB-1228] added testcases for the organizeGlobalsPage

---
+++ ui/admin/src/test/java/org/openengsb/ui/admin/organizeGlobalsPage/OrganizeGlobalsPageTest.java
@@ -0,0 +1,160 @@
+package org.openengsb.ui.admin.organizeGlobalsPage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.tree.LinkTree;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.apache.wicket.spring.injection.annot.test.AnnotApplicationContextMock;
+import org.apache.wicket.util.tester.FormTester;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.openengsb.core.api.context.ContextCurrentService;
+import org.openengsb.core.api.workflow.RuleBaseException;
+import org.openengsb.core.api.workflow.RuleManager;
+import org.openengsb.ui.admin.model.OpenEngSBVersion;
+
+public class OrganizeGlobalsPageTest {
+    private WicketTester tester;
+    private RuleManager ruleManager;
+    private Map<String, String> globals;
+
+    @Before
+    public void init() throws RuleBaseException {
+        tester = new WicketTester();
+        AnnotApplicationContextMock appContext = new AnnotApplicationContextMock();
+        ContextCurrentService contextService = mock(ContextCurrentService.class);
+        appContext.putBean(contextService);
+        appContext.putBean("openengsbVersion", new OpenEngSBVersion());
+        ruleManager = mock(RuleManager.class);
+        appContext.putBean(ruleManager);
+        tester.getApplication()
+            .addComponentInstantiationListener(new SpringComponentInjector(tester.getApplication(), appContext, false));
+
+        globals = new TreeMap<String, String>();
+        globals.put("glob1", "aaaa.bbbb.ccc");
+        globals.put("glob2", "aaaa.bbbb.ddd");
+        globals.put("glob3", "aaaa.bbbb.eee");
+
+        when(ruleManager.listGlobals()).thenReturn(globals);
+        doThrow(new RuleBaseException()).when(ruleManager).addGlobal("test", "test");
+        doThrow(new RuleBaseException()).when(ruleManager).removeGlobal("test");
+        doAnswer(new Answer<Object>() {
+            public Object answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                globals.put((String) args[1], (String) args[0]);
+                return null;
+            }
+        })
+            .when(ruleManager).addGlobal("glob4", "aaaa.bbbb.fff");
+        
+        doAnswer(new Answer<Object>() {
+            public Object answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                globals.put((String) args[1], (String) args[0]);
+                return null;
+            }
+        })
+            .when(ruleManager).addGlobal("glob4", "aaaa.bbbb.ffff");
+        
+        doAnswer(new Answer<Object>() {
+            public Object answer(InvocationOnMock invocation) {
+                globals.remove("glob4");
+                return null;
+            }
+        })
+            .when(ruleManager).removeGlobal("glob4");
+
+        tester.startPage(OrganizeGlobalsPage.class);
+        verify(ruleManager).listGlobals();
+    }
+
+    @Test
+    public void renderOrganizeGlobalsPage() throws Exception {
+        tester.assertRenderedPage(OrganizeGlobalsPage.class);
+        tester.assertComponent("tree", LinkTree.class);
+        tester.assertComponent("editForm", Form.class);
+        tester.assertComponent("editForm:globalName", TextField.class);
+        tester.assertComponent("editForm:className", TextField.class);
+        tester.assertComponent("editForm:submitButton", AjaxButton.class);
+        tester.assertComponent("editForm:deleteButton", AjaxButton.class);
+
+        LinkTree tree = (LinkTree) tester.getComponentFromLastRenderedPage("tree");
+        assertEquals(globals.size(), tree.getModelObject().getChildCount(tree.getModelObject().getRoot()));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testSelectGlobal() throws Exception {
+        tester.clickLink("tree:i:2:nodeComponent:contentLink", true);
+        TextField<String> globalName =
+            (TextField<String>) tester.getComponentFromLastRenderedPage("editForm:globalName");
+        assertTrue(globalName.isEnabled());
+        assertEquals("glob2", globalName.getModelObject());
+        TextField<String> className =
+            (TextField<String>) tester.getComponentFromLastRenderedPage("editForm:className");
+        assertTrue(className.isEnabled());
+        assertEquals("aaaa.bbbb.ddd", className.getModelObject());
+    }
+
+    @Test
+    public void testAddDoubleGlobal() throws Exception {
+        FormTester formTester = tester.newFormTester("editForm");
+        formTester.setValue("globalName", "test");
+        formTester.setValue("className", "test");
+        formTester.submit("submitButton");
+
+        LinkTree tree = (LinkTree) tester.getComponentFromLastRenderedPage("tree");
+        assertEquals(globals.size(), tree.getModelObject().getChildCount(tree.getModelObject().getRoot()));
+    }
+    
+    @Test
+    public void testAddNewGlobal() throws Exception {
+        FormTester formTester = tester.newFormTester("editForm");
+        formTester.setValue("globalName", "glob4");
+        formTester.setValue("className", "aaaa.bbbb.fff");
+        formTester.submit("submitButton");
+
+        LinkTree tree = (LinkTree) tester.getComponentFromLastRenderedPage("tree");
+        assertEquals(globals.size(), tree.getModelObject().getChildCount(tree.getModelObject().getRoot()));
+    }
+    
+    @Test
+    public void testDeleteGlobal() throws Exception {
+        FormTester formTester = tester.newFormTester("editForm");
+        formTester.setValue("globalName", "glob4");
+        formTester.setValue("className", "aaaa.bbbb.fff");
+        formTester.submit("deleteButton");
+
+        LinkTree tree = (LinkTree) tester.getComponentFromLastRenderedPage("tree");
+        assertEquals(globals.size(), tree.getModelObject().getChildCount(tree.getModelObject().getRoot()));
+    }
+    
+    @Test
+    public void testDeleteNotExistingGlobal() throws Exception {
+        FormTester formTester = tester.newFormTester("editForm");
+        formTester.setValue("globalName", "test");
+        formTester.setValue("className", "test");
+        formTester.submit("deleteButton");
+
+        LinkTree tree = (LinkTree) tester.getComponentFromLastRenderedPage("tree");
+        assertEquals(globals.size(), tree.getModelObject().getChildCount(tree.getModelObject().getRoot()));
+    }
+    
+    
+
+}
