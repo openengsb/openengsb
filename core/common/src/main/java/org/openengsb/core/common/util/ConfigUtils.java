5f7bdd02714767e135bcce35e611eea4838ab2fe
Andreas Pieber <anpieber@gmail.com>
2011-04-23 19:04:25 +0200

    [OPENENGSB-1333] Fixed license errors
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- core/common/src/main/java/org/openengsb/core/common/util/ConfigUtils.java
+++ core/common/src/main/java/org/openengsb/core/common/util/ConfigUtils.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openengsb.core.common.util;
 
 import java.util.HashMap;
@@ -13,7 +30,7 @@ public final class ConfigUtils {
 
     /**
      * apply the differences to the given map (3-way-merge)
-     * 
+     *
      * @throws MergeException if the changes cannot be applied because of merge-conflicts
      */
     public static <K, V> Map<K, V> updateMap(final Map<K, V> original, MapDifference<K, V> diff) throws MergeException {
