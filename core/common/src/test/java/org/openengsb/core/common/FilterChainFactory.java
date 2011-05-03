7c9e6f43c51b10ec5f1c5283d205e0732c8afc40
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-05-03 09:02:48 +0200

    [OPENENGSB-1482] add somewhat matured idea of the API

---
+++ core/common/src/test/java/org/openengsb/core/common/FilterChainFactory.java
@@ -0,0 +1,43 @@
+package org.openengsb.core.common;
+
+import org.openengsb.core.api.remote.AbstractFilterAction;
+import org.openengsb.core.api.remote.FilterAction;
+import org.openengsb.core.api.remote.FilterChainElement;
+import org.openengsb.core.api.remote.FilterException;
+
+import com.google.common.base.Preconditions;
+
+public final class FilterChainFactory {
+
+    private static final class FilterChain<InputType, OutputType> extends AbstractFilterAction<InputType, OutputType> {
+        private FilterChainElement<InputType, OutputType> first;
+
+        private FilterChain(Class<InputType> inputType, Class<OutputType> outputType,
+                FilterChainElement<InputType, OutputType> first) {
+            super(inputType, outputType);
+            this.first = first;
+        }
+
+        @Override
+        public OutputType apply(InputType input) throws FilterException {
+            return first.apply(input);
+        }
+    }
+
+    public static <InputType, OutputType> FilterAction<InputType, OutputType> build(Class<InputType> inputType,
+            Class<OutputType> outputType, FilterChainElement<?, ?>... filters) {
+        Preconditions.checkNotNull(filters);
+        Preconditions.checkArgument(filters.length > 0);
+        @SuppressWarnings("unchecked")
+        FilterChainElement<InputType, OutputType> first = (FilterChainElement<InputType, OutputType>) filters[0];
+        Preconditions.checkArgument(first.getSupportedInputType().isAssignableFrom(inputType));
+        Preconditions.checkArgument(first.getSupportedOutputType().isAssignableFrom(outputType));
+        for (int i = 1; i < filters.length; i++) {
+            filters[i - 1].setNext(filters[i]);
+        }
+        return new FilterChain<InputType, OutputType>(inputType, outputType, first);
+    }
+
+    private FilterChainFactory() {
+    }
+}
