be7b433826c7ff1c524c125ddd263753f812a078
Alexander Pucher <engsb@alexpucher.com>
2011-05-05 01:09:07 +0200

    [OPENENGSB-1514] added ci tutorial project to examples
    Signed-off-by: Alexander Pucher <engsb@alexpucher.com>

---
+++ docs/examples/src/main/citutorial/src/main/java/org/openengsb/Main.java
@@ -0,0 +1,11 @@
+package org.openengsb;
+
+/*
+ * This is a sample application application.
+ */
+
+public class Main {
+  public static void main(String[] argv) {
+    System.out.println("Hello World!");
+  }
+}
