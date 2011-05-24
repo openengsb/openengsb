5570ed25372519eedec32f8f35367130ce3b2407
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-05-24 12:28:08 +0200

    [OPENENGSB-1226] add failing authentication-itest

--- itests/src/test/java/org/openengsb/itests/exam/JMSPortIT.java
+++ itests/src/test/java/org/openengsb/itests/exam/JMSPortIT.java
@@ -34,9 +34,11 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openengsb.core.api.remote.OutgoingPort;
+import org.openengsb.core.api.security.EncryptionException;
 import org.openengsb.core.api.workflow.RuleBaseException;
 import org.openengsb.core.api.workflow.RuleManager;
 import org.openengsb.core.api.workflow.model.RuleBaseElementId;
@@ -50,6 +52,24 @@ import org.springframework.jms.core.JmsTemplate;
 @RunWith(JUnit4TestRunner.class)
 public class JMSPortIT extends AbstractExamTestHelper {
 
+    private static final String METHOD_CALL_STRING = ""
+            + "{"
+            + "    \"classes\": ["
+            + "        \"java.lang.String\","
+            + "        \"org.openengsb.core.api.workflow.model.ProcessBag\""
+            + "    ],"
+            + "    \"methodName\": \"executeWorkflow\","
+            + "    \"metaData\": {"
+            + "        \"serviceId\": \"workflowService\","
+            + "        \"contextId\": \"foo\""
+            + "    },"
+            + "    \"args\": ["
+            + "        \"simpleFlow\","
+            + "        {"
+            + "        }"
+            + "    ]"
+            + "}";
+
     private RuleManager ruleManager;
 
     @Before
@@ -57,6 +77,7 @@ public class JMSPortIT extends AbstractExamTestHelper {
         ruleManager = getOsgiService(RuleManager.class);
     }
 
+    @Ignore
     @Test
     public void jmsPort_shouldBeExportedWithCorrectId() throws Exception {
         OutgoingPort serviceWithId =
@@ -64,30 +85,65 @@ public class JMSPortIT extends AbstractExamTestHelper {
         assertNotNull(serviceWithId);
     }
 
+    @Ignore
     @Test
     public void startSimpleWorkflow_ShouldReturn42() throws Exception {
         addWorkflow("simpleFlow");
         ActiveMQConnectionFactory cf =
             new ActiveMQConnectionFactory("failover:(tcp://localhost:6549)?timeout=60000");
         JmsTemplate template = new JmsTemplate(cf);
-        String methodCall = ""
-                + "{"
-                + "    \"classes\": ["
-                + "        \"java.lang.String\","
-                + "        \"org.openengsb.core.api.workflow.model.ProcessBag\""
-                + "    ],"
-                + "    \"methodName\": \"executeWorkflow\","
-                + "    \"metaData\": {"
-                + "        \"serviceId\": \"workflowService\","
-                + "        \"contextId\": \"foo\""
-                + "    },"
-                + "    \"args\": ["
-                + "        \"simpleFlow\","
-                + "        {"
-                + "        }"
-                + "    ]"
-                + "}";
+        String secureRequest = prepareRequest(METHOD_CALL_STRING, "admin", "password");
+        SecretKey sessionKey = generateSessionKey();
+        String encryptedMessage = encryptMessage(secureRequest, sessionKey);
 
+        template.convertAndSend("receive", encryptedMessage);
+        String result = (String) template.receiveAndConvert("12345");
+
+        byte[] resultData = Base64.decodeBase64(result);
+        assertThat(result, not(containsString("The answer to life the universe and everything")));
+        byte[] decryptedResultData = CipherUtils.decrypt(resultData, sessionKey);
+        result = new String(decryptedResultData);
+        assertThat(result, containsString("The answer to life the universe and everything"));
+    }
+
+    @Test
+    public void testSendMethodCallWithWrongAuthentication_shouldFail() throws Exception {
+        addWorkflow("simpleFlow");
+        ActiveMQConnectionFactory cf =
+            new ActiveMQConnectionFactory("failover:(tcp://localhost:6549)?timeout=60000");
+        JmsTemplate template = new JmsTemplate(cf);
+        String secureRequest = prepareRequest(METHOD_CALL_STRING, "admin", "wrong-password");
+        SecretKey sessionKey = generateSessionKey();
+        String encryptedMessage = encryptMessage(secureRequest, sessionKey);
+
+        template.convertAndSend("receive", encryptedMessage);
+        String result = (String) template.receiveAndConvert("12345");
+
+        assertThat(result, containsString("Exception"));
+        assertThat(result, not(containsString("The answer to life the universe and everything")));
+    }
+
+    private String encryptMessage(String secureRequest, SecretKey sessionKey)
+        throws EncryptionException, InterruptedException, IOException {
+        PublicKey publicKey = getPublicKeyFromConfigFile();
+        String encodedMessage = Base64.encodeBase64String(CipherUtils.encrypt(secureRequest.getBytes(), sessionKey));
+        String encodedKey = Base64.encodeBase64String(CipherUtils.encrypt(sessionKey.getEncoded(), publicKey));
+
+        String encryptedMessage = ""
+                + "{"
+                + "  \"encryptedContent\":\"" + encodedMessage + "\","
+                + "  \"encryptedKey\":\"" + encodedKey + "\""
+                + "}";
+        return encryptedMessage;
+    }
+
+    private SecretKey generateSessionKey() {
+        SecretKey sessionKey =
+            CipherUtils.generateKey(CipherUtils.DEFAULT_SYMMETRIC_ALGORITHM, CipherUtils.DEFAULT_SYMMETRIC_KEYSIZE);
+        return sessionKey;
+    }
+
+    private String prepareRequest(String methodCall, String username, String password) {
         String request = ""
                 + "{"
                 + "  \"callId\":\"12345\","
@@ -100,8 +156,8 @@ public class JMSPortIT extends AbstractExamTestHelper {
                 + "  \"className\":\"org.openengsb.core.api.security.model.UsernamePasswordAuthenticationInfo\","
                 + "  \"data\":"
                 + "  {"
-                + "    \"username\":\"admin\","
-                + "    \"password\":\"password\""
+                + "    \"username\":\"" + username + "\","
+                + "    \"password\":\"" + password + "\""
                 + "  }"
                 + "}";
 
@@ -111,39 +167,20 @@ public class JMSPortIT extends AbstractExamTestHelper {
                 + "  \"timestamp\":" + System.currentTimeMillis() + ","
                 + "  \"message\":" + request
                 + "}";
+        return secureRequest;
+    }
 
-        SecretKey sessionKey =
-            CipherUtils.generateKey(CipherUtils.DEFAULT_SYMMETRIC_ALGORITHM, CipherUtils.DEFAULT_SYMMETRIC_KEYSIZE);
-
-        // TODO do this properly when OPENENGSB-1597 is resolved
+    private PublicKey getPublicKeyFromConfigFile() throws InterruptedException, IOException {
+        // FIXME do this properly when OPENENGSB-1597 is resolved
         File file = new File("target/karaf.home/etc/security/public.key.data");
         while (!file.exists()) {
             Thread.sleep(1000);
         }
         byte[] keyData;
-        try {
-            keyData = FileUtils.readFileToByteArray(file);
-        } catch (IOException e) {
-            throw new Exception(file.getAbsolutePath(), e);
-        }
+
+        keyData = FileUtils.readFileToByteArray(file);
         PublicKey publicKey = CipherUtils.deserializePublicKey(keyData, CipherUtils.DEFAULT_ASYMMETRIC_ALGORITHM);
-
-        String encodedMessage = Base64.encodeBase64String(CipherUtils.encrypt(secureRequest.getBytes(), sessionKey));
-        String encodedKey = Base64.encodeBase64String(CipherUtils.encrypt(sessionKey.getEncoded(), publicKey));
-
-        String encryptedMessage = ""
-                + "{"
-                + "  \"encryptedContent\":\"" + encodedMessage + "\","
-                + "  \"encryptedKey\":\"" + encodedKey + "\""
-                + "}";
-
-        template.convertAndSend("receive", encryptedMessage);
-        String result = (String) template.receiveAndConvert("12345");
-        byte[] resultData = Base64.decodeBase64(result);
-        assertThat(result, not(containsString("The answer to life the universe and everything")));
-        byte[] decryptedResultData = CipherUtils.decrypt(resultData, sessionKey);
-        result = new String(decryptedResultData);
-        assertThat(result, containsString("The answer to life the universe and everything"));
+        return publicKey;
     }
 
     private void addWorkflow(String workflow) throws IOException, RuleBaseException {
