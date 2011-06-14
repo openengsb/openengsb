fd12fd389ba468a339a515c5506c6236e9c2c068
Florian Motlik <flomotlik@gmail.com>
2011-06-14 18:11:17 +0200

    Fixing checkstyle error
    Signed-off-by: Florian Motlik <flomotlik@gmail.com>

--- ui/admin/src/test/java/org/openengsb/ui/admin/workflowEditor/WorkflowEditorTest.java
+++ ui/admin/src/test/java/org/openengsb/ui/admin/workflowEditor/WorkflowEditorTest.java
@@ -18,6 +18,7 @@
 package org.openengsb.ui.admin.workflowEditor;
 
 import static junit.framework.Assert.assertNull;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
