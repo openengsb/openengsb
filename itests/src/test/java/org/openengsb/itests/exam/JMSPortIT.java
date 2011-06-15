ad7a052058a90fe85455f6f0bf4312e89d9323c0
Andreas Pieber <anpieber@gmail.com>
2011-06-16 01:33:30 +0200

    [OPENENGSB-1599] Added integation test for ports-ws to avoid regressions
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- itests/src/test/java/org/openengsb/itests/exam/JMSPortIT.java
+++ itests/src/test/java/org/openengsb/itests/exam/JMSPortIT.java
@@ -25,74 +25,23 @@ import static org.ops4j.pax.exam.CoreOptions.maven;
 import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.security.PublicKey;
 
 import javax.crypto.SecretKey;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openengsb.core.api.remote.OutgoingPort;
-import org.openengsb.core.api.security.EncryptionException;
-import org.openengsb.core.api.workflow.RuleBaseException;
-import org.openengsb.core.api.workflow.RuleManager;
-import org.openengsb.core.api.workflow.model.RuleBaseElementId;
-import org.openengsb.core.api.workflow.model.RuleBaseElementType;
 import org.openengsb.core.common.OpenEngSBCoreServices;
-import org.openengsb.core.security.CipherUtils;
-import org.openengsb.itests.util.AbstractExamTestHelper;
+import org.openengsb.itests.util.AbstractRemoteTestHelper;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.springframework.jms.core.JmsTemplate;
 
 @RunWith(JUnit4TestRunner.class)
-public class JMSPortIT extends AbstractExamTestHelper {
-
-    private static final String METHOD_CALL_STRING = ""
-            + "{"
-            + "    \"classes\": ["
-            + "        \"java.lang.String\","
-            + "        \"org.openengsb.core.api.workflow.model.ProcessBag\""
-            + "    ],"
-            + "    \"methodName\": \"executeWorkflow\","
-            + "    \"metaData\": {"
-            + "        \"serviceId\": \"workflowService\","
-            + "        \"contextId\": \"foo\""
-            + "    },"
-            + "    \"args\": ["
-            + "        \"simpleFlow\","
-            + "        {"
-            + "        }"
-            + "    ]"
-            + "}";
-
-    private static final String METHOD_CALL_STRING_FILTER = ""
-            + "{"
-            + "    \"classes\": ["
-            + "        \"java.lang.String\","
-            + "        \"org.openengsb.core.api.workflow.model.ProcessBag\""
-            + "    ],"
-            + "    \"methodName\": \"executeWorkflow\","
-            + "    \"metaData\": {"
-            + "        \"serviceFilter\": \"(objectClass=org.openengsb.core.api.workflow.WorkflowService)\","
-            + "        \"contextId\": \"foo\""
-            + "    },"
-            + "    \"args\": ["
-            + "        \"simpleFlow\","
-            + "        {"
-            + "        }"
-            + "    ]"
-            + "}";
-
-    private RuleManager ruleManager;
+public class JMSPortIT extends AbstractRemoteTestHelper {
 
     @Configuration
     public final Option[] configureJms() {
@@ -101,143 +50,63 @@ public class JMSPortIT extends AbstractExamTestHelper {
                 .versionAsInProject(), "openengsb-ports-jms"));
     }
 
-    @Before
-    public void setUp() throws Exception {
-        ruleManager = getOsgiService(RuleManager.class);
-    }
-
     @Test
     public void jmsPort_shouldBeExportedWithCorrectId() throws Exception {
         OutgoingPort serviceWithId =
             OpenEngSBCoreServices.getServiceUtilsService().getServiceWithId(OutgoingPort.class, "jms-json", 60000);
+
         assertNotNull(serviceWithId);
     }
 
     @Test
     public void startSimpleWorkflow_ShouldReturn42() throws Exception {
-        addWorkflow("simpleFlow");
-        ActiveMQConnectionFactory cf =
-            new ActiveMQConnectionFactory("failover:(tcp://localhost:6549)?timeout=60000");
-        JmsTemplate template = new JmsTemplate(cf);
+        JmsTemplate template = prepareActiveMqConnection();
         String secureRequest = prepareRequest(METHOD_CALL_STRING, "admin", "password");
         SecretKey sessionKey = generateSessionKey();
         String encryptedMessage = encryptMessage(secureRequest, sessionKey);
 
-        template.convertAndSend("receive", encryptedMessage);
-        String result = (String) template.receiveAndConvert("12345");
+        String result = sendMessage(template, encryptedMessage);
 
-        byte[] resultData = Base64.decodeBase64(result);
-        assertThat(result, not(containsString("The answer to life the universe and everything")));
-        byte[] decryptedResultData = CipherUtils.decrypt(resultData, sessionKey);
-        result = new String(decryptedResultData);
-        assertThat(result, containsString("The answer to life the universe and everything"));
+        verifyEncryptedResult(sessionKey, result);
     }
-    
+
     @Test
     public void startSimpleWorkflowWithFilterMethodCall_ShouldReturn42() throws Exception {
-        addWorkflow("simpleFlow");
-        ActiveMQConnectionFactory cf =
-            new ActiveMQConnectionFactory("failover:(tcp://localhost:6549)?timeout=60000");
-        JmsTemplate template = new JmsTemplate(cf);
+        JmsTemplate template = prepareActiveMqConnection();
         String secureRequest = prepareRequest(METHOD_CALL_STRING_FILTER, "admin", "password");
         SecretKey sessionKey = generateSessionKey();
         String encryptedMessage = encryptMessage(secureRequest, sessionKey);
 
-        template.convertAndSend("receive", encryptedMessage);
-        String result = (String) template.receiveAndConvert("12345");
+        String result = sendMessage(template, encryptedMessage);
 
-        byte[] resultData = Base64.decodeBase64(result);
-        assertThat(result, not(containsString("The answer to life the universe and everything")));
-        byte[] decryptedResultData = CipherUtils.decrypt(resultData, sessionKey);
-        result = new String(decryptedResultData);
-        assertThat(result, containsString("The answer to life the universe and everything"));
+        verifyEncryptedResult(sessionKey, result);
     }
 
     @Test
     public void testSendMethodCallWithWrongAuthentication_shouldFail() throws Exception {
-        addWorkflow("simpleFlow");
-        ActiveMQConnectionFactory cf =
-            new ActiveMQConnectionFactory("failover:(tcp://localhost:6549)?timeout=60000");
-        JmsTemplate template = new JmsTemplate(cf);
+        JmsTemplate template = prepareActiveMqConnection();
         String secureRequest = prepareRequest(METHOD_CALL_STRING, "admin", "wrong-password");
         SecretKey sessionKey = generateSessionKey();
         String encryptedMessage = encryptMessage(secureRequest, sessionKey);
 
-        template.convertAndSend("receive", encryptedMessage);
-        String result = (String) template.receiveAndConvert("12345");
+        String result = sendMessage(template, encryptedMessage);
 
         assertThat(result, containsString("Exception"));
         assertThat(result, not(containsString("The answer to life the universe and everything")));
     }
 
-    private String encryptMessage(String secureRequest, SecretKey sessionKey)
-        throws EncryptionException, InterruptedException, IOException {
-        PublicKey publicKey = getPublicKeyFromConfigFile();
-        String encodedMessage = Base64.encodeBase64String(CipherUtils.encrypt(secureRequest.getBytes(), sessionKey));
-        String encodedKey = Base64.encodeBase64String(CipherUtils.encrypt(sessionKey.getEncoded(), publicKey));
-
-        String encryptedMessage = ""
-                + "{"
-                + "  \"encryptedContent\":\"" + encodedMessage + "\","
-                + "  \"encryptedKey\":\"" + encodedKey + "\""
-                + "}";
-        return encryptedMessage;
+    private String sendMessage(JmsTemplate template, String encryptedMessage) {
+        template.convertAndSend("receive", encryptedMessage);
+        String result = (String) template.receiveAndConvert("12345");
+        return result;
     }
 
-    private SecretKey generateSessionKey() {
-        SecretKey sessionKey =
-            CipherUtils.generateKey(CipherUtils.DEFAULT_SYMMETRIC_ALGORITHM, CipherUtils.DEFAULT_SYMMETRIC_KEYSIZE);
-        return sessionKey;
+    private JmsTemplate prepareActiveMqConnection() throws IOException {
+        addWorkflow("simpleFlow");
+        ActiveMQConnectionFactory cf =
+            new ActiveMQConnectionFactory("failover:(tcp://localhost:6549)?timeout=60000");
+        JmsTemplate template = new JmsTemplate(cf);
+        return template;
     }
 
-    private String prepareRequest(String methodCall, String username, String password) {
-        String request = ""
-                + "{"
-                + "  \"callId\":\"12345\","
-                + "  \"answer\":true,"
-                + "  \"methodCall\":" + methodCall
-                + "}";
-
-        String authInfo = ""
-                + "{"
-                + "  \"className\":\"org.openengsb.core.api.security.model.UsernamePasswordAuthenticationInfo\","
-                + "  \"data\":"
-                + "  {"
-                + "    \"username\":\"" + username + "\","
-                + "    \"password\":\"" + password + "\""
-                + "  }"
-                + "}";
-
-        String secureRequest = ""
-                + "{"
-                + "  \"authenticationData\":" + authInfo + ","
-                + "  \"timestamp\":" + System.currentTimeMillis() + ","
-                + "  \"message\":" + request
-                + "}";
-        return secureRequest;
-    }
-
-    private PublicKey getPublicKeyFromConfigFile() throws InterruptedException, IOException {
-        // FIXME do this properly when OPENENGSB-1597 is resolved
-        File file = new File(System.getProperty("karaf.home"), "/etc/keys/public.key.data");
-        while (!file.exists()) {
-            Thread.sleep(1000);
-        }
-        byte[] keyData;
-
-        keyData = FileUtils.readFileToByteArray(file);
-        PublicKey publicKey = CipherUtils.deserializePublicKey(keyData, CipherUtils.DEFAULT_ASYMMETRIC_ALGORITHM);
-        return publicKey;
-    }
-
-    private void addWorkflow(String workflow) throws IOException, RuleBaseException {
-        if (ruleManager.get(new RuleBaseElementId(RuleBaseElementType.Process, workflow)) == null) {
-            InputStream is =
-                getClass().getClassLoader().getResourceAsStream("rulebase/org/openengsb/" + workflow + ".rf");
-            String testWorkflow = IOUtils.toString(is);
-            RuleBaseElementId id = new RuleBaseElementId(RuleBaseElementType.Process, workflow);
-            ruleManager.add(id, testWorkflow);
-            IOUtils.closeQuietly(is);
-        }
-    }
 }
---
+++ itests/src/test/java/org/openengsb/itests/exam/WSPortIT.java
@@ -0,0 +1,157 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.itests.exam;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
+
+import java.io.ByteArrayInputStream;
+import java.net.URL;
+
+import javax.crypto.SecretKey;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Service;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openengsb.core.api.remote.OutgoingPort;
+import org.openengsb.core.common.OpenEngSBCoreServices;
+import org.openengsb.itests.util.AbstractRemoteTestHelper;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.w3c.dom.Document;
+
+@RunWith(JUnit4TestRunner.class)
+public class WSPortIT extends AbstractRemoteTestHelper {
+
+    @Configuration
+    public final Option[] configureWs() {
+        return options(scanFeatures(
+            maven().groupId("org.openengsb").artifactId("openengsb").type("xml").classifier("features-itests")
+                .versionAsInProject(), "openengsb-ports-ws"));
+    }
+
+    @Test
+    public void jmsPort_shouldBeExportedWithCorrectId() throws Exception {
+        OutgoingPort serviceWithId =
+            OpenEngSBCoreServices.getServiceUtilsService().getServiceWithId(OutgoingPort.class, "ws-json", 60000);
+        assertNotNull(serviceWithId);
+    }
+
+    @Test
+    public void startSimpleWorkflow_ShouldReturn42() throws Exception {
+        Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+        Dispatch<DOMSource> dispatcher = createMessageDispatcher();
+        String secureRequest = prepareRequest(METHOD_CALL_STRING, "admin", "password");
+        SecretKey sessionKey = generateSessionKey();
+        String encryptedMessage = encryptMessage(secureRequest, sessionKey);
+        DOMSource request = convertMessageToDomSource(encryptedMessage);
+
+        DOMSource response = dispatchMessage(dispatcher, request);
+        String result = transformResponseToMessage(response);
+
+        verifyEncryptedResult(sessionKey, result);
+    }
+
+    @Test
+    public void startSimpleWorkflowWithFilterMethodCall_ShouldReturn42() throws Exception {
+        Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+        Dispatch<DOMSource> dispatcher = createMessageDispatcher();
+        String secureRequest = prepareRequest(METHOD_CALL_STRING_FILTER, "admin", "password");
+        SecretKey sessionKey = generateSessionKey();
+        String encryptedMessage = encryptMessage(secureRequest, sessionKey);
+        DOMSource request = convertMessageToDomSource(encryptedMessage);
+
+        DOMSource response = dispatchMessage(dispatcher, request);
+        String result = transformResponseToMessage(response);
+
+        verifyEncryptedResult(sessionKey, result);
+    }
+
+    @Test
+    public void testSendMethodCallWithWrongAuthentication_shouldFail() throws Exception {
+        Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+        Dispatch<DOMSource> dispatcher = createMessageDispatcher();
+        String secureRequest = prepareRequest(METHOD_CALL_STRING, "admin", "wrong-password");
+        SecretKey sessionKey = generateSessionKey();
+        String encryptedMessage = encryptMessage(secureRequest, sessionKey);
+        DOMSource request = convertMessageToDomSource(encryptedMessage);
+
+        DOMSource response = dispatchMessage(dispatcher, request);
+        String result = transformResponseToMessage(response);
+
+        assertThat(result, containsString("Exception"));
+        assertThat(result, not(containsString("The answer to life the universe and everything")));
+    }
+
+    private Dispatch<DOMSource> createMessageDispatcher() throws Exception {
+        addWorkflow("simpleFlow");
+        QName serviceName = new QName("http://ws.ports.openengsb.org/", "PortReceiverService");
+        Service service = Service.create(new URL("http://localhost:8091/ws/receiver/?wsdl"), serviceName);
+        QName portName = new QName("http://ws.ports.openengsb.org/", "PortReceiverPort");
+        Dispatch<DOMSource> disp = service.createDispatch(portName, DOMSource.class, Service.Mode.MESSAGE);
+        return disp;
+    }
+
+    private DOMSource convertMessageToDomSource(String messageToTransport) throws Exception {
+        String message =
+            "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
+                    "xmlns:ws=\"http://ws.ports.openengsb.org/\" >" +
+                    "<soapenv:Header/>" +
+                    "<soapenv:Body>" +
+                    "<ws:receive>" +
+                    "<arg0>" + messageToTransport + "</arg0>" +
+                    "</ws:receive>" +
+                    "</soapenv:Body>" +
+                    "</soapenv:Envelope>";
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        DocumentBuilder db = factory.newDocumentBuilder();
+        Document requestDoc = db.parse(new ByteArrayInputStream(message.getBytes()));
+        DOMSource request = new DOMSource(requestDoc);
+        return request;
+    }
+
+    private DOMSource dispatchMessage(Dispatch<DOMSource> dispatcher, DOMSource request) {
+        DOMSource response = dispatcher.invoke(request);
+        return response;
+    }
+
+    private String transformResponseToMessage(DOMSource response) throws Exception {
+        Document document = (Document) response.getNode();
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        XPathExpression expression = xpath.compile("//return/text()");
+        String result = (String) expression.evaluate(document, XPathConstants.STRING);
+        return result;
+    }
+
+}
