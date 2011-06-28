2e69d610a451d8ea620944831db04e82abe8f6b6
Fabian Souczek <e0728541@student.tuwien.ac.at>
2011-06-28 12:23:28 +0200

    [OPENENGSB-781] change the way getting the springSecurityFilterChain

--- ui/common/src/main/java/org/openengsb/ui/common/util/DelegatingSecurityFilter.java
+++ ui/common/src/main/java/org/openengsb/ui/common/util/DelegatingSecurityFilter.java
@@ -28,51 +28,37 @@ import javax.servlet.ServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.security.web.FilterChainProxy;
 
-public class DelegatingSecurityFilter implements Filter, ApplicationContextAware {
+public class DelegatingSecurityFilter implements Filter {
     private static final Logger LOGGER = LoggerFactory.getLogger(DelegatingSecurityFilter.class);
 
-    private ApplicationContext applicationContext;
-    
+    private Filter securityFilterChain;
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
         ServletException {
         Filter delegatedFilter = getFilterBean();
-        if (delegatedFilter != null) {
-            LOGGER.debug("delegate to org.springframework.security.web.filterChainProxy");
-            delegatedFilter.doFilter(request, response, chain);
-        } else {
-            chain.doFilter(request, response);
-        }
+        LOGGER.debug("delegate to org.springframework.security.web.filterChainProxy");
+        delegatedFilter.doFilter(request, response, chain);
     }
 
     private Filter getFilterBean() {
-        if (applicationContext == null) {
-            throw new IllegalStateException("Could not load required security filter, because no application context was set.");
+        if (securityFilterChain == null) {
+            throw new IllegalStateException("Could not load required security filter.");
         }
-        try {
-            return applicationContext.getBean(FilterChainProxy.class);
-        } catch (NoSuchBeanDefinitionException e) {
-            throw new IllegalStateException("Could not load required security filter", e);
-        }
+        return securityFilterChain;
+    }
+
+    public void setSecurityFilterChain(Filter securityFilterChain) {
+        this.securityFilterChain = securityFilterChain;
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
-    
-    @Override
-    public void destroy() {
-    }
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+    public void destroy() {
     }
 
 }
