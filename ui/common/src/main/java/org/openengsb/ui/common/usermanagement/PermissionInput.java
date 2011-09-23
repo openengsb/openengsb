f2d2d96d02ed021d8778900867da19af5ef547ae
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-09-23 15:06:57 +0200

    [OPENENGSB-200] fix licences

--- ui/common/src/main/java/org/openengsb/ui/common/usermanagement/PermissionInput.java
+++ ui/common/src/main/java/org/openengsb/ui/common/usermanagement/PermissionInput.java
@@ -1,10 +1,32 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openengsb.ui.common.usermanagement;
 
 import java.io.Serializable;
 import java.util.Map;
 
 public class PermissionInput implements Serializable {
-    private static final long serialVersionUID = 1769481908418793294L;
+    private static final long serialVersionUID = 1257288811611665273L;
+
+    enum State {
+        UNMODIFIED, NEW, UPDATED, DELETED
+    }
+
+    private State state = State.UNMODIFIED;
 
     private Class<?> type;
 
@@ -32,6 +54,14 @@ public class PermissionInput implements Serializable {
 
     public void setValues(Map<String, String> values) {
         this.values = values;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
     }
 
 }
