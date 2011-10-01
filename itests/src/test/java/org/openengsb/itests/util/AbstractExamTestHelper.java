33b3f959b7504ae6740812dc8f4741c30f8777f4
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2011-10-01 13:16:51 +0200

    [OPENENGSB-2127] make sure rule-config in auditing and taskbox is done before starting itest This is just a workaround to make itests work properly

--- itests/src/test/java/org/openengsb/itests/util/AbstractExamTestHelper.java
+++ itests/src/test/java/org/openengsb/itests/util/AbstractExamTestHelper.java
@@ -17,7 +17,6 @@
 
 package org.openengsb.itests.util;
 
-import static java.lang.String.format;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.debugConfiguration;
@@ -33,19 +32,20 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Dictionary;
 import java.util.Enumeration;
-import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
-import java.util.UUID;
 
 import javax.inject.Inject;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.junit.Before;
+import org.openengsb.core.api.workflow.RuleManager;
+import org.openengsb.core.api.workflow.model.RuleBaseElementId;
+import org.openengsb.core.api.workflow.model.RuleBaseElementType;
 import org.openengsb.labs.paxexam.karaf.options.LogLevelOption.LogLevel;
 import org.openengsb.labs.paxexam.karaf.options.configs.ManagementCfg;
 import org.openengsb.labs.paxexam.karaf.options.configs.WebCfg;
@@ -57,9 +57,9 @@ import org.osgi.framework.Filter;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
-import org.osgi.service.cm.ConfigurationException;
-import org.osgi.service.cm.ManagedServiceFactory;
 import org.osgi.util.tracker.ServiceTracker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -74,6 +74,8 @@ public abstract class AbstractExamTestHelper {
      * is reachable and the hold option if the container should wait for a debugger to be attached or not.
      */
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractExamTestHelper.class);
+
     private static final int DEBUG_PORT = 5005;
     private static final String LOG_LEVEL = "WARN";
     protected static final String WEBUI_PORT = "8091";
@@ -83,6 +85,19 @@ public abstract class AbstractExamTestHelper {
 
     @Inject
     private BundleContext bundleContext;
+
+    @Before
+    public void waitForRequiredTasks() throws Exception {
+        RuleManager rm = getOsgiService(RuleManager.class);
+        while (rm.get(new RuleBaseElementId(RuleBaseElementType.Rule, "auditEvent")) == null) {
+            LOGGER.warn("waiting for auditing to finish init");
+            Thread.sleep(1000);
+        }
+        while (rm.get(new RuleBaseElementId(RuleBaseElementType.Process, "humantask")) == null) {
+            LOGGER.warn("waiting for taskboxConfig to finish init");
+            Thread.sleep(1000);
+        }
+    }
 
     protected <T> T getOsgiService(Class<T> type, long timeout) {
         return getOsgiService(type, null, timeout);
@@ -110,7 +125,7 @@ public abstract class AbstractExamTestHelper {
             Thread.sleep(1000);
             localCounter--;
         }
-        throw new IllegalStateException(format("Couldn't reach page %s within %s seconds", urlToWatchFor,
+        throw new IllegalStateException(String.format("Couldn't reach page %s within %s seconds", urlToWatchFor,
             maxWaitTime));
     }
 
@@ -216,29 +231,6 @@ public abstract class AbstractExamTestHelper {
         return "target/paxrunner/features/";
     }
 
-    public void registerConfigPersistence(String backendId, String configurationId) throws ConfigurationException {
-        ManagedServiceFactory factoryService =
-            getOsgiService(ManagedServiceFactory.class, "(service.pid=org.openengsb.persistence.config)",
-                30000);
-        Hashtable<String, Object> props = new Hashtable<String, Object>();
-        String pid = "org.openengsb.persistence.config." + UUID.randomUUID();
-        props.put("backend.id", backendId);
-        props.put("configuration.id", configurationId);
-        props.put("service.pid", pid);
-        factoryService.updated(pid, props);
-    }
-
-    public static List<String> getImportantBundleSymbolicNames() {
-        List<String> importantBundles = new ArrayList<String>();
-        importantBundles.add("org.openengsb.core.api");
-        importantBundles.add("org.openengsb.core.common");
-        importantBundles.add("org.openengsb.core.service");
-        importantBundles.add("org.openengsb.core.persistence");
-        importantBundles.add("org.openengsb.core.workflow");
-        importantBundles.add("org.openengsb.core.security");
-        return importantBundles;
-    }
-
     protected void authenticateAsAdmin() throws InterruptedException {
         authenticate("admin", "password");
     }
@@ -269,7 +261,8 @@ public abstract class AbstractExamTestHelper {
         Option[] mainOptions =
             new Option[]{
                 karafDistributionConfiguration().frameworkUrl(
-                    maven().groupId("org.openengsb.framework").artifactId("openengsb-framework").type("zip").versionAsInProject()),
+                    maven().groupId("org.openengsb.framework").artifactId("openengsb-framework").type("zip")
+                        .versionAsInProject()),
                 logLevel(realLogLevel),
                 editConfigurationFilePut(WebCfg.HTTP_PORT, WEBUI_PORT),
                 editConfigurationFilePut(ManagementCfg.RMI_SERVER_PORT, RMI_SERVER_PORT),
