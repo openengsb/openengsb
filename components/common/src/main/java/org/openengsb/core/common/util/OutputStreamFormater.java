153bfcf699bdaef7160d86747ddfc0ebf376fa6b
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2011-10-11 22:55:45 +0200

    [OPENENGSB-1639] added output stream helper class

---
+++ components/common/src/main/java/org/openengsb/core/common/util/OutputStreamFormater.java
@@ -0,0 +1,44 @@
+package org.openengsb.core.common.util;
+
+import java.io.PrintStream;
+
+import org.fusesource.jansi.Ansi;
+
+/**
+ * This class is a helper class for the console commands, it prints out the given values to the given stream,
+ * default is System.out. It further formats the output using Ansi formats.
+ */
+public class OutputStreamFormater {
+
+    private static int padding = 25;
+    private static PrintStream outputStream = System.out;
+
+    public static void printValue(String name, String value) {
+        outputStream.println(formatValues(name, value));
+    }
+
+    public static String formatValues(String name, String value) {
+        return Ansi.ansi().a("  ").a(Ansi.Attribute.INTENSITY_BOLD).a(name)
+            .a(spaces(padding - name.length())).a(Ansi.Attribute.RESET).a("   ").a(value).toString();
+    }
+
+    public static String spaces(int nb) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nb; i++) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+    public static void printValue(String value) {
+        outputStream.println(value);
+    }
+
+    public static void setOutputStream(PrintStream outputStream) {
+        OutputStreamFormater.outputStream = outputStream;
+    }
+
+    public static void setPadding(int padding) {
+        OutputStreamFormater.padding = padding;
+    }
+}
---
+++ components/common/src/test/java/org/openengsb/core/common/util/OutputStreamFormaterTest.java
@@ -0,0 +1,43 @@
+package org.openengsb.core.common.util;
+
+import java.io.PrintStream;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class OutputStreamFormaterTest {
+
+    @Test
+    public void testSpaces() {
+        assertThat(OutputStreamFormater.spaces(20).length(), is(20));
+    }
+
+    @Test
+    public void testFormatValues() {
+        String string = "  \u001B[1mOpenEngSB Version        \u001B[m   3.0.0-SNAPSHOT";
+        assertThat(OutputStreamFormater.formatValues("OpenEngSB Version", "3.0.0-SNAPSHOT"), equalTo(string));
+    }
+
+    @Test
+    public void testPrintValues() {
+        PrintStream streamMock = mock(PrintStream.class);
+        OutputStreamFormater.setOutputStream(streamMock);
+        OutputStreamFormater.printValue("OpenEngSB");
+        verify(streamMock, times(1)).println("OpenEngSB");
+    }
+
+    @Test
+    public void testPrintValues2() {
+        String string = "  \u001B[1mOpenEngSB Version        \u001B[m   3.0.0-SNAPSHOT";
+        PrintStream streamMock = mock(PrintStream.class);
+        OutputStreamFormater.setOutputStream(streamMock);
+        OutputStreamFormater.printValue("OpenEngSB Version", "3.0.0-SNAPSHOT");
+        verify(streamMock, times(1)).println(string);
+    }
+}
