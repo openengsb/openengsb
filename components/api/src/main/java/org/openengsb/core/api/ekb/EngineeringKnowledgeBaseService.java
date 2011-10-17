9b32727c52e80093aac42440e8dd7a5509d6ec21
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2011-10-17 15:54:40 +0200

    [OPENENGSB-2229] set the interface and the implementation of the EngineeringKnowledgeBaseService deprecated. Added information what to use instead in the javadoc.

--- components/api/src/main/java/org/openengsb/core/api/ekb/EngineeringKnowledgeBaseService.java
+++ components/api/src/main/java/org/openengsb/core/api/ekb/EngineeringKnowledgeBaseService.java
@@ -26,36 +26,46 @@ import org.openengsb.core.api.model.OpenEngSBModelEntry;
 /**
  * The interface for the ekb service. Contains the functionalities to query for models and also a function for creating
  * a proxy for simulating simple OpenEngSBModel interfaces.
+ * 
+ * This service interface is deprecated. If you want to use the proxiing functionality, you have to use the ModelFactory
+ * service. If you want to query for elements from the EDB, you have to use the QueryInterface service.
  */
+@Deprecated
 public interface EngineeringKnowledgeBaseService {
 
     /**
      * Creates a proxy for the model interface which simulates an implementation of the interface.
      */
+    @Deprecated
     <T extends OpenEngSBModel> T createEmptyModelObject(Class<T> model, OpenEngSBModelEntry... entries);
 
     /**
      * Loads the most actual tool data from the given oid
      */
+    @Deprecated
     <T extends OpenEngSBModel> T getModel(Class<T> model, String oid);
 
     /**
      * Loads the history (all saved versions) of the tool data from the given oid
      */
+    @Deprecated
     <T extends OpenEngSBModel> List<T> getModelHistory(Class<T> model, String oid);
 
     /**
      * Loads the history (all saved versions) of the tool data from the given oid for the given time range
      */
+    @Deprecated
     <T extends OpenEngSBModel> List<T> getModelHistoryForTimeRange(Class<T> model, String oid, Long from, Long to);
 
     /**
      * Queries for models which have a OpenEngSBModelEntry with the given key and the given value saved
      */
+    @Deprecated
     <T extends OpenEngSBModel> List<T> queryForModels(Class<T> model, String key, Object value);
 
     /**
      * Queries for models which have all key/value pairs given in the map saved in the OpenEngSBModelEntries
      */
+    @Deprecated
     <T extends OpenEngSBModel> List<T> queryForModels(Class<T> model, Map<String, Object> queryMap);
 }
