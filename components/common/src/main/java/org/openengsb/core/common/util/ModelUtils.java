dfc1a547e98f9b33a6cc18745ff976f658c9e430
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2011-11-09 22:00:45 +0100

    [OPENENGSB-2260] created static class ModelUtils, which makes the Model Proxy functionality for the EKB

---
+++ components/common/src/main/java/org/openengsb/core/common/util/ModelUtils.java
@@ -0,0 +1,115 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.common.util;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.List;
+
+import org.openengsb.core.api.model.OpenEngSBModel;
+import org.openengsb.core.api.model.OpenEngSBModelEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+
+public final class ModelUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModelUtils.class);
+
+    private ModelUtils() {
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends OpenEngSBModel> T createEmptyModelObject(Class<T> model, OpenEngSBModelEntry... entries) {
+        LOGGER.debug("createEmpytModelObject for model interface {} called", model.getName());
+        return (T) createModelObject(model, entries);
+    }
+
+    public static Object createModelObject(Class<?> model, OpenEngSBModelEntry... entries) {
+        if (!OpenEngSBModel.class.isAssignableFrom(model)) {
+            throw new IllegalArgumentException("OpenEngSBModel has to be deriveable from model parameter");
+        }
+        ClassLoader classLoader = model.getClassLoader();
+        Class<?>[] classes = new Class<?>[]{ OpenEngSBModel.class, model };
+        InvocationHandler handler = makeHandler(model, entries);
+
+        return Proxy.newProxyInstance(classLoader, classes, handler);
+    }
+
+    private static ModelProxyHandler makeHandler(Class<?> model, OpenEngSBModelEntry[] entries) {
+        ModelProxyHandler handler = new ModelProxyHandler(model, entries);
+        return handler;
+    }
+    
+    public static List<PropertyDescriptor> getPropertyDescriptorsForClass(Class<?> clasz) {
+        List<PropertyDescriptor> properties = Lists.newArrayList();
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(clasz);
+            for (PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors()) {
+                properties.add(descriptor);
+            }
+        } catch (IntrospectionException e) {
+            LOGGER.error("instantiation exception while trying to create instance of class {}", clasz.getName());
+        }
+        return properties;
+    }
+
+    public static void invokeSetterMethod(Method setterMethod, Object instance, Object parameter) {
+        try {
+            setterMethod.invoke(instance, parameter);
+        } catch (IllegalArgumentException ex) {
+            LOGGER.error("illegal argument exception when invoking {} with argument {}",
+                setterMethod.getName(), parameter);
+        } catch (IllegalAccessException ex) {
+            LOGGER.error("illegal access exception when invoking {} with argument {}",
+                setterMethod.getName(), parameter);
+        } catch (InvocationTargetException ex) {
+            LOGGER.error("invocatin target exception when invoking {} with argument {}",
+                setterMethod.getName(), parameter);
+        }
+    }
+
+    public static Object invokeGetterMethod(Method getterMethod, Object instance) {
+        try {
+            return getterMethod.invoke(instance);
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("IllegalArgumentException while loading the value for property {}",
+                getPropertyName(getterMethod));
+        } catch (IllegalAccessException e) {
+            LOGGER.error("IllegalAccessException while loading the value for property {}",
+                getPropertyName(getterMethod));
+        } catch (InvocationTargetException e) {
+            LOGGER.error("InvocationTargetException while loading the value for property {}",
+                getPropertyName(getterMethod));
+        }
+        return null;
+    }
+
+    public static String getPropertyName(Method propertyMethod) {
+        String propertyName = propertyMethod.getName().substring(3);
+        char firstChar = propertyName.charAt(0);
+        char newFirstChar = Character.toLowerCase(firstChar);
+        return propertyName.replaceFirst("" + firstChar, "" + newFirstChar);
+    }
+}
