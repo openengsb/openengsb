de82807f17c23c2767c5f4c86b664e7523eaf67a
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2011-12-01 20:15:55 +0100

    [OPENENGSB-2340] made several changes (edb now also gets common reference, many steps in the conversion way from the modelwrapper to the edb has been changed)

---
+++ components/common/src/main/java/org/openengsb/core/common/model/ModelConverterStep.java
@@ -0,0 +1,59 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.common.model;
+
+import org.openengsb.core.api.model.OpenEngSBModel;
+import org.openengsb.core.api.model.OpenEngSBModelWrapper;
+import org.openengsb.core.common.util.ModelUtils;
+
+public final class ModelConverterStep implements ModelEntryConverterStep {
+    private static ModelConverterStep instance;
+
+    public static ModelConverterStep getInstance() {
+        if (instance == null) {
+            instance = new ModelConverterStep();
+        }
+        return instance;
+    }
+
+    private ModelConverterStep() {
+    }
+
+    @Override
+    public boolean matches(Object object) {
+        return object != null && OpenEngSBModel.class.isAssignableFrom(object.getClass());
+    }
+
+    @Override
+    public Object convert(Object object) {
+        OpenEngSBModel model = (OpenEngSBModel) object;
+        return ModelUtils.generateWrapperOutOfModel(model);
+    }
+
+    @Override
+    public boolean matches2(Object object) {
+        return object != null && object.getClass().equals(OpenEngSBModelWrapper.class);
+    }
+
+    @Override
+    public Object convert2(Object object) {
+        OpenEngSBModelWrapper wrapper = (OpenEngSBModelWrapper) object;
+        return ModelUtils.generateModelOutOfWrapper(wrapper);
+    }
+
+}
