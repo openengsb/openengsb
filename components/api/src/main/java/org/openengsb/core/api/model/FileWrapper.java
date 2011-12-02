708d0f44bae2666096b39587b1ff85207dc61b59
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2011-12-02 15:41:40 +0100

    [OPENENGSB-2339] added FileWrapper and FileConverterStep

---
+++ components/api/src/main/java/org/openengsb/core/api/model/FileWrapper.java
@@ -0,0 +1,43 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.api.model;
+
+/**
+ * The FileWrapper class is needed for the proper sending of File objects (with Jason).
+ */
+public class FileWrapper {
+    private String filename;
+    private byte[] content;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+}
+
