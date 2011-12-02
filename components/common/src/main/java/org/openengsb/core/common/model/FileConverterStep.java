175037b35ce460ebc852c196221d66e7cda579b8
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2011-12-02 19:39:14 +0100

    [OPENENGSB-2339] added functionality so that the converting between File and FileWrapper is working

--- components/common/src/main/java/org/openengsb/core/common/model/FileConverterStep.java
+++ components/common/src/main/java/org/openengsb/core/common/model/FileConverterStep.java
@@ -17,10 +17,17 @@
 
 package org.openengsb.core.common.model;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 import org.openengsb.core.api.model.FileWrapper;
 import org.slf4j.Logger;
@@ -33,6 +40,8 @@ import org.slf4j.LoggerFactory;
 public final class FileConverterStep implements ModelEntryConverterStep {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileConverterStep.class);
     private static FileConverterStep instance;
+    private static final int BUFFER = 2048;
+    private static String tempDir = System.getProperty("java.io.tmpdir") + File.separator + "OpenEngSBModelTemp";
 
     public static FileConverterStep getInstance() {
         if (instance == null) {
@@ -52,24 +61,61 @@ public final class FileConverterStep implements ModelEntryConverterStep {
     @Override
     public Object convertForGetModelEntries(Object object) {
         File file = (File) object;
-        // TODO: create a zip file and get the stream from there
-
+        File zipFile = zipFile(file);
         try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-            byte[] data = new byte[(int) file.length()];
+            FileInputStream fileInputStream = new FileInputStream(zipFile);
+            byte[] data = new byte[(int) zipFile.length()];
             fileInputStream.read(data);
             fileInputStream.close();
 
             FileWrapper wrapper = new FileWrapper();
             wrapper.setFilename(file.getName());
             wrapper.setContent(data);
+            
+            zipFile.delete();
             return wrapper;
         } catch (FileNotFoundException ex) {
             LOGGER.error("File " + file.getAbsolutePath() + " was not found", ex);
         } catch (IOException ex) {
-            LOGGER.error("IOException while reading from filee " + file.getAbsolutePath(), ex);
+            LOGGER.error("IOException while reading from file " + file.getAbsolutePath(), ex);
         }
         return null;
+    }
+
+    private File zipFile(File file) {
+        new File(tempDir).mkdir();
+        File result = new File(tempDir + File.separator + file.getName() + ".zip");
+        try {
+            FileOutputStream dest = new FileOutputStream(result);
+            ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
+            byte[] data = new byte[BUFFER];
+
+            File[] files;
+            if (file.isDirectory()) {
+                files = file.listFiles();
+            } else {
+                files = new File[1];
+                files[0] = file;
+            }
+
+            for (File f : files) {
+                FileInputStream fi = new FileInputStream(f);
+                BufferedInputStream bis = new BufferedInputStream(fi, BUFFER);
+                ZipEntry entry = new ZipEntry(f.getName());
+                out.putNextEntry(entry);
+                int count;
+                while ((count = bis.read(data, 0, BUFFER)) != -1) {
+                    out.write(data, 0, count);
+                }
+                bis.close();
+            }
+            out.close();
+        } catch (FileNotFoundException ex) {
+            LOGGER.error("File " + file.getAbsolutePath() + " was not found", ex);
+        } catch (IOException ex) {
+            LOGGER.error("IOException while zipping file " + file.getAbsolutePath(), ex);
+        }
+        return result;
     }
 
     @Override
@@ -80,9 +126,62 @@ public final class FileConverterStep implements ModelEntryConverterStep {
     @Override
     public Object convertForGetter(Object object) {
         FileWrapper wrapper = (FileWrapper) object;
-        File f = new File(wrapper.getFilename());
-        // TODO do conversion Work
-        return f;
+        File f = new File(tempDir + File.separator + wrapper.getFilename() + ".zip"); 
+        try {
+            f.createNewFile();
+            FileOutputStream stream = new FileOutputStream(f);
+            stream.write(wrapper.getContent());
+            stream.flush();
+            stream.close();
+        } catch (FileNotFoundException ex) {
+            LOGGER.error("File " + f.getAbsolutePath() + " could not be created", ex);
+        } catch (IOException ex) {
+            LOGGER.error("IOException while writing the byte array of the wrapper to file " + f.getAbsolutePath(), ex);
+        }
+
+        return unzipFile(f, wrapper.getFilename());
+    }
+
+    private File unzipFile(File file, String fileName) {
+        String parentPath = file.getParentFile().getAbsolutePath();
+        File result = new File(parentPath + File.separator + fileName);
+        try {
+            BufferedOutputStream dest = null;
+            BufferedInputStream is = null;
+            ZipEntry entry;
+            ZipFile zipfile = new ZipFile(file);
+            int entryCount = zipfile.size();
+            @SuppressWarnings("rawtypes")
+            Enumeration e = zipfile.entries();
+            String elementPath = "";
+            if (entryCount == 1) {
+                elementPath = parentPath;
+            } else {
+                result.mkdir();
+                elementPath = parentPath + File.separator + fileName;
+            }
+
+            while (e.hasMoreElements()) {
+                entry = (ZipEntry) e.nextElement();
+                is = new BufferedInputStream(zipfile.getInputStream(entry));
+                int count;
+                byte[] data = new byte[BUFFER];
+                FileOutputStream fos = new FileOutputStream(elementPath + File.separator + entry.getName());
+                dest = new BufferedOutputStream(fos, BUFFER);
+                while ((count = is.read(data, 0, BUFFER)) != -1) {
+                    dest.write(data, 0, count);
+                }
+                dest.flush();
+                dest.close();
+                is.close();
+            }
+            file.delete();
+        } catch (FileNotFoundException ex) {
+            LOGGER.error("File " + file.getAbsolutePath() + " was not found", ex);
+        } catch (IOException ex) {
+            LOGGER.error("IOException while unzipping file " + file.getAbsolutePath(), ex);
+        }
+        return result;
     }
 
 }
