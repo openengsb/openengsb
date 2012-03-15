f857dc822878726a6c6aab542456c661034b8875
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-03-15 10:47:28 +0100

    [OPENENGSB-2824] store activemq-data from unit-tests in temp-directory

--- ports/jms/src/test/java/org/openengsb/ports/jms/JMSPortTest.java
+++ ports/jms/src/test/java/org/openengsb/ports/jms/JMSPortTest.java
@@ -60,7 +60,9 @@ import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.openengsb.connector.usernamepassword.Password;
@@ -199,8 +201,13 @@ public class JMSPortTest extends AbstractOsgiMockServiceTest {
     private PrivateKey privateKey;
     private PublicKey publicKey;
 
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
     @Before
     public void setup() {
+        System.setProperty("org.apache.activemq.default.directory.prefix",
+            tempFolder.getRoot().getAbsolutePath() + "/");
         setupKeys();
         String num = UUID.randomUUID().toString();
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost" + num);
@@ -255,7 +262,7 @@ public class JMSPortTest extends AbstractOsgiMockServiceTest {
         publicKey = CipherUtils.deserializePublicKey(Base64.decodeBase64(PUBLIC_KEY_64), "RSA");
     }
 
-    @Test(timeout = 10000)
+    @Test(timeout = 100000)
     public void start_ShouldListenToIncomingCallsAndCallSetRequestHandler() throws InterruptedException, IOException {
         FilterChainFactory<String, String> factory = new FilterChainFactory<String, String>(String.class, String.class);
         factory.setFilters(Arrays.asList(JsonMethodCallMarshalFilter.class, new RequestMapperFilter(handler)));
