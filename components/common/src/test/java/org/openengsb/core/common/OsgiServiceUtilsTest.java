ed999a27ed5b7416ea109ea802b172e1a4a112db
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-04-24 17:13:23 +0200

    [OPENENGSB-1735] fix race-condition in DefaultOsgiUtilsService

--- components/common/src/test/java/org/openengsb/core/common/OsgiServiceUtilsTest.java
+++ components/common/src/test/java/org/openengsb/core/common/OsgiServiceUtilsTest.java
@@ -17,6 +17,16 @@
 
 package org.openengsb.core.common;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openengsb.core.api.OsgiServiceNotAvailableException;
@@ -24,10 +34,24 @@ import org.openengsb.core.api.OsgiUtilsService;
 import org.openengsb.core.common.util.DefaultOsgiUtilsService;
 import org.openengsb.core.test.AbstractOsgiMockServiceTest;
 import org.openengsb.core.test.NullDomain;
+import org.openengsb.core.test.NullDomainImpl;
 
 public class OsgiServiceUtilsTest extends AbstractOsgiMockServiceTest {
 
+    private class BlockingNullDomain extends NullDomainImpl {
+        @Override
+        public Object nullMethod(Object o, String b) {
+            try {
+                sync.acquire();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return super.nullMethod(o, b);
+        }
+    }
+
     private OsgiUtilsService utils;
+    private Semaphore sync = new Semaphore(0);
 
     @Before
     public void setUp() throws Exception {
@@ -40,4 +64,45 @@ public class OsgiServiceUtilsTest extends AbstractOsgiMockServiceTest {
         osgiServiceProxy.getAliveState();
     }
 
+    @Test
+    public void handleParallelProxyCalls() throws Exception {
+        registerServiceViaId(new BlockingNullDomain(), "foo", NullDomain.class);
+
+        final NullDomain service = utils.getOsgiServiceProxy(NullDomain.class);
+
+        Callable<Object> normalCall = new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                return service.nullMethod(42);
+            }
+        };
+
+        Callable<Object> blockingCall = new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                return service.nullMethod(42, "foo");
+            }
+        };
+
+        FutureTask<Object> normalFuture = new FutureTask<Object>(normalCall);
+        new Thread(normalFuture).start();
+        FutureTask<Object> blockingFuture = new FutureTask<Object>(blockingCall);
+        new Thread(blockingFuture).start();
+
+        Object normalResult = normalFuture.get();
+
+        // verify(serviceMock).nullMethod(any());
+        /* getAnswer-call is finished */
+        assertThat((Integer) normalResult, is(42));
+        try {
+            blockingFuture.get(200, TimeUnit.MILLISECONDS);
+            fail("blocking method returned premature");
+        } catch (TimeoutException e) {
+            // ignore, this is expceted
+        }
+
+        sync.release();
+        Object blockingResult = blockingFuture.get();
+        assertThat((Integer) blockingResult, is(42));
+    }
 }
