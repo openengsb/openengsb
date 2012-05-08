f59b85f26b2c90fe870883c20a9009a3b4b40268
Stefan Dösinger <stefandoesinger@gmail.com>
2012-05-08 14:03:23 +0200

    OPENENGSB-2994: Correctly calculate the remaining timeout (cherry picked from commit 42657e44c17c5e6b963d7b82a06be58f3e52a54c)

--- components/workflow/src/main/java/org/openengsb/core/workflow/internal/WorkflowServiceImpl.java
+++ components/workflow/src/main/java/org/openengsb/core/workflow/internal/WorkflowServiceImpl.java
@@ -301,7 +301,7 @@ public class WorkflowServiceImpl extends AbstractOpenEngSBService implements Wor
         synchronized (session) {
             while (session.getProcessInstance(id) != null && timeout > 0) {
                 session.wait(timeout);
-                timeout = System.currentTimeMillis() - endTime;
+                timeout = endTime - System.currentTimeMillis();
             }
         }
         return !getRunningFlows().contains(id);
