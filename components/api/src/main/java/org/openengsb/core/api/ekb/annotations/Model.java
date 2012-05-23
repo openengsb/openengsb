2a9ca0acc0568042dd982a5dd0d6cb3d6071bd5e
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2012-05-23 13:10:29 +0200

    [OPENENGSB-3018] added model annotation

---
+++ components/api/src/main/java/org/openengsb/core/api/ekb/annotations/Model.java
@@ -0,0 +1,33 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.core.api.ekb.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to mark OpenEngSB models. Every class that is marked with this annotation, can be used with
+ * all semantic tools of the OpenEngSB.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Model {
+
+}
