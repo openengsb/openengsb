5f33bad14dde8435af3fc686c5a8a630d090e629
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2012-05-24 22:28:19 +0200

    [OPENENGSB-3018] added method to ModelUtils which create models out of a class + a list of OpenEngSBModels

--- components/common/src/main/java/org/openengsb/core/common/util/ModelUtils.java
+++ components/common/src/main/java/org/openengsb/core/common/util/ModelUtils.java
@@ -21,16 +21,12 @@ import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.List;
 
 import org.openengsb.core.api.model.OpenEngSBModel;
 import org.openengsb.core.api.model.OpenEngSBModelEntry;
-import org.openengsb.core.api.model.OpenEngSBModelWrapper;
-import org.openengsb.core.common.model.ModelProxyHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -50,9 +46,7 @@ public final class ModelUtils {
      * passed object is not an OpenEngSBModel instance.
      */
     public static List<OpenEngSBModelEntry> getOpenEngSBModelEntries(Object model) {
-        if (!OpenEngSBModel.class.isAssignableFrom(model.getClass())) {
-            throw new IllegalArgumentException("Can only be performed with models");            
-        }
+        checkIfObjectIsModel(model);
         return ((OpenEngSBModel) model).getOpenEngSBModelEntries();
     }
 
@@ -61,9 +55,7 @@ public final class ModelUtils {
      * object is not an OpenEngSBModel instance.
      */
     public static void addOpenEngSBModelEntry(Object model, OpenEngSBModelEntry entry) {
-        if (!OpenEngSBModel.class.isAssignableFrom(model.getClass())) {
-            throw new IllegalArgumentException("Can only be performed with models");            
-        }
+        checkIfObjectIsModel(model);
         ((OpenEngSBModel) model).addOpenEngSBModelEntry(entry);
     }
 
@@ -72,112 +64,154 @@ public final class ModelUtils {
      * passed object is not an OpenEngSBModel instance.
      */
     public static void removeOpenEngSBModelEntry(Object model, String key) {
-        if (!OpenEngSBModel.class.isAssignableFrom(model.getClass())) {
-            throw new IllegalArgumentException("Can only be performed with models");
-        }
+        checkIfObjectIsModel(model);
         ((OpenEngSBModel) model).removeOpenEngSBModelEntry(key);
     }
 
-    /**
-     * Creates a proxy for the model interface which simulates an implementation of the interface.
-     */
-    @SuppressWarnings("unchecked")
-    public static <T extends OpenEngSBModel> T createEmptyModelObject(Class<T> model, OpenEngSBModelEntry... entries) {
-        LOGGER.debug("createEmpytModelObject for model interface {} called", model.getName());
-        return (T) createModelObject(model, entries);
-    }
-
-    /**
-     * Creates a proxy for the model interface which simulates an implementation of the interface. Only useable with
-     * OpenEngSBModel interfaces. If the model parameter is not an OpenEngSBModel interface, an exception is thrown.
-     */
-    public static Object createModelObject(Class<?> model, OpenEngSBModelEntry... entries) {
-        if (!OpenEngSBModel.class.isAssignableFrom(model)) {
-            throw new IllegalArgumentException("OpenEngSBModel has to be deriveable from model parameter");
-        }
-        ClassLoader classLoader = model.getClassLoader();
-        Class<?>[] classes = new Class<?>[]{ OpenEngSBModel.class, model };
-        InvocationHandler handler = makeHandler(model, entries);
-
-        return Proxy.newProxyInstance(classLoader, classes, handler);
-    }
-
-    private static ModelProxyHandler makeHandler(Class<?> model, OpenEngSBModelEntry[] entries) {
-        ModelProxyHandler handler = new ModelProxyHandler(model, entries);
-        return handler;
-    }
-
-    /**
-     * Generates an OpenEngSBModelWrapper out of a model. This construct is needed so that the sending of
-     * OpenEngSBModels over remote can work properly
-     */
-    public static OpenEngSBModelWrapper generateWrapperOutOfModel(OpenEngSBModel model) {
-        OpenEngSBModelWrapper wrapper = new OpenEngSBModelWrapper();
-        Class<?> clazz = ModelUtils.getModelClassOfOpenEngSBModelObject(model.getClass());
-        wrapper.setEntries(model.getOpenEngSBModelEntries());
-        wrapper.setModelClass(clazz.getName());
-        return wrapper;
-    }
-
-    /**
-     * Tries to generate a model of the given class out of a wrapper. If something went wrong (e.g. the model contained
-     * in the wrapper is not of the class which is given as parameter) an illegal argument exception is thrown.
-     */
-    @SuppressWarnings("unchecked")
-    public static <T extends OpenEngSBModel> T generateModelOutOfWrapper(OpenEngSBModelWrapper wrapper,
-            Class<T> clazz) {
-        Object modelObject = ModelUtils.generateModelOutOfWrapper(wrapper);
-        if (modelObject != null) {
-            try {
-                return (T) modelObject;
-            } catch (ClassCastException ex) {
-                throw new IllegalArgumentException("Wrapper doesn't contain a model of class" + clazz.getName(), ex);
-            }
-        }
-        throw new IllegalArgumentException("Wrapper doesn't contain a model of class" + clazz.getName());
-    }
-
-    /**
-     * Tries to generate a model out of an OpenEngSB model wrapper. Without casting, only generating. If the model class
-     * can't be found, an illegal argument exception is thrown.
-     */
-    public static Object generateModelOutOfWrapper(OpenEngSBModelWrapper wrapper) {
-        return generateModelOutOfWrapper(wrapper, ModelUtils.class.getClassLoader());
-    }
-
-    /**
-     * Tries to generate a model out of an OpenEngSB model wrapper. Uses the given class loader to load the model class.
-     * Needed by sub models which aren't exported (like in the ITests).
-     */
-    public static Object generateModelOutOfWrapper(OpenEngSBModelWrapper wrapper, ClassLoader parentClassLoader) {
-        OpenEngSBModelEntry[] entries = wrapper.getEntries().toArray(new OpenEngSBModelEntry[0]);
-        Class<?> clazz;
+    public static <T> T createModel(Class<T> model, List<OpenEngSBModelEntry> entries) {
+        checkIfClassIsModel(model);
         try {
-            clazz = parentClassLoader.loadClass(wrapper.getModelClass());
-            return ModelUtils.createModelObject(clazz, entries);
-        } catch (ClassNotFoundException ex) {
-            throw new IllegalArgumentException("The class of the model can't be found", ex);
-        }
-    }
-
-    /**
-     * Returns the real model class of a model (instead of "$ProxyX"). If the given class is not a model, an illegal
-     * argument exception is thrown.
-     */
-    public static Class<?> getModelClassOfOpenEngSBModelObject(Class<?> clazz) {
-        Class<?> result = null;
-        for (Class<?> inter : clazz.getInterfaces()) {
-            if (OpenEngSBModel.class.isAssignableFrom(inter) && !inter.equals(OpenEngSBModel.class)) {
-                result = inter;
-                break;
+            T instance = model.newInstance();
+            for (OpenEngSBModelEntry entry : entries) {
+                try {
+                    String setterName = getSetterName(entry.getKey());
+                    Method method = model.getMethod(setterName, entry.getType());
+                    method.invoke(instance, entry.getValue());
+                } catch (NoSuchMethodException e) {
+                    ((OpenEngSBModel) instance).addOpenEngSBModelEntry(entry);
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
             }
+            return instance;
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
         }
-        if (result == null) {
-            throw new IllegalArgumentException("class " + clazz.getName() + " doesn't implement domain model object");
-        }
-        return result;
+        return null;
     }
 
+    public static void checkIfObjectIsModel(Object model) {
+        if (!OpenEngSBModel.class.isAssignableFrom(model.getClass())) {
+            throw new IllegalArgumentException("The given object is no model");
+        }
+    }
+    
+    public static void checkIfClassIsModel(Class<?> clazz) {
+        if (!OpenEngSBModel.class.isAssignableFrom(clazz)) {
+            throw new IllegalArgumentException("The given class is no model");
+        }
+    }
+
+    // /**
+    // * Creates a proxy for the model interface which simulates an implementation of the interface.
+    // */
+    // @SuppressWarnings("unchecked")
+    // public static <T extends OpenEngSBModel> T createEmptyModelObject(Class<T> model, OpenEngSBModelEntry... entries)
+    // {
+    // LOGGER.debug("createEmpytModelObject for model interface {} called", model.getName());
+    // return (T) createModelObject(model, entries);
+    // }
+    //
+    // /**
+    // * Creates a proxy for the model interface which simulates an implementation of the interface. Only useable with
+    // * OpenEngSBModel interfaces. If the model parameter is not an OpenEngSBModel interface, an exception is thrown.
+    // */
+    // public static Object createModelObject(Class<?> model, OpenEngSBModelEntry... entries) {
+    // if (!OpenEngSBModel.class.isAssignableFrom(model)) {
+    // throw new IllegalArgumentException("OpenEngSBModel has to be deriveable from model parameter");
+    // }
+    // ClassLoader classLoader = model.getClassLoader();
+    // Class<?>[] classes = new Class<?>[]{ OpenEngSBModel.class, model };
+    // InvocationHandler handler = makeHandler(model, entries);
+    //
+    // return Proxy.newProxyInstance(classLoader, classes, handler);
+    // }
+    //
+    // private static ModelProxyHandler makeHandler(Class<?> model, OpenEngSBModelEntry[] entries) {
+    // ModelProxyHandler handler = new ModelProxyHandler(model, entries);
+    // return handler;
+    // }
+    //
+    // /**
+    // * Generates an OpenEngSBModelWrapper out of a model. This construct is needed so that the sending of
+    // * OpenEngSBModels over remote can work properly
+    // */
+    // public static OpenEngSBModelWrapper generateWrapperOutOfModel(OpenEngSBModel model) {
+    // OpenEngSBModelWrapper wrapper = new OpenEngSBModelWrapper();
+    // Class<?> clazz = ModelUtils.getModelClassOfOpenEngSBModelObject(model.getClass());
+    // wrapper.setEntries(model.getOpenEngSBModelEntries());
+    // wrapper.setModelClass(clazz.getName());
+    // return wrapper;
+    // }
+    //
+    // /**
+    // * Tries to generate a model of the given class out of a wrapper. If something went wrong (e.g. the model
+    // contained
+    // * in the wrapper is not of the class which is given as parameter) an illegal argument exception is thrown.
+    // */
+    // @SuppressWarnings("unchecked")
+    // public static <T extends OpenEngSBModel> T generateModelOutOfWrapper(OpenEngSBModelWrapper wrapper,
+    // Class<T> clazz) {
+    // Object modelObject = ModelUtils.generateModelOutOfWrapper(wrapper);
+    // if (modelObject != null) {
+    // try {
+    // return (T) modelObject;
+    // } catch (ClassCastException ex) {
+    // throw new IllegalArgumentException("Wrapper doesn't contain a model of class" + clazz.getName(), ex);
+    // }
+    // }
+    // throw new IllegalArgumentException("Wrapper doesn't contain a model of class" + clazz.getName());
+    // }
+    //
+    // /**
+    // * Tries to generate a model out of an OpenEngSB model wrapper. Without casting, only generating. If the model
+    // class
+    // * can't be found, an illegal argument exception is thrown.
+    // */
+    // public static Object generateModelOutOfWrapper(OpenEngSBModelWrapper wrapper) {
+    // return generateModelOutOfWrapper(wrapper, ModelUtils.class.getClassLoader());
+    // }
+    //
+    // /**
+    // * Tries to generate a model out of an OpenEngSB model wrapper. Uses the given class loader to load the model
+    // class.
+    // * Needed by sub models which aren't exported (like in the ITests).
+    // */
+    // public static Object generateModelOutOfWrapper(OpenEngSBModelWrapper wrapper, ClassLoader parentClassLoader) {
+    // OpenEngSBModelEntry[] entries = wrapper.getEntries().toArray(new OpenEngSBModelEntry[0]);
+    // Class<?> clazz;
+    // try {
+    // clazz = parentClassLoader.loadClass(wrapper.getModelClass());
+    // return ModelUtils.createModelObject(clazz, entries);
+    // } catch (ClassNotFoundException ex) {
+    // throw new IllegalArgumentException("The class of the model can't be found", ex);
+    // }
+    // }
+    //
+    // /**
+    // * Returns the real model class of a model (instead of "$ProxyX"). If the given class is not a model, an illegal
+    // * argument exception is thrown.
+    // */
+    // public static Class<?> getModelClassOfOpenEngSBModelObject(Class<?> clazz) {
+    // Class<?> result = null;
+    // for (Class<?> inter : clazz.getInterfaces()) {
+    // if (OpenEngSBModel.class.isAssignableFrom(inter) && !inter.equals(OpenEngSBModel.class)) {
+    // result = inter;
+    // break;
+    // }
+    // }
+    // if (result == null) {
+    // throw new IllegalArgumentException("class " + clazz.getName() + " doesn't implement domain model object");
+    // }
+    // return result;
+    // }
+    //
     /**
      * Returns all property descriptors for a given class.
      */
@@ -194,50 +228,54 @@ public final class ModelUtils {
         return properties;
     }
 
-    /**
-     * Tries to invoke a setter method of a given instance, with the given parameter object.
-     */
-    public static void invokeSetterMethod(Method setterMethod, Object instance, Object parameter) {
-        try {
-            setterMethod.invoke(instance, parameter);
-        } catch (IllegalArgumentException ex) {
-            LOGGER.error("illegal argument exception when invoking {} with argument {}",
-                setterMethod.getName(), parameter);
-        } catch (IllegalAccessException ex) {
-            LOGGER.error("illegal access exception when invoking {} with argument {}",
-                setterMethod.getName(), parameter);
-        } catch (InvocationTargetException ex) {
-            LOGGER.error("invocatin target exception when invoking {} with argument {}",
-                setterMethod.getName(), parameter);
-        }
+    private static String getSetterName(String propertyName) {
+        return String.format("%s%s%s", "set", (propertyName.charAt(0) + "").toUpperCase(), propertyName.substring(1));
     }
-
-    /**
-     * Tries to invoke a getter method of the given instance.
-     */
-    public static Object invokeGetterMethod(Method getterMethod, Object instance) {
-        try {
-            return getterMethod.invoke(instance);
-        } catch (IllegalArgumentException e) {
-            LOGGER.error("IllegalArgumentException while loading the value for property {}",
-                getPropertyName(getterMethod));
-        } catch (IllegalAccessException e) {
-            LOGGER.error("IllegalAccessException while loading the value for property {}",
-                getPropertyName(getterMethod));
-        } catch (InvocationTargetException e) {
-            LOGGER.error("InvocationTargetException while loading the value for property {}",
-                getPropertyName(getterMethod));
-        }
-        return null;
-    }
-
-    /**
-     * Converts the property name out of a method(e.g. getId -> id).
-     */
-    public static String getPropertyName(Method propertyMethod) {
-        String propertyName = propertyMethod.getName().substring(3);
-        char firstChar = propertyName.charAt(0);
-        char newFirstChar = Character.toLowerCase(firstChar);
-        return propertyName.replaceFirst("" + firstChar, "" + newFirstChar);
-    }
+    //
+    // /**
+    // * Tries to invoke a setter method of a given instance, with the given parameter object.
+    // */
+    // public static void invokeSetterMethod(Method setterMethod, Object instance, Object parameter) {
+    // try {
+    // setterMethod.invoke(instance, parameter);
+    // } catch (IllegalArgumentException ex) {
+    // LOGGER.error("illegal argument exception when invoking {} with argument {}",
+    // setterMethod.getName(), parameter);
+    // } catch (IllegalAccessException ex) {
+    // LOGGER.error("illegal access exception when invoking {} with argument {}",
+    // setterMethod.getName(), parameter);
+    // } catch (InvocationTargetException ex) {
+    // LOGGER.error("invocatin target exception when invoking {} with argument {}",
+    // setterMethod.getName(), parameter);
+    // }
+    // }
+    //
+    // /**
+    // * Tries to invoke a getter method of the given instance.
+    // */
+    // public static Object invokeGetterMethod(Method getterMethod, Object instance) {
+    // try {
+    // return getterMethod.invoke(instance);
+    // } catch (IllegalArgumentException e) {
+    // LOGGER.error("IllegalArgumentException while loading the value for property {}",
+    // getPropertyName(getterMethod));
+    // } catch (IllegalAccessException e) {
+    // LOGGER.error("IllegalAccessException while loading the value for property {}",
+    // getPropertyName(getterMethod));
+    // } catch (InvocationTargetException e) {
+    // LOGGER.error("InvocationTargetException while loading the value for property {}",
+    // getPropertyName(getterMethod));
+    // }
+    // return null;
+    // }
+    //
+    // /**
+    // * Converts the property name out of a method(e.g. getId -> id).
+    // */
+    // public static String getPropertyName(Method propertyMethod) {
+    // String propertyName = propertyMethod.getName().substring(3);
+    // char firstChar = propertyName.charAt(0);
+    // char newFirstChar = Character.toLowerCase(firstChar);
+    // return propertyName.replaceFirst("" + firstChar, "" + newFirstChar);
+    // }
 }
