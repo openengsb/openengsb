bfc5b42b990ad9be6b291198fa6ddd7731028909
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-06-14 20:45:36 +0200

    [OPENENGSB-2828] add javadoc

--- components/api/src/main/java/org/openengsb/core/api/remote/ProxyConnectorRegistry.java
+++ components/api/src/main/java/org/openengsb/core/api/remote/ProxyConnectorRegistry.java
@@ -1,10 +1,20 @@
 package org.openengsb.core.api.remote;
 
-
+/**
+ * manages registrations of remote-connectors
+ */
 public interface ProxyConnectorRegistry {
 
+    /**
+     * registers the given portId and destination for the given connector identified with the given uuid. The given
+     * portId and destination are used for future calls on the proxy-connector.
+     */
     void registerConnector(String uuid, String portId, String destination);
 
+    /**
+     * unregisters a remote-connector. After that the ProxyConnector knows that the remote destination is no longer
+     * reachable.
+     */
     void unregisterConnector(String uuid);
 
 }
