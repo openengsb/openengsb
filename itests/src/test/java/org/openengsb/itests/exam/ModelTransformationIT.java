2772a50bef5d3dacc9882266beb2d359f43b81f3
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2012-07-05 01:36:06 +0200

    [OPENENGSB-3018] fixed failing integration test

--- itests/src/test/java/org/openengsb/itests/exam/ModelTransformationIT.java
+++ itests/src/test/java/org/openengsb/itests/exam/ModelTransformationIT.java
@@ -150,24 +150,15 @@ public class ModelTransformationIT extends AbstractPreConfiguredExamTestHelper {
             .add(
                 new RuleBaseElementId(RuleBaseElementType.Rule, "example"),
                 ""
-                        +
-                        "when\n"
-                        +
-                        "    event : LogEvent()\n"
-                        +
-                        "then\n"
-                        +
-                        sourceDescription
-                        +
-                        targetDescription
-                        +
-                        "    ExampleResponseModel object = (ExampleResponseModel) ModelUtils.createEmptyModelObject(ExampleResponseModel.class, new OpenEngSBModelEntry[] {});"
-                        +
-                        "    object.setResult(\"test-42\");"
-                        +
-                        "    ExampleRequestModel model = (ExampleRequestModel) ekbTransformationService.performTransformation(source, target, object);"
-                        +
-                        "    example2.doSomethingWithModel(model);\n"
+                        + "when\n"
+                        + "  event : LogEvent()\n"
+                        + "then\n"
+                        + sourceDescription
+                        + targetDescription
+                        + "  ExampleResponseModel object = new ExampleResponseModel();"
+                        + "  object.setResult(\"test-42\");"
+                        + "  ExampleRequestModel model = (ExampleRequestModel) ekbTransformationService.performTransformation(source, target, object);"
+                        + "  example2.doSomethingWithModel(model);\n"
             );
 
         ContextHolder.get().setCurrentContextId("foo");
@@ -179,7 +170,7 @@ public class ModelTransformationIT extends AbstractPreConfiguredExamTestHelper {
         ExampleRequestModel result = exampleMock.getModel();
         assertThat(result.getName(), is("test"));
     }
-    
+
     private void loadDescriptionsFromFile() throws Exception {
         InputStream stream =
             getClass().getClassLoader().getResourceAsStream("transformations/testDescription.transformation");
