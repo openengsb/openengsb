37e934d593215e781eb61961f01765fc5cb26181
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2012-07-19 23:40:00 +0200

    [OPENENGSB-1884] the deleting of properties works now, but refreshment doesn't work

--- ui/common/src/main/java/org/openengsb/ui/common/editor/ServiceEditorPanel.java
+++ ui/common/src/main/java/org/openengsb/ui/common/editor/ServiceEditorPanel.java
@@ -28,10 +28,13 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormValidatingBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.behavior.AbstractBehavior;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableLabel;
+import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
@@ -45,6 +48,7 @@ import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.resource.PackageResourceReference;
 import org.apache.wicket.validation.ValidationError;
 import org.openengsb.core.api.ConnectorValidationFailedException;
 import org.openengsb.core.api.descriptor.AttributeDefinition;
@@ -60,8 +64,9 @@ import com.google.common.collect.Lists;
 
 /**
  * Creates a panel containing a service-editor, for usage in forms.
- *
+ * 
  */
+@SuppressWarnings("deprecation")
 public class ServiceEditorPanel extends Panel {
 
     private static final long serialVersionUID = 5593901084329552949L;
@@ -89,6 +94,22 @@ public class ServiceEditorPanel extends Panel {
                 }
             } else if (index > 0) {
                 entry.setValue(new Object[index + 1]);
+            }
+        }
+
+        public void deleteSubElement(int index) {
+            if (isArray()) {
+                Object[] oldArray = getArray();
+                Object[] newArray = new Object[oldArray.length - 1];
+                int j = 0;
+                for (int i = 0; i < oldArray.length; i++) {
+                    if (i == index) {
+                        continue;
+                    }
+                    newArray[j] = oldArray[i];
+                    j++;
+                }
+                entry.setValue(newArray.length == 1 ? newArray[0] : newArray);
             }
         }
 
@@ -139,12 +160,20 @@ public class ServiceEditorPanel extends Panel {
         org.openengsb.core.api.Constants.DOMAIN_KEY,
         Constants.SERVICE_ID, Constants.OBJECTCLASS, Constants.SERVICE_PID);
 
+    @SuppressWarnings({ "serial" })
     public ServiceEditorPanel(String id, List<AttributeDefinition> attributes,
             Map<String, String> attributeMap, Map<String, Object> properties, Form<?> parentForm) {
         super(id);
         this.attributes = attributes;
         this.parentForm = parentForm;
         initPanel(attributes, attributeMap, properties);
+        add(new AbstractBehavior() {
+            @Override
+            public void renderHead(Component component, IHeaderResponse response) {
+                response.renderCSSReference(new PackageResourceReference(ServiceEditorPanel.class,
+                    "ServiceEditorPanel.css"));
+            }
+        });
     }
 
     public void reloadList(Map<String, Object> properties) {
@@ -206,16 +235,25 @@ public class ServiceEditorPanel extends Panel {
                 IModel<String> keyModel = new PropertyModel<String>(modelObject, "key");
                 item.add(new Label("key", keyModel));
 
-                RepeatingView repeater = new RepeatingView("values");
+                final RepeatingView repeater = new RepeatingView("values");
                 item.add(repeater);
                 Object value = modelObject.getValue();
                 if (value.getClass().isArray()) {
                     Object[] values = (Object[]) value;
                     for (int i = 0; i < values.length; i++) {
                         WebMarkupContainer container = new WebMarkupContainer(repeater.newChildId());
+                        final EntryModel model = new EntryModel(modelObject, i);
+                        final int index = i;
                         AjaxEditableLabel<String> l =
-                            new AjaxEditableLabel<String>("value", new EntryModel(modelObject, i));
+                            new AjaxEditableLabel<String>("value", model);
                         container.add(l);
+                        container.add(new WebMarkupContainer("button").add(new AjaxEventBehavior("onclick") {
+                            protected void onEvent(AjaxRequestTarget target) {
+                                model.deleteSubElement(index);
+                                target.add(propertiesContainer);
+                                System.out.println("ajax here!");
+                            }
+                        }));
                         repeater.add(container);
                     }
                 } else {
@@ -223,6 +261,7 @@ public class ServiceEditorPanel extends Panel {
                     IModel<String> valueModel = new EntryModel(modelObject, 0);
                     AjaxEditableLabel<String> l = new AjaxEditableLabel<String>("value", valueModel);
                     container.add(l);
+                    container.add(new WebMarkupContainer("button"));
                     repeater.add(container);
                 }
 
