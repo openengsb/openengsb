6a82f865876b33c4eb0224f2e4a0b1f05f1165ce
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-07-26 15:19:35 +0200

    [OPENENGSB-3151] move ekb-api to separate bundle

--- itests/src/test/java/org/openengsb/itests/exam/DomainEndpointFactoryIT.java
+++ itests/src/test/java/org/openengsb/itests/exam/DomainEndpointFactoryIT.java
@@ -29,7 +29,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openengsb.core.api.AliveState;
