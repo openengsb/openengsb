d24ba84c0527ce7b472ad8b179fea2af301d1e05
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-08-02 18:54:06 +0200

    [OPENENGSB-3221] checkstyle-fix

--- components/workflow/src/main/java/org/openengsb/core/workflow/deployer/WorkflowDeployerService.java
+++ components/workflow/src/main/java/org/openengsb/core/workflow/deployer/WorkflowDeployerService.java
@@ -199,7 +199,7 @@ public class WorkflowDeployerService extends AbstractOpenEngSBService implements
         try {
             RuleBaseElementType typeFromFile = getTypeFromFile(artifact);
             String ending = FilenameUtils.getExtension(artifact.getName());
-            if(typeFromFile != null){
+            if (typeFromFile != null) {
                 doUpdateArtifact(artifact);
             } else if (IMPORT_ENDING.equals(ending)) {
                 installImportFile(artifact);
