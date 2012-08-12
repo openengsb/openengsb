0a1554e3123be862451d593673c5250592d947b2
Christoph Prybila <christoph.prybila@gmail.com>
2012-08-12 22:45:13 +0200

    [OPENENGSB-2881] added SecurityLogin Mock; adapted Tests to it

--- ui/admin/src/test/java/org/openengsb/ui/admin/xlink/ToolChooserTest.java
+++ ui/admin/src/test/java/org/openengsb/ui/admin/xlink/ToolChooserTest.java
@@ -17,51 +17,133 @@
 
 package org.openengsb.ui.admin.xlink;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
 import java.io.IOException;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import java.util.Collections;
+import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.aop.MethodInvocation;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.Authenticator;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.util.ThreadContext;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
+import org.junit.After;
 import static org.mockito.Mockito.anyList;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.isA;
 import static org.mockito.Mockito.verify;
+import org.openengsb.connector.usernamepassword.internal.UsernamePasswordServiceImpl;
+import org.openengsb.connector.wicketacl.WicketPermission;
+import org.openengsb.connector.wicketacl.internal.WicketAclServiceImpl;
+import org.openengsb.core.api.CompositeConnectorStrategy;
+import org.openengsb.core.api.Connector;
+import org.openengsb.core.api.ConnectorInstanceFactory;
+import org.openengsb.core.api.Constants;
+import org.openengsb.core.api.Domain;
+import org.openengsb.core.api.DomainProvider;
 import org.openengsb.core.api.OsgiUtilsService;
+import org.openengsb.core.api.context.ContextCurrentService;
 import org.openengsb.core.api.model.ModelDescription;
+import org.openengsb.core.api.persistence.ConfigPersistenceService;
+import org.openengsb.core.api.security.SecurityAttributeProvider;
+import org.openengsb.core.api.security.service.UserExistsException;
 import org.openengsb.core.api.xlink.model.ModelToViewsTupel;
 import org.openengsb.core.api.xlink.model.XLinkToolView;
+import org.openengsb.core.common.SecurityAttributeProviderImpl;
+import org.openengsb.core.common.util.DefaultOsgiUtilsService;
 import org.openengsb.core.ekb.api.ModelRegistry;
+import org.openengsb.core.persistence.internal.DefaultConfigPersistenceService;
+import org.openengsb.core.security.OpenEngSBShiroAuthenticator;
+import org.openengsb.core.security.internal.AdminAccessConnector;
+import org.openengsb.core.security.internal.AffirmativeBasedAuthorizationStrategy;
+import org.openengsb.core.security.internal.SecurityInterceptor;
+import org.openengsb.core.security.internal.model.RootPermission;
+import org.openengsb.core.services.internal.ConnectorManagerImpl;
+import org.openengsb.core.services.internal.ConnectorRegistrationManager;
+import org.openengsb.core.services.internal.DefaultWiringService;
+import org.openengsb.core.services.internal.virtual.CompositeConnectorProvider;
 import org.openengsb.core.services.xlink.XLinkUtils;
+import org.openengsb.core.test.DummyConfigPersistenceService;
+import org.openengsb.core.test.UserManagerStub;
+import org.openengsb.core.workflow.api.RuleManager;
 import org.openengsb.domain.DomainModelSQL.DomainModelSQLDomain;
+import org.openengsb.domain.auditing.AuditingDomain;
+import org.openengsb.domain.authorization.AuthorizationDomain;
+import org.openengsb.domain.authorization.AuthorizationDomain.Access;
+import org.openengsb.labs.delegation.service.ClassProvider;
+import org.openengsb.labs.delegation.service.internal.ClassProviderImpl;
 import org.openengsb.ui.admin.AbstractUITest;
+import org.openengsb.ui.admin.model.OpenEngSBFallbackVersion;
 import org.openengsb.ui.admin.xlink.mocking.ExampleObjectOrientedModel;
+import org.openengsb.ui.api.OpenEngSBVersionService;
 import org.ops4j.pax.wicket.test.spring.ApplicationContextMock;
 import org.ops4j.pax.wicket.test.spring.PaxWicketSpringBeanComponentInjector;
 
 public class ToolChooserTest extends AbstractUITest {
     
-    private static OsgiUtilsService mockedServiceUtils;
-    private static ApplicationContextMock customContext;
-    private static DomainModelSQLDomain connector;
+    private OsgiUtilsService mockedServiceUtils;
+    private ApplicationContextMock customContext;
+    private DomainModelSQLDomain connector;
     
     @Before
     public void setup() throws Exception {
         mockOsgiService();
+        startSecurityManager();
+        customMockContext();
         setupTesterWithSpringMockContext();
         mockRegistrationOfTools();
+        
+    }
+    
+    @After
+    public void cleanupShiro() {
+        // although a new thread should be spawned because of the DedicatedThread @Rule, but we want to be really sure
+        ThreadContext.unbindSecurityManager();
+        ThreadContext.unbindSubject();
+    }
+    
+    private void startSecurityManager(){
+        DefaultSecurityManager sm = new DefaultSecurityManager();
+        sm.setAuthenticator(new Authenticator() {
+            @Override
+            public AuthenticationInfo authenticate(AuthenticationToken authenticationToken)
+                throws AuthenticationException {
+                return new SimpleAuthenticationInfo(new SimplePrincipalCollection(authenticationToken.getPrincipal(),
+                    "openengsb"), authenticationToken.getCredentials());
+            }
+        });
+        SecurityUtils.setSecurityManager(sm);
+        ThreadContext.bind(sm);
+
+        AuthorizationDomain authorizer = mock(AuthorizationDomain.class);
+        when(authorizer.checkAccess(eq("admin"), any(MethodInvocation.class))).thenReturn(Access.GRANTED);
+
     }
     
     private void mockOsgiService() throws Exception{
@@ -71,13 +153,16 @@ public class ToolChooserTest extends AbstractUITest {
         Class clazz = ExampleObjectOrientedModel.class;
         when(registry.loadModel(isA(ModelDescription.class))).thenReturn(clazz);  
         connector = mock(DomainModelSQLDomain.class);
-        when(mockedServiceUtils.getService("service.pid=test2+test2+test2", 100L)).thenReturn(connector);
+        when(mockedServiceUtils.getService("(service.pid=test2+test2+test2)", 100L)).thenReturn(connector);
+    }
+    
+    private void customMockContext() throws UserExistsException{
+        customContext = new ApplicationContextMock();
+        customContext.putBean("osgiUtilsService", mockedServiceUtils);
+        customContext.putBean("serviceManager", serviceManager);      
     }
 
     private void setupTesterWithSpringMockContext() {
-        customContext = new ApplicationContextMock();
-        customContext.putBean("osgiUtilsService", mockedServiceUtils);
-        customContext.putBean("serviceManager", serviceManager);
         tester.getApplication().getComponentInstantiationListeners().remove(defaultPaxWicketInjector);
         tester.getApplication().getComponentInstantiationListeners()
             .add(new PaxWicketSpringBeanComponentInjector(tester.getApplication(), customContext));
