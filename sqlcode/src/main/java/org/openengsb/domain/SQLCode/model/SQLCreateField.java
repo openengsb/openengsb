2982e0dd567b1507e4ae354fd3482820934fd070
Christoph Prybila <christoph.prybila@gmail.com>
2012-08-16 17:14:40 +0200

    [OPENENGSB-2881] readded Files to Git after directory rename; changed Constraints List to arrays in SQLCreateField

---
+++ sqlcode/src/main/java/org/openengsb/domain/SQLCode/model/SQLCreateField.java
@@ -0,0 +1,60 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.domain.SQLCode.model;
+
+import java.util.List;
+
+public class SQLCreateField {
+    
+    private String fieldName;    
+    private String fieldType;    
+    private String[] constraints;
+
+    public SQLCreateField() {}
+
+    public SQLCreateField(String fieldName, String fieldType, String[] constraints) {
+        this.fieldName = fieldName;
+        this.fieldType = fieldType;
+        this.constraints = constraints;
+    }
+    
+    public String[] getConstraints() {
+        return constraints;
+    }
+
+    public void setConstraints(String[] constraints) {
+        this.constraints = constraints;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldType() {
+        return fieldType;
+    }
+
+    public void setFieldType(String fieldType) {
+        this.fieldType = fieldType;
+    }
+
+}
