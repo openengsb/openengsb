678e6f10d2a1f0d58fb88db26582a15cfbf81d7b
Felix Mayerhuber <felix.mayerhuber@gmail.com>
2012-10-08 20:59:11 +0200

    [OPENENGSB-3315] changed the model type value of the annotation from String to Class

--- src/main/java/org/openengsb/core/api/model/annotation/OpenEngSBForeignKey.java
+++ src/main/java/org/openengsb/core/api/model/annotation/OpenEngSBForeignKey.java
@@ -30,7 +30,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface OpenEngSBForeignKey {
-    String modelType();
+    Class<?> modelType();
 
     String modelVersion();
 }
