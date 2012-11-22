44a2981cdf1e8c9c32afb737b5b502706be4f22b
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-11-22 12:36:32 +0100

    [OPENENGSB-3375] add FileSerializer interface and implement CSV-parser

---
+++ connector/filewatcher/src/main/java/org/openengsb/connector/virtual/filewatcher/internal/CSVParser.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openengsb.connector.virtual.filewatcher.internal;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import org.openengsb.connector.virtual.filewatcher.FileSerializer;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.google.common.collect.Lists;
+
+public class CSVParser<ResultType> implements FileSerializer<ResultType> {
+
+    private Class<ResultType> resultType;
+
+    public CSVParser(Class<ResultType> resultType) {
+        this.resultType = resultType;
+    }
+
+    @Override
+    public List<ResultType> readFile(File path) throws IOException {
+        CsvMapper csvMapper = new CsvMapper();
+        CsvSchema schema = csvMapper.schemaFor(resultType);
+        try {
+            MappingIterator<ResultType> iterator = csvMapper.reader(schema).withType(resultType)
+                .readValues(path);
+            return Lists.newArrayList(iterator);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void writeFile(File path, List<ResultType> models) throws IOException {
+        CsvMapper csvMapper = new CsvMapper();
+        CsvSchema schema = csvMapper.schemaFor(resultType);
+        System.out.println(schema.getColumnDesc());
+        ObjectWriter writer = csvMapper.writer(schema);
+        // supplying the file directly does not work.
+        // java.lang.UnsupportedOperationException: Generator of type com.fasterxml.jackson.core.json.UTF8JsonGenerator
+        // does not support schema of type 'CSV'
+        writer.writeValue(new FileOutputStream(path), models);
+    }
+}
---
+++ connector/filewatcher/src/test/java/org/openengsb/connector/virtual/filewatcher/internal/CSVFileParserTest.java
@@ -0,0 +1,73 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openengsb.connector.virtual.filewatcher.internal;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.openengsb.connector.virtual.filewatcher.FileSerializer;
+
+public class CSVFileParserTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    private FileSerializer<TestModel> parser = new CSVParser<TestModel>(TestModel.class);
+    private File testcsv;
+
+    @Before
+    public void setUp() throws Exception {
+        testcsv = temporaryFolder.newFile("test.csv");
+    }
+
+    @Test
+    public void testParseCSVShouldCreateObject() throws Exception {
+        FileUtils.write(testcsv, ""
+                + "42,\"foo\", 7\n"
+                + "21,\"bar\", 9\n");
+        List<TestModel> testModels = parser.readFile(testcsv);
+        assertThat(testModels.size(), is(2));
+        TestModel model = testModels.get(0);
+        assertThat(model.getA(), is(42));
+        assertThat(model.getB(), is("foo"));
+        assertThat(model.getC(), is(7L));
+        model = testModels.get(1);
+        assertThat(model.getA(), is(21));
+        assertThat(model.getB(), is("bar"));
+        assertThat(model.getC(), is(9L));
+    }
+
+    @Test
+    public void testWriteCSVAndParseShouldBeTheSame() throws Exception {
+        List<TestModel> models = Arrays.asList(
+            new TestModel(42, "foo", 7L),
+            new TestModel(21, "bar", 9L)
+        );
+        parser.writeFile(testcsv, models);
+        List<TestModel> parsedModels = parser.readFile(testcsv);
+        assertThat(parsedModels, is(models));
+    }
+}
