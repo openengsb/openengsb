368cbd0ea7161decc0007191315af6d53ccb603e
Felix Mayerhuber <felix.mayerhuber@gmail.com>
2013-08-27 22:55:28 +0200

    [OPENENGSB-3659] added javadoc for the query request object

--- api/core/src/main/java/org/openengsb/core/api/model/QueryRequest.java
+++ api/core/src/main/java/org/openengsb/core/api/model/QueryRequest.java
@@ -20,7 +20,17 @@ package org.openengsb.core.api.model;
 import java.util.HashMap;
 import java.util.Map;
 
-// DOCU: 
+/**
+ * The QueryRequest object encapsulates a query request for data against the Engineering Database. It contains
+ * parameters which represent properties of models and allows to define meta data for the query. This meta data is:
+ * 
+ * timestamp = The timestamp defines for which point in time the query is performed (only the objects which were active
+ * at the given point in time and which are fitting the parameters are returned). The default value is
+ * System.currentTimeMillis().
+ * 
+ * wildcardAware = Defines if the values of the parameters are aware of wildcards. Wildcards are % for a generic
+ * sequence of characters and _ for exactly one unknown character. The default value for false. 
+ */
 public final class QueryRequest {
     private Map<String, Object> parameters;
     private long timestamp;
@@ -31,51 +41,84 @@ public final class QueryRequest {
         timestamp = System.currentTimeMillis();
         wildcardAware = false;
     }
-    
+
+    /**
+     * Creates a new QueryRequest object with no parameters.
+     */
     public static QueryRequest create() {
         return new QueryRequest();
     }
-    
+
+    /**
+     * Creates a new QueryRequest object and adds the given first parameter.
+     */
     public static QueryRequest query(String key, Object value) {
         return QueryRequest.create().addParameter(key, value);
     }
-    
+
+    /**
+     * Adds a parameter to this request.
+     */
     public QueryRequest addParameter(String key, Object value) {
         parameters.put(key, value);
         return this;
     }
-    
+
+    /**
+     * Removes a parameter from this request.
+     */
     public QueryRequest removeParameter(String key) {
         parameters.remove(key);
         return this;
     }
-    
+
+    /**
+     * Returns the value for the parameter with the given key in the request.
+     */
     public Object getParameter(String key) {
         return parameters.get(key);
     }
 
+    /**
+     * Returns the map of parameters for this request.
+     */
     public Map<String, Object> getParameters() {
         return parameters;
     }
 
+    /**
+     * Returns the timestamp which was choosen for this request.
+     */
     public long getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * Sets the timestamp for which this request should be performed.
+     */
     public QueryRequest setTimestamp(long timestamp) {
         this.timestamp = timestamp;
         return this;
     }
-    
+
+    /**
+     * Returns true if this request is wildcard aware and false if it is not.
+     */
     public boolean isWildcardAware() {
         return wildcardAware;
     }
-    
+
+    /**
+     * Sets this request wildcard aware.
+     */
     public QueryRequest wildcardAware() {
         this.wildcardAware = true;
         return this;
     }
 
+    /**
+     * Sets this request wildcard unaware.
+     */
     public QueryRequest wildcardUnaware() {
         this.wildcardAware = false;
         return this;
