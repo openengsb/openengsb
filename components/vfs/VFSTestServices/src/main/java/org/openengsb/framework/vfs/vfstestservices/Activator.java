c323142b141e09e5cef17e91de7000a98b907df8
Julian Pfanner <julian.pfanner@gmail.com>
2013-09-25 16:16:07 +0200

    added test services for manual testing

---
+++ components/vfs/VFSTestServices/src/main/java/org/openengsb/framework/vfs/vfstestservices/Activator.java
@@ -0,0 +1,37 @@
+package org.openengsb.framework.vfs.vfstestservices;
+
+import com.sun.org.apache.bcel.internal.util.BCELifier;
+import java.util.ArrayList;
+import java.util.List;
+import org.openengsb.framework.vfs.configurationserviceapi.configurableservice.ConfigurableService;
+import org.openengsb.framework.vfs.configurationserviceapi.remoteservice.RemoteService;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+    @Override
+    public void start(BundleContext context) throws Exception {
+        List<String> propertyList1 = new ArrayList<>();
+        propertyList1.add(""); //TODO
+        
+        ConfigurableService configurableService1 = new TestConfigurableService(propertyList1, true);
+        
+        context.registerService(ConfigurableService.class.getName(), configurableService1, null);
+        
+        List<String> propertyList2 = new ArrayList<>();
+        propertyList1.add(""); //TODO
+        
+        ConfigurableService configurableService2 = new TestConfigurableService(propertyList2, true);
+        
+        context.registerService(ConfigurableService.class.getName(), configurableService2, null);
+        
+        RemoteService remoteService = new TestRemoteService(true, true);
+        context.registerService(RemoteService.class.getName(), remoteService, null);
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        
+    }
+
+}
